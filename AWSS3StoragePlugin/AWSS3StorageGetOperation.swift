20e63935b4de48d881394325aee4bacd48c61039
Michael Law <1365977+lawmicha@users.noreply.github.com>
2019-08-19 10:43:17 -0700

    Remove AWSS3StorageGetOperation temp code

--- AWSS3StoragePlugin/AWSS3StorageGetOperation.swift
+++ AWSS3StoragePlugin/AWSS3StorageGetOperation.swift
@@ -46,69 +46,6 @@ public class AWSS3StorageGetOperation: AmplifyOperation<Progress, StorageGetResu
     }
     
     override public func main() {
-        print("Executing AWSS3StorageGetOperation")
-        let transferUtility = AWSS3TransferUtility.s3TransferUtility(forKey: "AWSS3StoragePlugin")
-        let downloadExpression = AWSS3TransferUtilityDownloadExpression()
-        downloadExpression.progressBlock = {(task, progress) in
-            print("got progress")
-            self.emitEvent(progress: progress)
-        }
         
-        let downloadCompletionHandler: AWSS3TransferUtilityDownloadCompletionHandlerBlock = { (task, location, data, error ) in
-            print("completion handler called")
-            if let HTTPResponse = task.response {
-                print("Status: \(HTTPResponse)")
-            }
-            print("data : \(data!)")
-            if let data =  data {
-                self.emitSuccess(data: data)
-            }
-            if let error = error {
-                print("Error \(error)")
-            }
-        }
-        
-        if (isCancelled) {
-            return
-        }
-        
-        print("downloading for key \(key)")
-        transferUtility?.downloadData(
-            fromBucket: "swiftstoragesample1fd7e03cf4804cdaac1f0d548fbe3aa0-devo", // TODO: replace this call with the one that uses the internally configured bucket.
-            key: key,
-            expression: downloadExpression,
-            completionHandler: downloadCompletionHandler).continueWith(block: { (task) -> AnyObject? in
-                print("contineWith")
-                if let error = task.error {
-                    print("Error: \(error.localizedDescription)");
-//                    DispatchQueue.main.async(execute: {
-//                        self.statusLabel.text = "Failed"
-//                    })
-                    // todo emit failed event
-                }
-                // emit error event if any
-                if let downloadTask = task.result {
-                    if (self.isCancelled) {
-                        downloadTask.cancel()
-                    } else {
-                        self.refGetTask = downloadTask
-                        self.emitEvent(progress: Progress())
-                    }
-                }
-                
-                return nil
-            })
-    }
-    
-    // Temporary hack to replace Hub notifications
-//    override open func subscribe(_ onEvent: @escaping (AsyncEvent<Progress, StorageGetResult, StorageGetError>) -> Void) -> Unsubscribe {
-//        print("subscribed with event listener")
-//        self.onEvent = onEvent
-//        return unsubscribe
-//    }
-    
-    func unsubscribe() {
-        print("unsubscribing")
-        self.onEvent = nil
     }
 }
