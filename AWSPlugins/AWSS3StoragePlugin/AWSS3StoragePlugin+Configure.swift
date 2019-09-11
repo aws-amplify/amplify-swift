0b676e63557e236944ca6029f3c34758a8a80c53
Michael Law <1365977+lawmicha@users.noreply.github.com>
2019-09-11 15:53:09 -0700

    [Storage][AWSS3PluginConfigure] Moved plugin configuraiton error to PluginErrorConstants file and unit testing

--- AWSPlugins/AWSS3StoragePlugin/AWSS3StoragePlugin+Configure.swift
+++ AWSPlugins/AWSS3StoragePlugin/AWSS3StoragePlugin+Configure.swift
@@ -21,13 +21,14 @@ extension AWSS3StoragePlugin {
     ///   - PluginError.pluginConfigurationError: If one of the configuration values is invalid or empty
     public func configure(using configuration: Any) throws {
         guard let config = configuration as? JSONValue else {
-            throw PluginError.pluginConfigurationError("Unexpected configuration object not castable to JSONValue",
-                                                       "")
+            throw PluginError.pluginConfigurationError(PluginErrorConstants.decodeConfigurationError.errorDescription,
+                                                       PluginErrorConstants.decodeConfigurationError.recoverySuggestion)
         }
 
         guard case let .object(configObject) = config else {
-            throw PluginError.pluginConfigurationError("Did not find config object",
-                                                       "")
+            throw PluginError.pluginConfigurationError(
+                PluginErrorConstants.configurationObjectExpected.errorDescription,
+                PluginErrorConstants.configurationObjectExpected.recoverySuggestion)
         }
 
         let bucket = try AWSS3StoragePlugin.getBucket(configObject)
@@ -71,18 +72,18 @@ extension AWSS3StoragePlugin {
     /// Retrieves the bucket from configuration, validates, and returns it.
     private static func getBucket(_ configuration: [String: JSONValue]) throws -> String {
         guard let bucket = configuration[PluginConstants.Bucket] else {
-            throw PluginError.pluginConfigurationError("Bucket not in configuration",
-                                                       "Bucket should be in the configuration")
+            throw PluginError.pluginConfigurationError(PluginErrorConstants.missingBucket.errorDescription,
+                                                       PluginErrorConstants.missingBucket.recoverySuggestion)
         }
 
         guard case let .string(bucketValue) = bucket else {
-            throw PluginError.pluginConfigurationError("Missing bucket value",
-                                                       "")
+            throw PluginError.pluginConfigurationError(PluginErrorConstants.invalidBucket.errorDescription,
+                                                       PluginErrorConstants.invalidBucket.recoverySuggestion)
         }
 
         if bucketValue.isEmpty {
-            throw PluginError.pluginConfigurationError("Bucket is empty",
-                                                       "Bucket should not be empty in the configuration")
+            throw PluginError.pluginConfigurationError(PluginErrorConstants.emptyBucket.errorDescription,
+                                                       PluginErrorConstants.emptyBucket.recoverySuggestion)
         }
 
         return bucketValue
@@ -91,24 +92,24 @@ extension AWSS3StoragePlugin {
     /// Retrieves the region from configuration, validates, and transforms to and returns the AWSRegionType
     private static func getRegionType(_ configuration: [String: JSONValue]) throws -> AWSRegionType {
         guard let region = configuration[PluginConstants.Region] else {
-            throw PluginError.pluginConfigurationError("Region not in configuration",
-                                                       "Region should be in the configuration")
+            throw PluginError.pluginConfigurationError(PluginErrorConstants.missingRegion.errorDescription,
+                                                       PluginErrorConstants.missingRegion.recoverySuggestion)
         }
 
         guard case let .string(regionValue) = region else {
-            throw PluginError.pluginConfigurationError("Missing region value",
-                                                       "")
+            throw PluginError.pluginConfigurationError(PluginErrorConstants.invalidRegion.errorDescription,
+                                                       PluginErrorConstants.invalidRegion.recoverySuggestion)
         }
 
         if regionValue.isEmpty {
-            throw PluginError.pluginConfigurationError("Region is empty",
-                                                       "")
+            throw PluginError.pluginConfigurationError(PluginErrorConstants.emptyRegion.errorDescription,
+                                                       PluginErrorConstants.emptyRegion.recoverySuggestion)
         }
 
         let regionType = regionValue.aws_regionTypeValue()
         guard regionType != AWSRegionType.Unknown else {
-            throw PluginError.pluginConfigurationError("Region is Unknown",
-                                                       "")
+            throw PluginError.pluginConfigurationError(PluginErrorConstants.invalidRegion.errorDescription,
+                                                       PluginErrorConstants.invalidRegion.recoverySuggestion)
         }
 
         return regionType
@@ -119,14 +120,16 @@ extension AWSS3StoragePlugin {
     private static func getDefaultAccessLevel(_ configuration: [String: JSONValue]) throws -> StorageAccessLevel {
         if let defaultAccessLevelConfig = configuration[PluginConstants.DefaultAccessLevel] {
             guard case let .string(defaultAccessLevelString) = defaultAccessLevelConfig else {
-                throw PluginError.pluginConfigurationError("Default Access Level configured but not a string",
-                                                           "")
+                throw PluginError.pluginConfigurationError(
+                    PluginErrorConstants.invalidDefaultAccessLevel.errorDescription,
+                    PluginErrorConstants.invalidDefaultAccessLevel.recoverySuggestion)
             }
 
             let defaultAccessLevelOptional = StorageAccessLevel.init(rawValue: defaultAccessLevelString)
             guard let defaultAccessLevel = defaultAccessLevelOptional else {
-                throw PluginError.pluginConfigurationError("Default Access Level not correct string",
-                                                           "")
+                throw PluginError.pluginConfigurationError(
+                    PluginErrorConstants.invalidDefaultAccessLevel.errorDescription,
+                    PluginErrorConstants.invalidDefaultAccessLevel.recoverySuggestion)
             }
 
             return defaultAccessLevel
--- AWSPlugins/AWSS3StoragePluginTests/AWSS3StoragePluginConfigureTests.swift
+++ AWSPlugins/AWSS3StoragePluginTests/AWSS3StoragePluginConfigureTests.swift
@@ -11,8 +11,7 @@ import Amplify
 
 class AWSS3StoragePluginConfigureTests: AWSS3StoragePluginTests {
 
-    // MARK: configuration tests
-    func testConfigure() throws {
+    func testConfigureSuccess() throws {
         let bucket = JSONValue.init(stringLiteral: testBucket)
         let region = JSONValue.init(stringLiteral: testRegion)
         let storagePluginConfig = JSONValue.init(
@@ -25,37 +24,227 @@ class AWSS3StoragePluginConfigureTests: AWSS3StoragePluginTests {
         }
     }
 
-    func testConfigureWithDefaultAccessLevelWithOverrideRequestAccessLevelOnAPICalls() {
-        XCTFail("Not yet implemented")
+    func testConfigureWithDefaultAccessLevelSuccess() {
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let region = JSONValue.init(stringLiteral: testRegion)
+        let accessLevel = JSONValue.init(stringLiteral: defaultAccessLevel.rawValue)
+        let storagePluginConfig = JSONValue.init(dictionaryLiteral:
+            (PluginConstants.Bucket, bucket),
+            (PluginConstants.Region, region),
+            (PluginConstants.DefaultAccessLevel, accessLevel))
+
+        do {
+            try storagePlugin.configure(using: storagePluginConfig)
+        } catch {
+            XCTFail("Failed to configure storage plugin with default access level")
+        }
     }
 
-    func testConfigureThrowsErrorForMissingBucket() {
-        XCTFail("Not yet implemented")
+    func testConfigureThrowsErrorForMissingConfiguration() {
+        XCTAssertThrowsError(try storagePlugin.configure(using: "")) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.decodeConfigurationError.errorDescription)
+        }
     }
 
-    func testConfigureThrowsForEmptyBucket() {
-        XCTFail("Not yet implemented")
+    func testConfigureThrowsErrorForMissingConfigurationObject() {
+        let storagePluginConfig = JSONValue.init(stringLiteral: "notADictionaryLiteral")
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.configurationObjectExpected.errorDescription)
+        }
     }
 
-    func testConfigureThrowsErrorForMissingRegion() {
-        XCTFail("Not yet implemented")
+    func testConfigureThrowsErrorForMissingBucketConfig() {
+        let region = JSONValue.init(stringLiteral: testRegion)
+        let storagePluginConfig = JSONValue.init(
+            dictionaryLiteral: (PluginConstants.Region, region))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.missingBucket.errorDescription)
+        }
     }
 
-    func testConfigureThrowsForEmptyRegion() {
-        XCTFail("Not yet implemented")
+    func testConfigureThrowsForEmptyBucketValue() {
+        let region = JSONValue.init(stringLiteral: testRegion)
+        let storagePluginConfig = JSONValue.init(
+            dictionaryLiteral: (PluginConstants.Bucket, ""), (PluginConstants.Region, region))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.emptyBucket.errorDescription)
+        }
     }
 
-    func testConfigureThrowsForInvalidRegion() {
-        XCTFail("Not yet implemented")
+    func testConfigureThrowsForInvalidBucketValue() {
+        let bucket = JSONValue.init(integerLiteral: 1)
+        let region = JSONValue.init(stringLiteral: testRegion)
+        let storagePluginConfig = JSONValue.init(
+            dictionaryLiteral: (PluginConstants.Bucket, bucket), (PluginConstants.Region, region))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.invalidBucket.errorDescription)
+        }
+    }
+
+    func testConfigureThrowsErrorForMissingRegionConfig() {
+
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let storagePluginConfig = JSONValue.init(
+            dictionaryLiteral: (PluginConstants.Bucket, bucket))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.missingRegion.errorDescription)
+        }
+    }
+
+    func testConfigureThrowsErrorForMissingRegionValue() {
+
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let storagePluginConfig = JSONValue.init(
+            dictionaryLiteral: (PluginConstants.Bucket, bucket), (PluginConstants.Region, ""))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.missingRegion.errorDescription)
+        }
+    }
+
+    func testConfigureThrowsForEmptyRegionValue() {
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let region = JSONValue.init(stringLiteral: "")
+        let storagePluginConfig = JSONValue.init(
+            dictionaryLiteral: (PluginConstants.Bucket, bucket), (PluginConstants.Region, region))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.emptyRegion.errorDescription)
+        }
+    }
+
+    func testConfigureThrowsForInvalidRegionValue() {
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let region = JSONValue.init(integerLiteral: 1)
+        let storagePluginConfig = JSONValue.init(
+            dictionaryLiteral: (PluginConstants.Bucket, bucket), (PluginConstants.Region, region))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.invalidBucket.errorDescription)
+        }
+    }
+
+    func testConfigureThrowsForInvalidRegionType() {
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let region = JSONValue.init(stringLiteral: "invalidRegionType")
+        let storagePluginConfig = JSONValue.init(
+            dictionaryLiteral: (PluginConstants.Bucket, bucket), (PluginConstants.Region, region))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.invalidRegion.errorDescription)
+        }
     }
 
     func testConfigureThrowsForInvalidDefaultAccessLevel() {
-        XCTFail("Not yet implemented")
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let region = JSONValue.init(stringLiteral: testRegion)
+        let accessLevel = JSONValue.init(stringLiteral: "invalidAccessLevel")
+        let storagePluginConfig = JSONValue.init(dictionaryLiteral:
+            (PluginConstants.Bucket, bucket),
+            (PluginConstants.Region, region),
+            (PluginConstants.DefaultAccessLevel, accessLevel))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.invalidDefaultAccessLevel.errorDescription)
+        }
+    }
+
+    func testConfigureThrowsForInvalidDefaultAccessLevelString() {
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let region = JSONValue.init(stringLiteral: testRegion)
+        let accessLevel = JSONValue.init(integerLiteral: 1)
+        let storagePluginConfig = JSONValue.init(dictionaryLiteral:
+            (PluginConstants.Bucket, bucket),
+                                                 (PluginConstants.Region, region),
+                                                 (PluginConstants.DefaultAccessLevel, accessLevel))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.invalidDefaultAccessLevel.errorDescription)
+        }
     }
 
     func testConfigureThrowsForSpecifiedAndEmptyDefaultAccessLevel() {
-        XCTFail("Not yet implemented")
+
+        let bucket = JSONValue.init(stringLiteral: testBucket)
+        let region = JSONValue.init(stringLiteral: testRegion)
+        let accessLevel = JSONValue.init(stringLiteral: "")
+        let storagePluginConfig = JSONValue.init(dictionaryLiteral:
+            (PluginConstants.Bucket, bucket),
+            (PluginConstants.Region, region),
+            (PluginConstants.DefaultAccessLevel, accessLevel))
+
+        XCTAssertThrowsError(try storagePlugin.configure(using: storagePluginConfig)) { error in
+            guard case let PluginError.pluginConfigurationError(errorDescription, _) = error else {
+                XCTFail("Expected PluginError pluginConfigurationError, got: \(error)")
+                return
+            }
+
+            XCTAssertEqual(errorDescription, PluginErrorConstants.invalidDefaultAccessLevel.errorDescription)
+        }
     }
-
-
 }
