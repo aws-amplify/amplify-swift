a72163ed56134337278e0584491d96a5583995bf
Michael Law <1365977+lawmicha@users.noreply.github.com>
2019-10-25 10:55:43 -0700

    AWSPinpointAnalyticsPlugin Configure implementation (merged to master) (#51)
    * AWSPinpointAnalyticsPlugin Configure implementation
    
        - Setting up AWSPinpointAnalyticsPlugin (properties, configure, reset, API stubs)
        - configure using Any and AWSPinpointAnalyticsPluginConfiguration
        - Adding PinpointAdapter class to instantiate pinpoint and later wrap pinpoint APIs
        - Integration tests on configuring plugin and pinpoint adapter
    
    * Add integration tests

---
+++ AWSPlugins/Analytics/AWSPinpointAnalyticsPluginTests/Mocks/MockAWSPinpoint.swift
@@ -0,0 +1,14 @@
+//
+// Copyright 2018-2019 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+@testable import AWSPinpointAnalyticsPlugin
+
+public class MockAWSPinpoint: AWSPinpointBehavior {
+    // TODO: implment MockAWSPinpoint
+}
