899db76d4c7601d988f234ad1568aff0d379cf86
Tim Schmelter <schmelte@amazon.com>
2019-11-07 13:57:07 -0800

    Rename DefaultHubPlugin to AWSHubPlugin

--- AmplifyTests/CategoryTests/Hub/DefaultPluginTests/DefaultHubPluginCustomChannelTests.swift
+++ AmplifyTests/CategoryTests/Hub/DefaultPluginTests/DefaultHubPluginCustomChannelTests.swift
@@ -13,9 +13,9 @@ import XCTest
 class DefaultHubPluginCustomChannelTests: XCTestCase {
 
     var plugin: HubCategoryPlugin {
-        guard let plugin = try? Amplify.Hub.getPlugin(for: "DefaultHubCategoryPlugin"),
-            plugin.key == "DefaultHubCategoryPlugin" else {
-                fatalError("Could not access DefaultHubCategoryPlugin")
+        guard let plugin = try? Amplify.Hub.getPlugin(for: "AWSHubPlugin"),
+            plugin.key == "AWSHubPlugin" else {
+                fatalError("Could not access AWSHubPlugin")
         }
         return plugin
     }
