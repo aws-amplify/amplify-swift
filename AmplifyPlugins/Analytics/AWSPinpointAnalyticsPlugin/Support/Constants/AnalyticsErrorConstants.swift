8d2dfa0b91b3fdc521de18aad631bfba0fdff623
Michael Law <1365977+lawmicha@users.noreply.github.com>
2019-11-13 17:47:36 -0800

    Analytics Plugin fixes for issues when doing external pod install  (#95)
    * Analytics Plugin fixes for issues when doing external pod install in E2E app
    
    * fix podspec

--- AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Support/Constants/AnalyticsErrorConstants.swift
+++ AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Support/Constants/AnalyticsErrorConstants.swift
@@ -8,74 +8,74 @@
 import Foundation
 import Amplify
 
-typealias PluginErrorString = (errorDescription: ErrorDescription, recoverySuggestion: RecoverySuggestion)
+typealias AnalyticsErrorString = (errorDescription: ErrorDescription, recoverySuggestion: RecoverySuggestion)
 
-struct PluginErrorConstants {
-    static let decodeConfigurationError: PluginErrorString = (
+struct AnalyticsErrorConstants {
+    static let decodeConfigurationError: AnalyticsErrorString = (
         "Unable to decode configuration",
         "Make sure the plugin configuration is JSONValue")
 
-    static let configurationObjectExpected: PluginErrorString = (
+    static let configurationObjectExpected: AnalyticsErrorString = (
         "Configuration was not a dictionary literal",
         "Make sure the value for the plugin is a dictionary literal with keys")
 
-    static let missingPinpointAnalyicsConfiguration: PluginErrorString = (
+    static let missingPinpointAnalyicsConfiguration: AnalyticsErrorString = (
         "Plugin is missing `PinpointAnalytics` section.",
         "Add the `PinpointAnalytics` section to the plugin.")
 
-    static let pinpointAnalyticsConfigurationExpected: PluginErrorString = (
+    static let pinpointAnalyticsConfigurationExpected: AnalyticsErrorString = (
         "Configuration at `PinpointAnalytics` is not a dictionary literal",
         "Make sure the value for the `PinpointAnalytics` is a dictionary literal with `AppId` and `Region`")
 
-    static let missingPinpointTargetingConfiguration: PluginErrorString = (
+    static let missingPinpointTargetingConfiguration: AnalyticsErrorString = (
         "Plugin is missing `PinpointTargeting` section.",
         "Add the `PinpointTargeting` section to the plugin.")
 
-    static let pinpointTargetingConfigurationExpected: PluginErrorString = (
+    static let pinpointTargetingConfigurationExpected: AnalyticsErrorString = (
         "Configuration at `PinpointTargeting` is not a dictionary literal",
         "Make sure the value for the `PinpointTargeting` is a dictionary literal with `Region`")
 
-    static let missingAppId: PluginErrorString = (
+    static let missingAppId: AnalyticsErrorString = (
         "AppId is missing",
         "Add AppId to the configuration")
 
-    static let invalidAppId: PluginErrorString = (
+    static let invalidAppId: AnalyticsErrorString = (
         "AppId is not a string",
         "Ensure AppId is a string")
 
-    static let emptyAppId: PluginErrorString = (
+    static let emptyAppId: AnalyticsErrorString = (
         "AppId is specified but is empty",
         "AppId should not be empty")
 
-    static let missingRegion: PluginErrorString = (
+    static let missingRegion: AnalyticsErrorString = (
         "Region is missing",
         "Add region to the configuration")
 
-    static let invalidRegion: PluginErrorString = (
+    static let invalidRegion: AnalyticsErrorString = (
         "Region is invalid",
         "Ensure Region is a valid region value")
 
-    static let emptyRegion: PluginErrorString = (
+    static let emptyRegion: AnalyticsErrorString = (
         "Region is empty",
         "Ensure should not be empty")
 
-    static let invalidAutoFlushEventsInterval: PluginErrorString = (
+    static let invalidAutoFlushEventsInterval: AnalyticsErrorString = (
         "AutoFlushEventsInterval is not a number or is less than 0",
         "Ensure AutoFlushEventsInterval is zero or positive number")
 
-    static let invalidTrackAppSessions: PluginErrorString = (
+    static let invalidTrackAppSessions: AnalyticsErrorString = (
         "TrackAppSessions is not a boolean value",
         "Ensure TrackAppSessions is either `true` or `false`")
 
-    static let invalidAutoSessionTrackingInterval: PluginErrorString = (
+    static let invalidAutoSessionTrackingInterval: AnalyticsErrorString = (
         "AutoSessionTrackingInterval is not a number of is less than 0",
         "Ensure AutoSessionTrackingInterval is zero or positive number")
 
-    static let pinpointAnalyticsServiceConfigurationError: PluginErrorString = (
+    static let pinpointAnalyticsServiceConfigurationError: AnalyticsErrorString = (
         "Could not instantiate service configuration from pinpoint analytics region",
         "Make sure the pinpoint analytics region and cognito credentials provider are correct")
 
-    static let pinpointTargetingServiceConfigurationError: PluginErrorString = (
+    static let pinpointTargetingServiceConfigurationError: AnalyticsErrorString = (
         "Could not instantiate service configuration from pinpoint targeting region",
         "Make sure the pinpoint targeting region and cognito credentials provider are correct")
 }
