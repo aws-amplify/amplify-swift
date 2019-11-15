592582549704e950e3f0200e1b5c3972dccaf309
Nicki Stone <nickik@amazon.com>
2019-11-15 15:11:12 -0800

    review changes with major refactor to polly voice id enum

--- Amplify/Categories/Predictions/Models/VoiceType.swift
+++ Amplify/Categories/Predictions/Models/VoiceType.swift
@@ -1,70 +1,8 @@
 //
-// Copyright 2018-2019 Amazon.com,
-// Inc. or its affiliates. All Rights Reserved.
+//  VoiceType.swift
+//  Amplify
 //
-// SPDX-License-Identifier: Apache-2.0
+//  Created by Stone, Nicki on 11/15/19.
 //
-
 
 import Foundation
-
-public enum VoiceType: CaseIterable {
-    case arabicFemaleZeina
-    case chineseFemaleZhiyu
-    case danishFemaleNaja
-    case danishMaleMads
-    case dutchFemaleLotte
-    case dutchMaleRuben
-    case ausEnglishFemaleNicole
-    case ausEnglishMaleRussell
-    case britEnglishFemaleAmy
-    case britEnglishFemaleEmma
-    case britEnglishMaleBrian
-    case indiEnglishAndHindFemaleAditi
-    case indiEnglishFemaleRaveena
-    case englishFemaleIvy
-    case englishFemaleJoanna
-    case englishFemaleKendra
-    case englishFemaleKimberly
-    case englishFemaleSalli
-    case englishMaleJoey
-    case englishMaleJustin
-    case englishMaleMatthew
-    case welshEnglishMaleGeraint
-    case frenchFemaleCeline
-    case frenchFemaleLea
-    case frenchMaleMathieu
-    case canadianFrenchFemaleChantal
-    case germanFemaleMarlene
-    case germanFemaleVicki
-    case germanMaleHans
-    case icelandicFemaleDora
-    case icelandicMaleKarl
-    case italianFemaleCarla
-    case italianFemaleBianca
-    case italianMaleGiorgio
-    case japaneseFemaleMizuki
-    case japaneseMaleTakumi
-    case koreanFemaleSeoyeon
-    case norwegianFemaleLiv
-    case polishFemaleEwa
-    case polishFemaleMaja
-    case polishMaleJacek
-    case polishMaleJan
-    case brazPortugueseFemaleVitoria
-    case brazPortugueseMaleRicardo
-    case euroPortugueseFemaleInes
-    case euroPortugueseMaleCristiano
-    case romanianFemaleCarmen
-    case russianFemaleTatyana
-    case russianMaleMaxim
-    case euroSpanishFemaleConchita
-    case euroSpanishFemaleLucia
-    case euroSpanishMaleEnrique
-    case mexSpanishFemaleMia
-    case usSpanishFemalePenelope
-    case usSpanishMaleMiguel
-    case swedishFemaleAstrid
-    case turkishFemaleFiliz
-    case welshFemaleGwyneth
-}
---
+++ AmplifyPlugins/Predictions/AWSPredictionsPlugin/Support/Internal/AWSPollyVoiceId+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  AWSPollyVoiceId+Extension.swift
+//  AWSPredictionsPlugin
+//
+//  Created by Stone, Nicki on 11/15/19.
+//  Copyright © 2019 Amazon Web Services. All rights reserved.
+//
+
+import Foundation
