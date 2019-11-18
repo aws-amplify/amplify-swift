265e6c37e59c0e6c78df931b78311141eba04e91
Michael Law <1365977+lawmicha@users.noreply.github.com>
2019-11-18 11:25:35 -0800

    finish merge

--- Amplify/Categories/Predictions/Request/PredictionsTextToSpeechRequest.swift
+++ Amplify/Categories/Predictions/Request/PredictionsTextToSpeechRequest.swift
@@ -28,17 +28,17 @@ public extension PredictionsTextToSpeechRequest {
 
         /// The calltype for the operation. The default value will be `auto`.
         public let callType: CallType
-        
+
         public let voiceType: VoiceType?
 
         /// Extra plugin specific options, only used in special circumstances when the existing options do not provide
         /// a way to utilize the underlying storage system's functionality. See plugin documentation for expected
         /// key/values
-        public let pluginOptions: [String:Any]?
+        public let pluginOptions: [String: Any]?
 
         public init(callType: CallType = .auto,
                     voiceType: VoiceType? = nil,
-                    pluginOptions: [String:Any]? = nil) {
+                    pluginOptions: [String: Any]? = nil) {
             self.callType = callType
             self.voiceType = voiceType
             self.pluginOptions = pluginOptions
