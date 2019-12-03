36e793b42e34a3859df6223a1aa51112509c0481
Tim Schmelter <schmelte@amazon.com>
2019-12-02 19:50:45 -0800

    Add concurrency queue to ModelRegistry

--- Amplify/Categories/DataStore/Model/ModelRegistry.swift
+++ Amplify/Categories/DataStore/Model/ModelRegistry.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 public struct ModelRegistry {
+    private static let concurrencyQueue = DispatchQueue(label: "com.amazonaws.ModelRegistry.concurrency",
+                                                        target: DispatchQueue.global())
 
     /// ModelDecoders are used to decode untyped model data, looking up by model name
     private typealias ModelDecoder = (String, JSONDecoder?) throws -> Model
@@ -17,43 +19,53 @@ public struct ModelRegistry {
     private static var modelDecoders = [String: ModelDecoder]()
 
     public static var models: [Model.Type] {
-        Array(modelTypes.values)
+        concurrencyQueue.sync {
+            Array(modelTypes.values)
+        }
     }
 
     public static func register<M: Model>(modelType: M.Type) {
-        let modelDecoder: ModelDecoder = { jsonString, jsonDecoder in
-            let model = try modelType.from(json: jsonString, decoder: jsonDecoder)
-            return model
+        concurrencyQueue.sync {
+            let modelDecoder: ModelDecoder = { jsonString, jsonDecoder in
+                let model = try modelType.from(json: jsonString, decoder: jsonDecoder)
+                return model
+            }
+
+            modelDecoders[modelType.modelName] = modelDecoder
+
+            modelTypes[modelType.modelName] = modelType
         }
-
-        modelDecoders[modelType.modelName] = modelDecoder
-
-        modelTypes[modelType.modelName] = modelType
     }
 
     public static func modelType(from name: String) -> Model.Type? {
-        modelTypes[name]
+        concurrencyQueue.sync {
+            modelTypes[name]
+        }
     }
 
     public static func decode(modelName: String,
                               from jsonString: String,
                               jsonDecoder: JSONDecoder? = nil) throws -> Model {
-        guard let decoder = modelDecoders[modelName] else {
-            throw DataStoreError.decodingError(
-                "No decoder found for model named \(modelName)",
-                """
-                There is no decoder registered for the model named \(modelName). \
-                Register models with `ModelRegistry.register(modelName:)` at startup.
-                """)
-        }
+        try concurrencyQueue.sync {
+            guard let decoder = modelDecoders[modelName] else {
+                throw DataStoreError.decodingError(
+                    "No decoder found for model named \(modelName)",
+                    """
+                    There is no decoder registered for the model named \(modelName). \
+                    Register models with `ModelRegistry.register(modelName:)` at startup.
+                    """)
+            }
 
-        return try decoder(jsonString, jsonDecoder)
+            return try decoder(jsonString, jsonDecoder)
+        }
     }
 }
 
 extension ModelRegistry {
     static func reset() {
-        modelTypes = [:]
-        modelDecoders = [:]
+        concurrencyQueue.sync {
+            modelTypes = [:]
+            modelDecoders = [:]
+        }
     }
 }
