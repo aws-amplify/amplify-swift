859976b01fb48d6c2f4d509fefe0c784b3628e95
Jithin Roy <51138777+royjit@users.noreply.github.com>
2020-04-06 15:05:41 -0700

    Bare bone setup for Auth Plugin (#372)
    * Bare bone setup for Auth Plugin
    
    * Fixed deployment to 11.0

---
+++ AmplifyPlugins/Auth/HostApp/Source/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+// Copyright 2018-2020 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    @available(iOS 13.0, *)
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
