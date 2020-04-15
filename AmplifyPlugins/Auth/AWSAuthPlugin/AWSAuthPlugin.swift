374cfbb67e5a78afc8f15afc3163edd5304828c2
Jithin Roy <51138777+royjit@users.noreply.github.com>
2020-04-15 14:08:30 -0700

    Implemented plugin configuration and signup method (#384)

--- AmplifyPlugins/Auth/AWSAuthPlugin/AWSAuthPlugin.swift
+++ AmplifyPlugins/Auth/AWSAuthPlugin/AWSAuthPlugin.swift
@@ -5,4 +5,30 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-import Foundation
+import Amplify
+import AWSMobileClient
+
+/// Auth plugin that uses AWS Cognito UserPool and IdentityPool.
+///
+/// The implicitly unwrapped optionals in this class are assigned in the `configure` method in `AWSAuthPlugin+Configure`
+/// extension. Make sure to call `Amplify.configure` after adding the plugin to `Amplify`.
+final public class AWSAuthPlugin: AuthCategoryPlugin {
+
+    /// A queue that regulates the execution of operations.
+    var queue: OperationQueue!
+
+    /// Operations related to the authentication provider.
+    var authenticationProvider: AuthenticationProviderBehavior!
+
+    /// Operations related to the authorization provider
+    var authorizationProvider: AuthorizationProviderBehavior!
+
+    /// The unique key of the plugin within the auth category.
+    public var key: PluginKey {
+        return "awsCognitoAuthPlugin"
+    }
+
+    /// Instantiates an instance of the AWSAuthPlugin.
+    public init() {
+    }
+}
