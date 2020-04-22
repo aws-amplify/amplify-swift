e5ce1a1483a0eee436ec12ecb2f7ab07d3cc295e
Jithin Roy <51138777+royjit@users.noreply.github.com>
2020-04-22 07:26:07 -0700

    Auth - Mapping of AWSMobileClient error to Auth Error (#393)

--- Amplify/Categories/Auth/Error/AmplifyAuthError.swift
+++ Amplify/Categories/Auth/Error/AmplifyAuthError.swift
@@ -13,19 +13,47 @@ public enum AmplifyAuthError {
     case service(ErrorDescription, RecoverySuggestion, Error? = nil)
     case unknown(ErrorDescription, Error? = nil)
     case validation(Field, ErrorDescription, RecoverySuggestion, Error? = nil)
+    case notAuthorized(ErrorDescription, RecoverySuggestion, Error? = nil)
+    case invalidState(ErrorDescription, RecoverySuggestion, Error? = nil)
 }
 
 extension AmplifyAuthError: AmplifyError {
 
     public var underlyingError: Error? {
-        return nil
+        switch self {
+        case .configuration(_, _, let underlyingError),
+             .service(_, _, let underlyingError),
+             .unknown(_, let underlyingError),
+             .validation(_, _, _, let underlyingError),
+             .notAuthorized(_, _, let underlyingError),
+             .invalidState(_, _, let underlyingError):
+            return underlyingError
+        }
     }
 
     public var errorDescription: ErrorDescription {
-        return "Error"
+        switch self {
+        case .configuration(let errorDescription, _, _),
+             .service(let errorDescription, _, _),
+             .validation(_, let errorDescription, _, _),
+             .notAuthorized(let errorDescription, _, _),
+             .invalidState(let errorDescription, _, _):
+            return errorDescription
+        case .unknown(let errorDescription, _):
+            return "Unexpected error occurred with message: \(errorDescription)"
+        }
     }
 
     public var recoverySuggestion: RecoverySuggestion {
-        return "RecoverySuggestion"
+        switch self {
+        case .configuration(_, let recoverySuggestion, _),
+             .service(_, let recoverySuggestion, _),
+             .validation(_, _, let recoverySuggestion, _),
+             .notAuthorized(_, let recoverySuggestion, _),
+             .invalidState(_, let recoverySuggestion, _):
+            return recoverySuggestion
+        case .unknown:
+            return AmplifyErrorMessages.shouldNotHappenReportBugToAWS()
+        }
     }
 }
--- AmplifyPlugins/Auth/AWSAuthPlugin/Support/Constants/AuthPluginErrorConstants.swift
+++ AmplifyPlugins/Auth/AWSAuthPlugin/Support/Constants/AuthPluginErrorConstants.swift
@@ -10,6 +10,10 @@ import Amplify
 
 typealias AuthPluginErrorString = (errorDescription: ErrorDescription, recoverySuggestion: RecoverySuggestion)
 
+typealias AuthPluginValidationErrorString = (field: Field,
+    errorDescription: ErrorDescription,
+    recoverySuggestion: RecoverySuggestion)
+
 struct AuthPluginErrorConstants {
 
     static let decodeConfigurationError: AuthPluginErrorString = (
@@ -20,8 +24,70 @@ struct AuthPluginErrorConstants {
         "Configuration was not a dictionary literal",
         "Make sure the value for the plugin is a dictionary literal")
 
-
     static let mobileClientInitializeError: AuthPluginErrorString = (
         "Unable to initialize the underlying AWSMobileClient",
         "Make sure that the necessary configuration are present in the configuration file")
+}
+
+// Field validation errors
+extension AuthPluginErrorConstants {
+    static let signInUsernameError: AuthPluginValidationErrorString = (
+        "username",
+        "Username is required to signIn",
+        "Make sure that a valid username is passed during sigIn"
+    )
+}
+
+// Recovery Messages
+extension AuthPluginErrorConstants {
+    static let userNotFoundError: RecoverySuggestion = "Make sure that the user is present in the backend"
+
+    static let aliasExistsError: RecoverySuggestion = "Try with a different alias for the user"
+
+    static let codeDeliveryError: RecoverySuggestion = "Make sure that the delivery destination is valid."
+
+    static let codeMismatchError: RecoverySuggestion = "Retry with a valid code"
+
+    static let codeExpiredError: RecoverySuggestion = "Rerun the flow to send the code again"
+
+    static let lambdaError: RecoverySuggestion = "Make sure that the lambda configuration is correct"
+
+    static let invalidParameterError: RecoverySuggestion = "Make sure that the parameters passed are valid"
+
+    static let invalidPasswordError: RecoverySuggestion = "Make sure that the password is valid"
+
+    static let mfaMethodNotFoundError: RecoverySuggestion = "Make sure that the user pool has a valid MFA configured"
+
+    static let passwordResetRequired: RecoverySuggestion = "Reset the user password using the changePassword API"
+
+    static let resourceNotFoundError: RecoverySuggestion = "Make sure that the user pool has a requested resource"
+
+    static let softwareTokenNotFoundError: RecoverySuggestion = "Enable the software token MFA for the user"
+
+    static let tooManyFailedError: RecoverySuggestion = "User might have tried too many times with failed input"
+
+    static let tooManyRequestError: RecoverySuggestion = """
+    Make sure the requests send are controlled and the errors are properlly handled
+    """
+
+    static let configurationError: RecoverySuggestion = """
+    Make sure that the amplify configuration passed to Auth plugin is valid
+    """
+
+    static let userNotConfirmedError: RecoverySuggestion = "Confirm the user by calling confirmSignUp api"
+
+    static let userNameExistsError: RecoverySuggestion = "Invoke the api with a different username"
+
+    static let errorLoadingPageError: RecoverySuggestion = "Make sure that the UI is configured correctly"
+
+    static let deviceNotRememberedError: RecoverySuggestion = "Call remeberDevice to track the device"
+
+    static let invalidStateError: RecoverySuggestion = """
+    Operation performed is not a valid operation for the current auth state
+    """
+
+    static let notAuthorizedError: RecoverySuggestion = """
+    Check whether the given values are correct and the user is authorized to perform the operation.
+    """
+
 }
