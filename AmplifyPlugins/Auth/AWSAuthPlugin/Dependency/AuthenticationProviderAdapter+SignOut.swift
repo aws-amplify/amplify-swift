b084db429e8e70356dd0ecb17202bfc9cb3dee70
Jithin Roy <royji@amazon.com>
2020-05-06 19:00:41 -0700

    fix(Auth): Crash in WebUI signout
    If the user is signed in through the webUI calling SignOut require the api to be called in main thread.
    This change make sure that signOut happens in main thread.

--- AmplifyPlugins/Auth/AWSAuthPlugin/Dependency/AuthenticationProviderAdapter+SignOut.swift
+++ AmplifyPlugins/Auth/AWSAuthPlugin/Dependency/AuthenticationProviderAdapter+SignOut.swift
@@ -10,9 +10,23 @@ import AWSMobileClient
 
 extension AuthenticationProviderAdapter {
 
-    func signOut(request: AuthSignOutRequest,
-                 completionHandler: @escaping (Result<Void, AmplifyAuthError>) -> Void) {
+    func signOut(request: AuthSignOutRequest, completionHandler: @escaping (Result<Void, AmplifyAuthError>) -> Void) {
 
+        // If user is signed in through HostedUI the signout require UI to complete. So calling this in main thread.
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {
+                return
+            }
+            self.signOutWithUI(completionHandler)
+        }
+    }
+
+    private func signOutWithUI(_ completionHandler: @escaping (Result<Void, AmplifyAuthError>) -> Void) {
+
+        // Stop the execution here if we are not running on the main thread.
+        // There is no point on returning an error back to the developer, because
+        // they do not control how the UI is presented.
+        dispatchPrecondition(condition: .onQueue(DispatchQueue.main))
         let signOutOptions = SignOutOptions(signOutGlobally: true, invalidateTokens: true)
         awsMobileClient.signOut(options: signOutOptions) { error in
             guard error == nil else {
