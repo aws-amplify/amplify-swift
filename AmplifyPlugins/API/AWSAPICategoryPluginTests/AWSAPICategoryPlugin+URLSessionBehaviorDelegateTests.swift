882de5fd46aec5b3112209a620d974e7b504c725
Tim Schmelter <schmelte@amazon.com>
2020-05-15 12:23:19 -0700

    refactor(api): Convert plugin to use Result instead of AsyncOperation (#450)
    - chore: Fix SwiftFormat and SwiftLint invocations to use common configs
    - chore: Fix linting & formatting issues

--- AmplifyPlugins/API/AWSAPICategoryPluginTests/AWSAPICategoryPlugin+URLSessionBehaviorDelegateTests.swift
+++ AmplifyPlugins/API/AWSAPICategoryPluginTests/AWSAPICategoryPlugin+URLSessionBehaviorDelegateTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 
+// swiftlint:disable:next type_name
 class AWSAPICategoryPluginURLSessionBehaviorDelegateTests: AWSAPICategoryPluginTestBase {
     func testClassMustNotBeEmptyOrSwiftFormatWillCrash() {
         //TODO implement code
