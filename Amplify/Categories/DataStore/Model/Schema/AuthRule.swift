57ac3eea0c1d2e6cb66cea0549c9d77a634536d3
Michael law <1365977+lawmicha@users.noreply.github.com>
2020-05-18 16:32:14 -0700

    feat: Remove AuthProvider from AuthRule

--- Amplify/Categories/DataStore/Model/Schema/AuthRule.swift
+++ Amplify/Categories/DataStore/Model/Schema/AuthRule.swift
@@ -12,13 +12,6 @@ public enum AuthStrategy {
     case `public`
 }
 
-public enum AuthProvider {
-    case apiKey
-    case iam
-    case oidc
-    case userPools
-}
-
 public enum ModelOperation {
     case create
     case update
@@ -31,31 +24,25 @@ public typealias AuthRules = [AuthRule]
 public struct AuthRule {
     public let allow: AuthStrategy
     public let field: CodingKey?
-    public let provider: AuthProvider?
     public let ownerField: CodingKey?
     public let identityClaim: String?
     public let groupClaim: String?
     public let groups: [String]
-    public let groupsField: CodingKey?
     public let operations: [ModelOperation]
 
     public init(allow: AuthStrategy,
                 field: CodingKey? = nil,
-                provider: AuthProvider? = nil,
                 ownerField: CodingKey? = nil,
                 identityClaim: String? = nil,
                 groupClaim: String? = nil,
                 groups: [String] = [],
-                groupsField: CodingKey? = nil,
                 operations: [ModelOperation] = []) {
         self.allow = allow
         self.field = field
-        self.provider = provider
         self.ownerField = ownerField
         self.identityClaim = identityClaim
         self.groupClaim = groupClaim
         self.groups = groups
-        self.groupsField = groupsField
         self.operations = operations
     }
 }
--- Amplify/Categories/DataStore/Model/Schema/Model+Schema.swift
+++ Amplify/Categories/DataStore/Model/Schema/Model+Schema.swift
@@ -47,21 +47,17 @@ extension Model {
 
     public static func rule(allow: AuthStrategy,
                             field: CodingKey? = nil,
-                            provider: AuthProvider? = nil,
                             ownerField: CodingKey? = nil,
                             identityClaim: String? = nil,
                             groupClaim: String? = nil,
                             groups: [String] = [],
-                            groupsField: CodingKey? = nil,
                             operations: [ModelOperation] = []) -> AuthRule {
         return AuthRule(allow: allow,
                         field: field,
-                        provider: provider,
                         ownerField: ownerField,
                         identityClaim: identityClaim,
                         groupClaim: groupClaim,
                         groups: groups,
-                        groupsField: groupsField,
                         operations: operations)
     }
 }
