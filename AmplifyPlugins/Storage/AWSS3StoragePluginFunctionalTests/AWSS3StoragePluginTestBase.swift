36191de6178fefa666f8c2c0fed809ace15057e9
Michael Law <1365977+lawmicha@users.noreply.github.com>
2020-05-19 10:09:22 -0400

    feat: [Storage] use Amplify.Auth in storage integration tests (#446)
    * removed reference to AWSMobileClient

--- AmplifyPlugins/Storage/AWSS3StoragePluginFunctionalTests/AWSS3StoragePluginTestBase.swift
+++ AmplifyPlugins/Storage/AWSS3StoragePluginFunctionalTests/AWSS3StoragePluginTestBase.swift
@@ -6,9 +6,10 @@
 //
 
 import XCTest
-import AWSMobileClient
+
 @testable import Amplify
 import AWSS3StoragePlugin
+import AmplifyPlugins
 import AWSS3
 import AWSCognitoIdentityProvider
 @testable import AmplifyTestCommon
@@ -16,7 +17,6 @@ import AWSCognitoIdentityProvider
 class AWSS3StoragePluginTestBase: XCTestCase {
 
     static let amplifyConfiguration = "AWSS3StoragePluginTests-amplifyconfiguration"
-    static let awsconfiguration = "AWSS3StoragePluginTests-awsconfiguration"
     static let credentials = "AWSS3StoragePluginTests-credentials"
 
     static let largeDataObject = Data(repeating: 0xff, count: 1_024 * 1_024 * 6) // 6MB
@@ -36,15 +36,9 @@ class AWSS3StoragePluginTestBase: XCTestCase {
                 XCTFail("Missing credentials.json data")
                 return
             }
-
             AWSS3StoragePluginTestBase.user1 = user1
             AWSS3StoragePluginTestBase.user2 = user2
             AWSS3StoragePluginTestBase.password = password
-
-            let awsConfiguration = try TestConfigHelper.retrieveAWSConfiguration(
-                forResource: AWSS3StoragePluginTestBase.awsconfiguration)
-            AWSInfo.configureDefaultAWSInfo(awsConfiguration)
-
         } catch {
             XCTFail("Failed to initialize test set up \(error)")
         }
@@ -52,9 +46,8 @@ class AWSS3StoragePluginTestBase: XCTestCase {
 
     override func setUp() {
         do {
-            AuthHelper.initializeMobileClient()
             Amplify.reset()
-
+            try Amplify.add(plugin: AWSAuthPlugin())
             try Amplify.add(plugin: AWSS3StoragePlugin())
             let amplifyConfig = try TestConfigHelper.retrieveAmplifyConfiguration(
                 forResource: AWSS3StoragePluginTestBase.amplifyConfiguration)
