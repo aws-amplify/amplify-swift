11cc6a84b43e9b19af841f84219b27d9f6c02999
Jithin Roy <royji@amazon.com>
2020-05-21 21:17:21 -0700

    test: Auth integration test for signUp, signIn

--- AmplifyPlugins/Auth/AWSCognitoAuthPlugin/Operations/AWSAuthSignInOperation.swift
+++ AmplifyPlugins/Auth/AWSCognitoAuthPlugin/Operations/AWSAuthSignInOperation.swift
@@ -45,6 +45,11 @@ public class AWSAuthSignInOperation: AmplifyOperation<
             defer {
                 self.finish()
             }
+
+            if self.isCancelled {
+                return
+            }
+
             switch result {
             case .failure(let error):
                 self.dispatch(error)
