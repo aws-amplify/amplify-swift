c2342ba19b598950dc105d2e385b7eec84058975
Michael law <1365977+lawmicha@users.noreply.github.com>
2020-05-26 11:27:12 -0700

    fix unit test

--- AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginTests/Sync/InitialSync/InitialSyncOperationTests.swift
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginTests/Sync/InitialSync/InitialSyncOperationTests.swift
@@ -291,7 +291,7 @@ class InitialSyncOperationTests: XCTestCase {
                 case let .operationError(_, _, underlyingError) = actualAPIError,
                 let authError = underlyingError as? AuthError,
                 case .signedOut = authError else {
-                    XCTFail("Should be `signedOut` error but got \(actualAPIError)")
+                    XCTFail("Should be `signedOut` error but got \(amplifyError)")
                     return
             }
             expectErrorHandlerCalled.fulfill()
