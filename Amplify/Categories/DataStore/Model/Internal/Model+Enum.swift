db2fc61205c61dcf31cf4d493e5bc75ba13c9ad5
Michael law <1365977+lawmicha@users.noreply.github.com>
2020-06-18 14:14:23 -0700

    comment update

--- Amplify/Categories/DataStore/Model/Internal/Model+Enum.swift
+++ Amplify/Categories/DataStore/Model/Internal/Model+Enum.swift
@@ -10,8 +10,7 @@ import Foundation
 /// Protocol that represents a `Codable` Enum that can be persisted and easily
 /// integrate with remote APIs since it must have a raw `String` value.
 ///
-/// That means only simple enums (i.e. the ones that don't have arguments) can be used
-/// as model properties.
+/// That means only enums without associated values can be used as model properties.
 ///
 /// - Example:
 ///
