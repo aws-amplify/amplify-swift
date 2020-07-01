5c3f2addb00be5eaa5f1227c0e170d38d64863e1
Abhash Kumar Singh <thisisabhash@gmail.com>
2020-07-01 12:43:04 -0500

    feat(amplify-dev-menu): Initial code setup for Amplify Developer Menu (#594)

---
+++ Amplify/DevMenu/View/AmplifyDevMenuRow.swift
@@ -0,0 +1,28 @@
+//
+// Copyright 2018-2020 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+struct AmplifyDevMenuRow: View {
+    var rowItem: AmplifyDevMenuItem
+
+    var body: some View {
+        HStack {
+            Spacer()
+            Text(rowItem.title)
+            Spacer()
+        }
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct AmplifyDevMenuRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AmplifyDevMenuRow(rowItem: AmplifyDevMenuItem(title: "View Environment Information"))
+    }
+}
