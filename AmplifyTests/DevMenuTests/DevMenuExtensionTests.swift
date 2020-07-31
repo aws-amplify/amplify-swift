78a5ecbed19a6db2455b212dbf7c78103d716334
Abhash Kumar Singh <thisisabhash@gmail.com>
2020-07-31 12:42:06 -0500

    test(dev-menu): Unit tests for Amplify+DevMenu extension

---
+++ AmplifyTests/DevMenuTests/DevMenuExtensionTests.swift
@@ -0,0 +1,41 @@
+//
+// Copyright 2018-2020 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import XCTest
+@testable import Amplify
+@testable import AmplifyTestCommon
+
+@available(iOS 13.0.0, *)
+class DevMenuExtensionTests: XCTestCase {
+
+    var contextProvider: DevMenuPresentationContextProvider!
+    var plugin: LoggingCategoryPlugin!
+
+    override func setUp() {
+        contextProvider = MockDevMenuContextProvider()
+        plugin = AWSUnifiedLoggingPlugin()
+        Amplify.enableDevMenu(contextProvider: contextProvider)
+    }
+
+    func testAmplifyInit() {
+        #if DEBUG
+            XCTAssertTrue(Amplify.isDevMenuEnabled())
+        #else
+            XCTAssertFalse(Amplify.isDevMenuEnabled())
+        #endif
+    }
+
+    func testLoggingCategoryPlugin() {
+        #if DEBUG
+            let devMenuPlugin = Amplify.getLoggingCategoryPlugin(loggingPlugin: plugin)
+            XCTAssertTrue(devMenuPlugin is PersistentLoggingPlugin)
+        #else
+            let devMenuPlugin = Amplify.getLoggingCategoryPlugin(loggingPlugin: plugin)
+            XCTAssertFalse(devMenuPlugin is PersistentLoggingPlugin)
+        #endif
+    }
+}
