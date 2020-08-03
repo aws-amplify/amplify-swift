011cee654cbd100a3eeb4e264c3127d0cae53156
Tim Schmelter <schmelte@amazon.com>
2020-08-03 10:18:10 -0700

    chore: Pin to SwiftFormat 0.44.17 pending context-sensitive brace rules in SwiftLint (#685)

--- build-support/dependencies.rb
+++ build-support/dependencies.rb
@@ -13,7 +13,8 @@ $OPTIMISTIC_AWS_SDK_VERSION = "~> #{$AWS_SDK_VERSION}"
 
 # Include common tooling
 def include_build_tools!
-  pod 'SwiftFormat/CLI'
+  # Pin to 0.44.17 until we resolve closing braces
+  pod 'SwiftFormat/CLI', '0.44.17'
   pod 'SwiftLint'
 end
 
