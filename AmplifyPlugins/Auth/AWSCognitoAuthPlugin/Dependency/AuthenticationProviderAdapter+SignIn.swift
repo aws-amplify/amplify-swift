976a5c79ecac2692700ca33ed21a902fd12f6f05
Roy <51138777+royjit@users.noreply.github.com>
2020-10-12 15:20:53 -0700

    fix(auth): Dismiss UI first before sending callback for HostedUI

--- AmplifyPlugins/Auth/AWSCognitoAuthPlugin/Dependency/AuthenticationProviderAdapter+SignIn.swift
+++ AmplifyPlugins/Auth/AWSCognitoAuthPlugin/Dependency/AuthenticationProviderAdapter+SignIn.swift
@@ -145,35 +145,33 @@ extension AuthenticationProviderAdapter {
         }
 
         parentViewController?.present(navController, animated: false, completion: {
-
+            
             self.awsMobileClient.showSignIn(navigationController: navController,
                                             signInUIOptions: SignInUIOptions(),
                                             hostedUIOptions: hostedUIOptions) { [weak self] state, error in
-                                                defer {
-                                                    DispatchQueue.main.async {
-                                                        navController.dismiss(animated: false)
-                                                    }
-                                                }
-                                                guard let self = self else { return }
-
-                                                if let error = error {
-                                                    let authError = self.convertSignUIErrorToAuthError(error)
-                                                    completionHandler(.failure(authError))
-                                                    return
-                                                }
-
-                                                guard let state = state, state == .signedIn else {
-
-                                                    let error = AuthError.unknown("""
-                                                    signInWithWebUI did not produce a valid result.
-                                                    """)
-                                                    completionHandler(.failure(error))
-                                                    return
-                                                }
-                                                let authResult = AuthSignInResult(nextStep: .done)
-                                                completionHandler(.success(authResult))
+                
+                DispatchQueue.main.async {
+                    navController.dismiss(animated: false) {
+                        guard let self = self else { return }
+                        
+                        if let error = error {
+                            let authError = self.convertSignUIErrorToAuthError(error)
+                            completionHandler(.failure(authError))
+                            return
+                        }
+                        
+                        guard let state = state, state == .signedIn else {
+                            
+                            let error = AuthError.unknown("signInWithWebUI did not produce a valid result.")
+                            completionHandler(.failure(error))
+                            return
+                        }
+                        let authResult = AuthSignInResult(nextStep: .done)
+                        completionHandler(.success(authResult))
+                    }
+                }
             }
-
+            
         })
     }
 
