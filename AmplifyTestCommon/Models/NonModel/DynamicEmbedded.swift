30765f88cf723bb9198f4a03ae22b1831082c7c5
Jithin Roy <51138777+royjit@users.noreply.github.com>
2020-10-15 10:37:47 -0700

    refactor(datastore): Fix selectionset for embedded type (#822)
    - Added an extra optional parameter ModelSchema for .embedded and .embeddedCollection enums. This is required for non modeltype to create the current selection set.
    - Added test around non modeltype using the above enums.

---
+++ AmplifyTestCommon/Models/NonModel/DynamicEmbedded.swift
@@ -0,0 +1,63 @@
+//
+// Copyright 2018-2020 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Amplify
+
+struct DynamicEmbedded: Embeddable, JSONValueHolder {
+
+    public let values: [String: JSONValue]
+
+    public init(map: [String: JSONValue]) {
+        self.values = map
+    }
+
+    public init(from decoder: Decoder) throws {
+        let json = try JSONValue(from: decoder)
+        if case .object(let jsonValue) = json {
+            values = jsonValue
+        } else {
+            self.values = [:]
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var unkeyedContainer = encoder.unkeyedContainer()
+        try unkeyedContainer.encode(values)
+    }
+
+    public func jsonValue(for key: String) -> Any?? {
+        switch values[key] {
+        case .some(.array(let deserializedValue)):
+            return deserializedValue
+        case .some(.boolean(let deserializedValue)):
+            return deserializedValue
+        case .some(.number(let deserializedValue)):
+            return deserializedValue
+        case .some(.object(let deserializedValue)):
+            return deserializedValue
+        case .some(.string(let deserializedValue)):
+            return deserializedValue
+        case .some(.null):
+            return nil
+        case .none:
+            return nil
+        }
+    }
+}
+
+extension DynamicEmbedded {
+
+    public enum CodingKeys: String, ModelKey {
+        case values
+    }
+
+    public static let keys = CodingKeys.self
+
+    public static let schema = defineSchema { _ in
+        fatalError("Schema for dynamic model should not be called using static method")
+    }
+}
