4febacb3a43d5cdbb68a7bb0f7bda59ce1ded41e
Tim Schmelter <schmelte@amazon.com>
2020-11-13 14:23:37 -0800

    fix: Designate SwiftUI a weak_framework in podspec

--- Amplify/DevMenu/DevMenuPresentationContextProvider.swift
+++ Amplify/DevMenu/DevMenuPresentationContextProvider.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 /// A protocol which provides a UI context over which views can be presented
+@available(iOS 13.0, *)
 public protocol DevMenuPresentationContextProvider: AnyObject {
     func devMenuPresentationContext() -> UIWindow
 }
