11c19bf03529e8646cf766e2d565e990699a9837
Guo <48600426+DongQuanRui@users.noreply.github.com>
2020-12-20 12:33:25 -0800

    rebase

--- AmplifyPlugins/Auth/AWSCognitoAuthPluginTests/AuthenticationProviderTests/AuthenticationProviderConfirmSigninTests.swift
+++ AmplifyPlugins/Auth/AWSCognitoAuthPluginTests/AuthenticationProviderTests/AuthenticationProviderConfirmSigninTests.swift
@@ -21,7 +21,32 @@ class AuthenticationProviderConfirmSigninTests: BaseAuthenticationProviderTest {
 
         let resultExpectation = expectation(description: "Should receive a result")
         let options = AuthConfirmSignInRequest.Options()
-        _ = plugin.confirmSignIn(challengeResponse: "reponse", options: options) { result in
+        _ = plugin.confirmSignIn(challengeResponse: "code", options: options) { result in
+            defer {
+                resultExpectation.fulfill()
+            }
+            switch result {
+            case .success(let confirmSignInResult):
+                guard case .done = confirmSignInResult.nextStep else {
+                    XCTFail("Result should be .done for next step")
+                    return
+                }
+                XCTAssertTrue(confirmSignInResult.isSignedIn, "Signin result should be complete")
+            case .failure(let error):
+                XCTFail("Received failure with error \(error)")
+            }
+        }
+        wait(for: [resultExpectation], timeout: 2)
+    }
+
+    func testSuccessfullyConfirmSignIn() {
+
+        let mockSigninResult = SignInResult(signInState: .signedIn)
+        mockAWSMobileClient?.confirmSignInMockResult = .success(mockSigninResult)
+
+        let resultExpectation = expectation(description: "Should receive a result")
+        let options = AuthConfirmSignInRequest.Options()
+        _ = plugin.confirmSignIn(challengeResponse: "", options: options) { result in
             defer {
                 resultExpectation.fulfill()
             }
