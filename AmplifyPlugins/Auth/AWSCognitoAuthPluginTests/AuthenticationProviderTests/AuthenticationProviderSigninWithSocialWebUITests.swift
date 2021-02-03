1a3f75a9957afdf813db04c023bf67b362792152
Jithin Roy <51138777+royjit@users.noreply.github.com>
2021-02-03 07:50:46 -0800

    Update AmplifyPlugins/Auth/AWSCognitoAuthPluginTests/AuthenticationProviderTests/AuthenticationProviderSigninWithSocialWebUITests.swift
    Co-authored-by: Tim Schmelter <schmelte@amazon.com>

--- AmplifyPlugins/Auth/AWSCognitoAuthPluginTests/AuthenticationProviderTests/AuthenticationProviderSigninWithSocialWebUITests.swift
+++ AmplifyPlugins/Auth/AWSCognitoAuthPluginTests/AuthenticationProviderTests/AuthenticationProviderSigninWithSocialWebUITests.swift
@@ -153,7 +153,7 @@ class AuthenticationProviderSigninWithSocialWebUITests: BaseAuthenticationProvid
             case .failure(let error):
                 guard case .service(_, _, let underlyingError) = error,
                       case .userCancelled = (underlyingError as? AWSCognitoAuthError) else {
-                    XCTFail("Should produce SFAuthenticationError error but instead produced \(error)")
+                    XCTFail("Should produce AWSCognitoAuthError error but instead produced \(error)")
                     return
                 }
             }
