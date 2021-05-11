644bfb7e1e347ccdd04450b7f2d2ff054784bf6c
Diego Costantino <diego@diegocostantino.com>
2021-05-10 17:00:26 -0700

    feat(datastore): [WIP] support for @key directive

---
+++ AmplifyTestCommon/Models/CustomerOrder.swift
@@ -0,0 +1,24 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+// swiftlint:disable all
+import Amplify
+import Foundation
+
+public struct CustomerOrder: Model {
+  public let id: String
+  public var orderId: String
+  public var email: String
+  
+  public init(id: String = UUID().uuidString,
+      orderId: String,
+      email: String) {
+      self.id = id
+      self.orderId = orderId
+      self.email = email
+  }
+}
