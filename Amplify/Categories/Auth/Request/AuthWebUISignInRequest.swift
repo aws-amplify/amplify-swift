b97c95ee22592014ff81d84a0ce30d98d971d36e
Jithin Roy <51138777+royjit@users.noreply.github.com>
2021-06-16 13:52:07 -0700

    Update Amplify/Categories/Auth/Request/AuthWebUISignInRequest.swift
    Co-authored-by: Tim Schmelter <schmelte@amazon.com>

--- Amplify/Categories/Auth/Request/AuthWebUISignInRequest.swift
+++ Amplify/Categories/Auth/Request/AuthWebUISignInRequest.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-/// Request to initiate a webUI sign in
+/// Request to initiate sign in using a web UI. Note that this call would also be used for sign up, forgot password, confirm password, and similar flows.
 public struct AuthWebUISignInRequest: AmplifyOperationRequest {
 
     /// Presentation anchor on which the webUI is displayed
