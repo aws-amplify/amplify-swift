263066c099324eb8a1e7d03bae9118ef1eab8360
Jithin Roy <51138777+royjit@users.noreply.github.com>
2021-06-16 13:53:49 -0700

    Update Amplify/Categories/Auth/Request/AuthResendSignUpCodeRequest.swift
    Co-authored-by: Tim Schmelter <schmelte@amazon.com>

--- Amplify/Categories/Auth/Request/AuthResendSignUpCodeRequest.swift
+++ Amplify/Categories/Auth/Request/AuthResendSignUpCodeRequest.swift
@@ -10,7 +10,7 @@ import Foundation
 /// Request to resend sign up code
 public struct AuthResendSignUpCodeRequest: AmplifyOperationRequest {
 
-    /// User for which the sign up code to be resend
+    /// User for which the sign up code should be resent
     public let username: String
 
     /// Extra request options defined in `AuthResendSignUpCodeRequest.Options`
