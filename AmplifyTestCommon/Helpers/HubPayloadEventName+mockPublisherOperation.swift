e9eaef2c188f982cc4c6b3a2d44caa8501d4dcdd
Diego Costantino <diego@diegocostantino.com>
2021-07-07 10:24:59 -0700

    chore: shared helpers symbols

---
+++ AmplifyTestCommon/Helpers/HubPayloadEventName+mockPublisherOperation.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Costantino, Diego on 2021-07-07.
+//
+
+import Foundation
+import Amplify
+
+extension HubPayloadEventName {
+    static var mockPublisherOperation = "MockPublisherOperation"
+}
---
+++ AmplifyTestCommon/Mocks/MockPublisherRequest.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Costantino, Diego on 2021-07-07.
+//
+
+import Foundation
+import Amplify
+
+struct MockPublisherRequest: AmplifyOperationRequest {
+    struct Options { }
+    let options = Options()
+}
