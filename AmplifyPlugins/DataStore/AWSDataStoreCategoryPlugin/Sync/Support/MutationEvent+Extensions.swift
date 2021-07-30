b4707260b8cef0acfe1c12863d255dabb0ea19fd
Abhash Kumar Singh <thisisabhash@gmail.com>
2021-07-30 14:48:41 -0700

    Move processing logic to OutgoingMutationQueue and add new files for Mutation Event extension

---
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPlugin/Sync/Support/MutationEvent+Extensions.swift
@@ -0,0 +1,48 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Amplify
+import Dispatch
+import AWSPluginsCore
+
+extension MutationEvent {
+    
+    // updates the head of pending mutation event queue with with syncMetadata version in `mutationSync`
+    // and saves it in the mutation event table (returned as API response)
+    static func updatePendingMutationEventVersionIfNil(for modelId: Model.Identifier,
+                                                  mutationSync: MutationSync<AnyModel>,
+                                                  storageAdapter: StorageEngineAdapter,
+                                                  completion: @escaping DataStoreCallback<Void>) {
+        MutationEvent.pendingMutationEvents(
+            for: modelId,
+            storageAdapter: storageAdapter) { queryResult in
+            switch queryResult {
+            case .failure(let dataStoreError):
+                completion(.failure(dataStoreError))
+            case .success(let localMutationEvents):
+                guard var existingEvent = localMutationEvents.first else {
+                    Amplify.log.verbose(
+                        "\(#function) Mutation event table doesn't have mutation events with model id : \(modelId)")
+                    break
+                }
+
+                if existingEvent.version == nil {
+                    existingEvent.version = mutationSync.syncMetadata.version
+                    storageAdapter.save(existingEvent, condition: nil) { result in
+                        switch result {
+                        case .failure(let dataStoreError):
+                            completion(.failure(dataStoreError))
+                        case .success:
+                            completion(.success(()))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginTests/Sync/Support/MutationEventExtensionsTests.swift
@@ -0,0 +1,9 @@
+//
+//  MutationEventExtensionsTests.swift
+//  AWSDataStoreCategoryPluginTests
+//
+//  Created by Singh, Abhash Kumar on 7/30/21.
+//  Copyright © 2021 Amazon Web Services. All rights reserved.
+//
+
+import Foundation
