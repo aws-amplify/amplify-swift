e957491a5754d2b4e9072982ef2c6e9cf07845c3
Dillon Nys <nydillon@amazon.com>
2021-10-10 16:45:14 -0700

    Clean up

--- AmplifyPlugins/DataStore/AWSDataStoreCategoryPlugin/Storage/StorageEngine+SyncRequirement.swift
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPlugin/Storage/StorageEngine+SyncRequirement.swift
@@ -19,7 +19,6 @@ extension StorageEngine {
                                                "Ensure the API category has been setup and configured for your project", nil)))
             return
         }
-        
         let authPluginRequired = requiresAuthPlugin(api)
 
         guard authPluginRequired else {
