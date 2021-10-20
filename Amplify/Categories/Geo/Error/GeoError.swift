bf1e7d5252e861a54ff067168389e85fd088a06f
Chris Ameter <29186822+ameter@users.noreply.github.com>
2021-10-19 21:09:20 -0600

    working on Geo.Error

--- Amplify/Categories/Geo/Error/GeoError.swift
+++ Amplify/Categories/Geo/Error/GeoError.swift
@@ -7,121 +7,82 @@
 
 import Foundation
 
-///// Geo Error
-//public enum GeoError {
-//    /// Configuration Error
-//    case configuration(ErrorDescription, RecoverySuggestion, Error? = nil)
-//    /// Unknown Error
-//    case unknown(ErrorDescription, RecoverySuggestion, Error? = nil)
-//}
-//
-//extension GeoError: AmplifyError {
-//    /// Initializer
-//    /// - Parameters:
-//    ///   - errorDescription: Error Description
-//    ///   - recoverySuggestion: Recovery Suggestion
-//    ///   - error: Underlying Error
-//    public init(
-//        errorDescription: ErrorDescription = "An unknown error occurred",
-//        recoverySuggestion: RecoverySuggestion = "See `underlyingError` for more details",
-//        error: Error) {
-//        if let error = error as? Self {
-//            self = error
-//        } else if error.isOperationCancelledError {
-//            self = .unknown("Operation cancelled", "", error)
-//        } else {
-//            self = .unknown(errorDescription, recoverySuggestion, error)
-//        }
-//    }
-//
-//    /// Error Description
-//    public var errorDescription: ErrorDescription {
-//        switch self {
-//        case .configuration(let errorDescription, _, _):
-//            return errorDescription
-//        case .unknown(let errorDescription, _, _):
-//            return "Unexpected error occurred with message: \(errorDescription)"
-//        }
-//    }
-//
-//    /// Recovery Suggestion
-//    public var recoverySuggestion: RecoverySuggestion {
-//        switch self {
-//        case .configuration(_, let recoverySuggestion, _):
-//            return recoverySuggestion
-//        case .unknown:
-//            return AmplifyErrorMessages.shouldNotHappenReportBugToAWS()
-//        }
-//    }
-//
-//    /// Underlying Error
-//    public var underlyingError: Error? {
-//        switch self {
-//        case .configuration(_, _, let error):
-//            return error
-//        case .unknown(_, _, let error):
-//            return error
-//        }
-//    }
-//}
-
 public extension Geo {
-    struct Error: AmplifyError {
-        public let errorDescription: ErrorDescription
-        public let recoverySuggestion: RecoverySuggestion
-        public let underlyingError: Swift.Error?
-        
-        public init(errorDescription: ErrorDescription, recoverySuggestion: RecoverySuggestion, error: Swift.Error) {
-            if let error = error as? Self {
-                self = error
-            } else if error.isOperationCancelledError {
-                self = .unknown("Operation cancelled", error)
-            } else {
-                self = .unknown(errorDescription, error)
-            }
+    /// Geo Error
+    enum Error {
+        /// Invalid Configuration.
+        case invalidConfiguration(ErrorDescription, RecoverySuggestion, Swift.Error? = nil)
+        /// Network Error - Request failed or network unavailable.
+        case networkError(ErrorDescription, RecoverySuggestion, Swift.Error? = nil)
+        // Access Denied - request authorization issue
+        case accessDenied(ErrorDescription, RecoverySuggestion, Swift.Error? = nil)
+        /// Service Error - Service may be down [500, 503]
+        case serviceError(ErrorDescription, RecoverySuggestion, Swift.Error? = nil)
+        /// Encapsulated error received by a dependent plugin
+        case pluginError(AmplifyError)
+        /// Unknown Error
+        case unknown(ErrorDescription, RecoverySuggestion, Swift.Error? = nil)
+    }
+}
+
+extension Geo.Error: AmplifyError {
+    /// Initializer
+    /// - Parameters:
+    ///   - errorDescription: Error Description
+    ///   - recoverySuggestion: Recovery Suggestion
+    ///   - error: Underlying Error
+    public init(
+        errorDescription: ErrorDescription = "An unknown error occurred",
+        recoverySuggestion: RecoverySuggestion = "See `underlyingError` for more details",
+        error: Error) {
+        if let error = error as? Self {
+            self = error
+        } else if error.isOperationCancelledError {
+            self = .unknown("Operation cancelled", "", error)
+        } else {
+            self = .unknown(errorDescription, recoverySuggestion, error)
         }
     }
-}
 
-extension Geo.Error: Equatable {
-    public static func == (lhs: Geo.Error, rhs: Geo.Error) -> Bool {
-        lhs.errorDescription == rhs.errorDescription &&
-        lhs.recoverySuggestion == rhs.recoverySuggestion
+    /// Error Description
+    public var errorDescription: ErrorDescription {
+        switch self {
+        case .invalidConfiguration(let errorDescription, _, _),
+             .networkError(let errorDescription, _, _),
+             .accessDenied(let errorDescription, _, _),
+             .serviceError(let errorDescription, _, _),
+             .unknown(let errorDescription, _, _):
+            return errorDescription
+        case .pluginError(let error):
+            return error.errorDescription
+        }
     }
-}
 
-public extension Geo.Error {
-    init(errorDescription: ErrorDescription, recoverySuggestion: RecoverySuggestion, error: Swift.Error? = nil) {
-        self.errorDescription = errorDescription
-        self.recoverySuggestion = recoverySuggestion
-        self.underlyingError = error
+    /// Recovery Suggestion
+    public var recoverySuggestion: RecoverySuggestion {
+        switch self {
+        case .invalidConfiguration(_, let recoverySuggestion, _),
+             .networkError(_, let recoverySuggestion, _),
+             .accessDenied(_, let recoverySuggestion, _),
+             .serviceError(_, let recoverySuggestion, _),
+             .unknown(_, let recoverySuggestion, _):
+            return recoverySuggestion
+        case .pluginError(let error):
+            return error.recoverySuggestion
+        }
     }
-    
-    static let noAvailableMaps = Geo.Error(
-        errorDescription: "There are no available map styles",
-        recoverySuggestion: "There are no available map styles"
-    )
-    
-    static let accessDeniedException = Geo.Error(
-        errorDescription: "AccessDeniedException [400]",
-        recoverySuggestion: "You do not have sufficient access to perform this action."
-    )
-    
-    static let incompleteSignature = Geo.Error(
-        errorDescription: "IncompleteSignature [400]",
-        recoverySuggestion: "The request signature does not conform to AWS standards."
-    )
-    
-    static let internalFailure = Geo.Error(
-        errorDescription: "InternalFailure [500]",
-        recoverySuggestion: "The request processing has failed because of an unknown error, exception or failure."
-    )
-    
-    static func unknown(_ description: String, _ error: Swift.Error) -> Geo.Error {
-        .init(
-            errorDescription: description,
-            recoverySuggestion: "",
-            error: error
-        )
+
+    /// Underlying Error
+    public var underlyingError: Error? {
+        switch self {
+        case .invalidConfiguration(_, _, let underlyingError),
+             .networkError(_, _, let underlyingError),
+             .accessDenied(_, _, let underlyingError),
+             .serviceError(_, _, let underlyingError),
+             .unknown(_, _, let underlyingError):
+            return underlyingError
+        case .pluginError(let error):
+            return error.underlyingError
+        }
     }
 }
