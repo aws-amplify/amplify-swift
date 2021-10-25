83fb9d223472b904286f8a4f45023259eb1716e1
Michael <1365977+lawmicha@users.noreply.github.com>
2021-10-25 11:55:41 -0400

    add DataStore Integ tests

--- AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginIntegrationTests/TestSupport/SyncEngineIntegrationTestBase.swift
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginIntegrationTests/TestSupport/SyncEngineIntegrationTestBase.swift
@@ -16,6 +16,8 @@ import AWSMobileClient
 
 class SyncEngineIntegrationTestBase: DataStoreTestBase {
 
+    static let amplifyConfigurationFile = "testconfiguration/AWSDataStoreCategoryPluginIntegrationTests-amplifyconfiguration"
+
     static let networkTimeout = TimeInterval(180)
     let networkTimeout = SyncEngineIntegrationTestBase.networkTimeout
 
@@ -50,14 +52,7 @@ class SyncEngineIntegrationTestBase: DataStoreTestBase {
     }
 
     func startAmplify(_ completion: BasicClosure? = nil) throws {
-        let bundle = Bundle(for: type(of: self))
-        guard let configFile = bundle.url(forResource: "amplifyconfiguration", withExtension: "json") else {
-            XCTFail("Could not get URL for amplifyconfiguration.json from \(bundle)")
-            return
-        }
-
-        let configData = try Data(contentsOf: configFile)
-        let amplifyConfig = try JSONDecoder().decode(AmplifyConfiguration.self, from: configData)
+        let amplifyConfig = try TestConfigHelper.retrieveAmplifyConfiguration(forResource: Self.amplifyConfigurationFile)
 
         DispatchQueue.global().async {
             do {
