baabf68a963f99e729bd0b015a8ebd06337da294
Diego Costantino <diego@diegocostantino.com>
2021-11-05 16:41:35 -0700

    chore(datastore): @auth explicit owner test

--- AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginAuthIntegrationTests/DefaultAuth/AWSDataStoreCategoryPluginAuthIntegrationTests+Support.swift
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginAuthIntegrationTests/DefaultAuth/AWSDataStoreCategoryPluginAuthIntegrationTests+Support.swift
@@ -12,39 +12,39 @@ import Amplify
 @testable import AmplifyTestCommon
 
 extension AWSDataStoreCategoryPluginAuthIntegrationTests {
-    func saveNote(_ id: String = UUID().uuidString, content: String) -> SocialNote {
-        let localNote = SocialNote(id: id, content: content, owner: nil)
-        var savedNoteOptional: SocialNote?
-        let localNoteSaveInvoked = expectation(description: "local note was saved")
-        Amplify.DataStore.save(localNote) { result in
+    func saveModel(_ id: String = UUID().uuidString, content: String) -> TodoExplicitOwnerField {
+        let localTodo = TodoExplicitOwnerField(id: id, content: content, owner: nil)
+        var savedTodoOptional: TodoExplicitOwnerField?
+        let localTodoSaveInvoked = expectation(description: "local todo was saved")
+        Amplify.DataStore.save(localTodo) { result in
             switch result {
-            case .success(let note):
-                savedNoteOptional = note
-                localNoteSaveInvoked.fulfill()
+            case .success(let todo):
+                savedTodoOptional = todo
+                localTodoSaveInvoked.fulfill()
             case .failure(let error):
-                XCTFail("Failed to save note \(error)")
+                XCTFail("Failed to save todo \(error)")
             }
         }
-        wait(for: [localNoteSaveInvoked], timeout: TestCommonConstants.networkTimeout)
-        guard let savedNote = savedNoteOptional else {
-            fatalError("Could not save note")
+        wait(for: [localTodoSaveInvoked], timeout: TestCommonConstants.networkTimeout)
+        guard let savedTodo = savedTodoOptional else {
+            fatalError("Could not save todo")
         }
-        return savedNote
+        return savedTodo
     }
 
-    func queryNote(byId id: String) -> SocialNote? {
-        var queriedNoteOptional: SocialNote?
-        let localNoteQueriedInvoked = expectation(description: "note was queried")
-        Amplify.DataStore.query(SocialNote.self, byId: id) { result in
+    func queryModel(byId id: String) -> TodoExplicitOwnerField? {
+        var queriedTodoOptional: TodoExplicitOwnerField?
+        let localTodoQueriedInvoked = expectation(description: "todo was queried")
+        Amplify.DataStore.query(TodoExplicitOwnerField.self, byId: id) { result in
             switch result {
-            case .success(let socialNoteOptional):
-                queriedNoteOptional = socialNoteOptional
-                localNoteQueriedInvoked.fulfill()
+            case .success(let todoOptional):
+                queriedTodoOptional = todoOptional
+                localTodoQueriedInvoked.fulfill()
             case .failure(let error):
-                XCTFail("Failed to query note \(error)")
+                XCTFail("Failed to query todo \(error)")
             }
         }
-        wait(for: [localNoteQueriedInvoked], timeout: TestCommonConstants.networkTimeout)
-        return queriedNoteOptional
+        wait(for: [localTodoQueriedInvoked], timeout: TestCommonConstants.networkTimeout)
+        return queriedTodoOptional
     }
 }
--- AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginAuthIntegrationTests/DefaultAuth/AWSDataStoreCategoryPluginAuthIntegrationTests.swift
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPluginAuthIntegrationTests/DefaultAuth/AWSDataStoreCategoryPluginAuthIntegrationTests.swift
@@ -12,60 +12,9 @@ import AWSDataStoreCategoryPlugin
 @testable import Amplify
 @testable import AmplifyTestCommon
 
-class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
-
-    struct User {
-        let username: String
-        let password: String
-    }
-
-    let amplifyConfigurationFile = "testconfiguration/AWSDataStoreCategoryPluginAuthIntegrationTests-amplifyconfiguration"
-    let credentialsFile = "testconfiguration/AWSDataStoreCategoryPluginAuthIntegrationTests-credentials"
-    var user1: User!
-    var user2: User!
-
+class AWSDataStoreCategoryPluginAuthIntegrationTests: AWSDataStoreAuthBaseTest {
     let syncReceived = HubPayload.EventName.DataStore.syncReceived
     let syncStarted = HubPayload.EventName.DataStore.syncStarted
-
-    override func setUp() {
-        Amplify.Logging.logLevel = .verbose
-
-        do {
-            let credentials = try TestConfigHelper.retrieveCredentials(forResource: credentialsFile)
-
-            guard let user1 = credentials["user1"],
-                let user2 = credentials["user2"],
-                let passwordUser1 = credentials["passwordUser1"],
-                let passwordUser2 = credentials["passwordUser2"] else {
-                    XCTFail("Missing credentials.json data")
-                    return
-            }
-
-            self.user1 = User(username: user1, password: passwordUser1)
-            self.user2 = User(username: user2, password: passwordUser2)
-
-            try Amplify.add(plugin: AWSDataStorePlugin(modelRegistration: SocialNoteModelRegistration()))
-            try Amplify.add(plugin: AWSAPIPlugin())
-            try Amplify.add(plugin: AWSCognitoAuthPlugin())
-            let amplifyConfig = try TestConfigHelper.retrieveAmplifyConfiguration(forResource: amplifyConfigurationFile)
-            try Amplify.configure(amplifyConfig)
-
-        } catch {
-            XCTFail("Error during setup: \(error)")
-        }
-        if isSignedIn() {
-            signOut()
-        }
-    }
-
-    override func tearDown() {
-        let stopped = expectation(description: "stopped")
-        Amplify.DataStore.stop { _ in stopped.fulfill() }
-        waitForExpectations(timeout: 1.0)
-
-        signOut()
-        Amplify.reset()
-    }
 
     /// A user can persist data in the local store without signing in. Once the user signs in,
     /// the sync engine will start and sync the mutations to the cloud. Once the reconciliation is complete, retrieving
@@ -73,32 +22,34 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
     ///
     /// - Given: A DataStore plugin configured with SocialNote model containing with auth rules
     /// - When:
-    ///    - User is not signed in, then user can successfully save a note to local store
-    ///    - User remains signed out, then user can successfully retrieve the saved note, with empty owner field
-    ///    - User signs in, retrieves note, sync engine is started and reconciles local store with the ownerId
-    ///    - The note now it contains the ownerId
+    ///    - User is not signed in, then user can successfully save a todo to local store
+    ///    - User remains signed out, then user can successfully retrieve the saved todo, with empty owner field
+    ///    - User signs in, retrieves tods, sync engine is started and reconciles local store with the ownerId
+    ///    - The todo now it contains the ownerId
     func testUnauthenticatedSavesToLocalStoreIsReconciledWithCloudStoreAfterAuthentication() throws {
-        let savedLocalNote = saveNote(content: "owner saved note")
-        let queriedNoteOptional = queryNote(byId: savedLocalNote.id)
-        guard let note = queriedNoteOptional else {
-            XCTFail("Failed to query local note")
+        setup(withModels: ModelsRegistration(), authStrategy: .default)
+
+        let savedLocalTodo = saveModel(content: "owner saved model")
+        let queriedNoteOptional = queryModel(byId: savedLocalTodo.id)
+        guard let model = queriedNoteOptional else {
+            XCTFail("Failed to query local model")
             return
         }
-        guard note.owner == nil else {
+        guard model.owner == nil else {
             XCTFail("Owner field in model is not automatically set on a local DataStore call. It should be empty")
             return
         }
 
         let syncReceivedInvoked = expectation(description: "Received SyncReceived event")
-        var remoteNoteOptional: SocialNote?
+        var remoteTodoOptional: TodoExplicitOwnerField?
         let syncReceivedListener = Amplify.Hub.listen(to: .dataStore, eventName: syncReceived) { payload in
             guard let mutationEvent = payload.data as? MutationEvent,
-                let note = try? mutationEvent.decodeModel() as? SocialNote else {
+                let todo = try? mutationEvent.decodeModel() as? TodoExplicitOwnerField else {
                     XCTFail("Can't cast payload as mutation event")
                     return
             }
-            if note.id == savedLocalNote.id {
-                remoteNoteOptional = note
+            if todo.id == savedLocalTodo.id {
+                remoteTodoOptional = todo
                 syncReceivedInvoked.fulfill()
             }
         }
@@ -107,16 +58,16 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
             return
         }
 
-        signIn(username: user1.username, password: user1.password)
+        signIn(user: user1)
 
         wait(for: [syncReceivedInvoked], timeout: TestCommonConstants.networkTimeout)
         Amplify.Hub.removeListener(syncReceivedListener)
-        guard let remoteNote = remoteNoteOptional else {
+        guard let remoteTodo = remoteTodoOptional else {
             XCTFail("Should have received a SyncReceived event with the remote note reconciled to local store")
             return
         }
-        guard let remoteNoteOwner = remoteNote.owner else {
-            XCTFail("The synchronized model from remote should contain the owner field peristed")
+        guard let remoteTodoOwner = remoteTodo.owner else {
+            XCTFail("The synchronized model from remote should contain the owner field persisted")
             return
         }
 
@@ -124,7 +75,7 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
             XCTFail("Couldn't get current user signed in user")
             return
         }
-        XCTAssertEqual(user.username, remoteNoteOwner)
+        XCTAssertEqual(user.username, remoteTodoOwner)
     }
 
     /// User1, while signed in, creates some data, called "NewUser1Data" in a local store. We wait for this data to be
@@ -132,29 +83,31 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
     /// the remote sync engine will be halted. After this finishes, we sign out of user1 and sign in with user2, which
     /// will restart the sync engine and read all of data from the backend including "NewUser1Data".
     ///
-    /// - Given: A DataStore plugin configured with auth enabled SocialNote model that can be read others.
+    /// - Given: A DataStore plugin configured with auth enabled TodoExplicitOwnerField model that can be read others.
     /// - When:
-    ///    - The owner user is signed in, user saves a note, syncReceived successfully
-    ///    - Owner signs out, `DataStore.clear`, then retrieving the note returns nil
+    ///    - The owner user is signed in, user saves a todo, syncReceived successfully
+    ///    - Owner signs out, `DataStore.clear`, then retrieving the todo returns nil
     ///    - The other user signs in, sync engine is started and does a full sync
-    ///    - The other user is able to retrieve the owner's note
+    ///    - The other user is able to retrieve the owner's todo
     func testOwnerCreatedDataCanBeReadByOtherUsersForReadableModel() throws {
-        signIn(username: user1.username, password: user1.password)
+        setup(withModels: ModelsRegistration(), authStrategy: .default)
+
+        signIn(user: user1)
 
         let id = UUID().uuidString
-        let localNote = SocialNote(id: id, content: "owner created content", owner: nil)
-        let localNoteSaveInvoked = expectation(description: "local note was saved")
+        let localTodo = TodoExplicitOwnerField(id: id, content: "owner created content", owner: nil)
+        let localTodoSaveInvoked = expectation(description: "local note was saved")
 
         let syncReceivedInvoked = expectation(description: "received SyncReceived event")
-        var remoteNoteOptional: SocialNote?
+        var remoteTodoOptional: TodoExplicitOwnerField?
         let syncReceivedListener = Amplify.Hub.listen(to: .dataStore, eventName: syncReceived) { payload in
             guard let mutationEvent = payload.data as? MutationEvent,
-                let note = try? mutationEvent.decodeModel() as? SocialNote else {
+                let todo = try? mutationEvent.decodeModel() as? TodoExplicitOwnerField else {
                     XCTFail("Can't cast payload as mutation event")
                     return
             }
-            if note.id == localNote.id {
-                remoteNoteOptional = note
+            if todo.id == localTodo.id {
+                remoteTodoOptional = todo
                 syncReceivedInvoked.fulfill()
             }
         }
@@ -163,23 +116,22 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
             return
         }
 
-        Amplify.DataStore.save(localNote) { result in
+        Amplify.DataStore.save(localTodo) { result in
             switch result {
             case .success(let note):
-                print(note)
-                localNoteSaveInvoked.fulfill()
+                localTodoSaveInvoked.fulfill()
             case .failure(let error):
                 XCTFail("Failed to save note \(error)")
             }
         }
-        wait(for: [localNoteSaveInvoked], timeout: TestCommonConstants.networkTimeout)
+        wait(for: [localTodoSaveInvoked], timeout: TestCommonConstants.networkTimeout)
         wait(for: [syncReceivedInvoked], timeout: TestCommonConstants.networkTimeout)
         Amplify.Hub.removeListener(syncReceivedListener)
-        guard let remoteNote = remoteNoteOptional else {
+        guard let remoteTodo = remoteTodoOptional else {
             XCTFail("Should have received a SyncReceived event with the remote note reconciled to local store")
             return
         }
-        guard let owner = remoteNote.owner else {
+        guard let owner = remoteTodo.owner else {
             XCTFail("Could not retrieve owner value from remote note")
             return
         }
@@ -198,7 +150,7 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
 
         wait(for: [clearCompletedInvoked], timeout: TestCommonConstants.networkTimeout)
 
-        let localNoteOptional = queryNote(byId: id)
+        let localNoteOptional = queryModel(byId: id)
         XCTAssertNil(localNoteOptional)
 
         let syncStartedInvoked2 = expectation(description: "Sync started after other sign in")
@@ -211,14 +163,14 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
         }
 
         let syncReceivedInvoked2 = expectation(description: "received SyncReceived event for owner")
-        var remoteNoteOptional2: SocialNote?
+        var remoteNoteOptional2: TodoExplicitOwnerField?
         let syncReceivedListener2 = Amplify.Hub.listen(to: .dataStore, eventName: syncReceived) { payload in
             guard let mutationEvent = payload.data as? MutationEvent,
-                let note = try? mutationEvent.decodeModel() as? SocialNote else {
+                let note = try? mutationEvent.decodeModel() as? TodoExplicitOwnerField else {
                     XCTFail("Can't cast payload as mutation event")
                     return
             }
-            if note.id == localNote.id {
+            if note.id == localTodo.id {
                 remoteNoteOptional2 = note
                 syncReceivedInvoked2.fulfill()
             }
@@ -227,7 +179,7 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
             XCTFail("syncReceivedListener2 registered for hub")
             return
         }
-        signIn(username: user2.username, password: user2.password)
+        signIn(user: user2)
         guard let currentUser = Amplify.Auth.getCurrentUser() else {
             XCTFail("Could not retrieve current user")
             return
@@ -243,5 +195,16 @@ class AWSDataStoreCategoryPluginAuthIntegrationTests: XCTestCase {
         }
 
         XCTAssertEqual(owner, remoteNoteOwner)
+    }
+}
+
+extension AWSDataStoreCategoryPluginAuthIntegrationTests {
+    private struct ModelsRegistration: AmplifyModelRegistration {
+        var version: String = "version"
+
+        public func registerModels(registry: ModelRegistry.Type) {
+            ModelRegistry.register(modelType: TodoExplicitOwnerField.self)
+            ModelRegistry.register(modelType: TodoImplicitOwnerField.self)
+          }
     }
 }
