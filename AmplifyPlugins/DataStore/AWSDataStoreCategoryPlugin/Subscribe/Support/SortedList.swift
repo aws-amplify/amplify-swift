45ba5cf5145ce705b522e956ad6d620fe7f802b1
Michael <1365977+lawmicha@users.noreply.github.com>
2021-11-11 16:41:24 -0500

    fix(DataStore): Remove from snapshot when item no longer matches predicate

--- AmplifyPlugins/DataStore/AWSDataStoreCategoryPlugin/Subscribe/Support/SortedList.swift
+++ AmplifyPlugins/DataStore/AWSDataStoreCategoryPlugin/Subscribe/Support/SortedList.swift
@@ -9,11 +9,13 @@ import Amplify
 
 class SortedList<ModelType: Model> {
     var sortedModels: [ModelType]
+    var modelIds: Set<Model.Identifier>
     private let sortInput: [QuerySortDescriptor]?
     private let modelSchema: ModelSchema
 
     init(sortInput: [QuerySortDescriptor]?, modelSchema: ModelSchema) {
         self.sortedModels = []
+        self.modelIds = []
         self.sortInput = sortInput
         self.modelSchema = modelSchema
     }
@@ -22,16 +24,16 @@ class SortedList<ModelType: Model> {
     /// events since identical events may have different sources (local and remote). When the mutation type is delete,
     /// remove it if it exists in the array. When create/update, replace it if it exists in the array. If it does not
     /// exist in the array, then add the model to the array in the correct position.
-    func apply(model: ModelType, mutationType: MutationEvent.MutationType) {
+    /// Return `true` if something occured (added, replaced, deleted), otherwise `false`
+    @discardableResult func apply(model: ModelType, mutationType: MutationEvent.MutationType) -> Bool {
         if mutationType == MutationEvent.MutationType.delete {
-            if let index = sortedModels.firstIndex(where: { $0.id == model.id }) {
-                sortedModels.remove(at: index)
-            }
-        } else if let index = sortedModels.firstIndex(where: { $0.id == model.id }) {
+            return remove(model)
+        } else if modelIds.contains(model.id), let index = sortedModels.firstIndex(where: { $0.id == model.id }) {
             sortedModels[index] = model
         } else {
             add(model: model)
         }
+        return true
     }
 
     /// Add the incoming `model` to the sorted array based on the sort input, or at the end if none is provided.
@@ -42,6 +44,7 @@ class SortedList<ModelType: Model> {
     func add(model: ModelType) {
         guard let sortInputs = sortInput else {
             sortedModels.append(model)
+            modelIds.insert(model.id)
             return
         }
 
@@ -59,6 +62,18 @@ class SortedList<ModelType: Model> {
         }
 
         sortedModels.insert(model, at: index)
+        modelIds.insert(model.id)
+    }
+
+    /// Tries to remove the `model`, if removed then return `true`, otherwise `false`
+    func remove(_ model: ModelType) -> Bool {
+        if modelIds.contains(model.id), let index = sortedModels.firstIndex(where: { $0.id == model.id }) {
+            sortedModels.remove(at: index)
+            modelIds.remove(model.id)
+            return true
+        } else {
+            return false
+        }
     }
 }
 
