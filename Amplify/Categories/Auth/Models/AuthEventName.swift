a419f11be0680bf3cb0ea483d522bbe7f0ded6a9
Chris Ameter <29186822+ameter@users.noreply.github.com>
2021-12-28 15:34:18 -0700

    added Auth.userDeleted hub event

--- Amplify/Categories/Auth/Models/AuthEventName.swift
+++ Amplify/Categories/Auth/Models/AuthEventName.swift
@@ -15,6 +15,9 @@ public extension HubPayload.EventName.Auth {
     /// eventName emitted when a user is signedOut from Auth category
     static let signedOut = "Auth.signedOut"
 
+    /// eventName emitted when a user is deleted from Auth category
+    static let userDeleted = "Auth.userDeleted"
+
     /// eventName emitted when the current session has expired
     static let sessionExpired = "Auth.sessionExpired"
 }
