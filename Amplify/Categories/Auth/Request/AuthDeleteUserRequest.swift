0ea37a033f7b5eb28247a004687378f4c0682ead
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-01-03 12:59:55 -0700

    Update Amplify/Categories/Auth/Request/AuthDeleteUserRequest.swift
    Co-authored-by: Jithin Roy <51138777+royjit@users.noreply.github.com>

--- Amplify/Categories/Auth/Request/AuthDeleteUserRequest.swift
+++ Amplify/Categories/Auth/Request/AuthDeleteUserRequest.swift
@@ -10,7 +10,7 @@ import Foundation
 /// Request for delete user
 public struct AuthDeleteUserRequest: AmplifyOperationRequest {
 
-    /// Options is unused for AuthDeleteUserRequest.  It is included for conformance
+    /// Options is unused for AuthDeleteUserRequest. It is included for conformance
     /// with the AmplifyOperationRequest protocol.
     public var options = Options()
 
