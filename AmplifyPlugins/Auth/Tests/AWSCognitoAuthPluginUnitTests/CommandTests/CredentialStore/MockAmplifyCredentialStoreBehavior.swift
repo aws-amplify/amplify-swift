585c35e10a8d423637f78e32e9e26c94734ce4c3
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2022-01-07 16:32:08 -0500

    feat: adding logic to migrate legacy credential store (#14)
    * feat: adding logic to migrate legacy credential store
    
    * feat: adding unit tests for legacy key store migration
    
    * Working on the review comments
    
    * feat: removing containsData api from credential store

---
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/CommandTests/CredentialStore/MockAmplifyCredentialStoreBehavior.swift
@@ -0,0 +1,42 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+@testable import AWSCognitoAuthPlugin
+
+class MockAmplifyCredentialStoreBehavior: AmplifyAuthCredentialStoreBehavior, AmplifyAuthCredentialStoreProvider {
+    
+    typealias Migrationhandler = () -> ()
+    typealias SaveCredentialHandler = (AWSCognitoAuthCredential) -> Void
+        
+    let migrationCompleteHandler: Migrationhandler?
+    let saveCredentialHandler: SaveCredentialHandler?
+    
+    init(migrationCompleteHandler: Migrationhandler? = nil,
+         saveCredentialHandler:  SaveCredentialHandler? = nil) {
+        self.migrationCompleteHandler = migrationCompleteHandler
+        self.saveCredentialHandler = saveCredentialHandler
+    }
+    
+    func saveCredential(_ credential: AWSCognitoAuthCredential) throws {
+        saveCredentialHandler?(credential)
+    }
+    
+    func retrieveCredential() throws -> AWSCognitoAuthCredential? {
+        return nil
+    }
+    
+    func deleteCredential() throws {
+        migrationCompleteHandler?()
+    }
+    
+    func getCredentialStore() -> CredentialStoreBehavior {
+        return MockCredentialStoreBehavior(data: "mock")
+    }
+
+}
---
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/CommandTests/CredentialStore/MockCredentialStoreBehavior.swift
@@ -0,0 +1,43 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+@testable import AWSCognitoAuthPlugin
+
+class MockCredentialStoreBehavior: CredentialStoreBehavior {
+    
+    typealias VoidHandler = () -> ()
+    
+    let data: String
+    let removeAllHandler: VoidHandler?
+    
+    init(data: String,
+         removeAllHandler: VoidHandler? = nil) {
+        self.data = data
+        self.removeAllHandler = removeAllHandler
+    }
+    
+    func getString(_ key: String) throws -> String {
+        return data
+    }
+    
+    func getData(_ key: String) throws -> Data {
+        return data.data(using: .utf8)!
+    }
+    
+    func set(_ value: String, key: String) throws { }
+    
+    func set(_ value: Data, key: String) throws { }
+    
+    func remove(_ key: String) throws {
+    }
+    
+    func removeAll() throws {
+        removeAllHandler?()
+    }
+}
