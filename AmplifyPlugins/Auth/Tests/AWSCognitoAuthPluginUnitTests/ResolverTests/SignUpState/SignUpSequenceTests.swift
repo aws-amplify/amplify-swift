32c1a3e976e3bc6e18c446179816cdad9f84bece
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2022-01-12 18:48:04 -0800

    feature: adds SignUp state machine support (#18)
    * feat: adds SignUp state machine support

---
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/ResolverTests/SignUpState/SignUpSequenceTests.swift
@@ -0,0 +1,73 @@
+//// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import XCTest
+
+@testable import AWSCognitoAuthPlugin
+
+import AWSCognitoIdentityProvider
+import hierarchical_state_machine_swift
+
+typealias SignUpStateSequence = StateSequence<SignUpState, SignUpEvent>
+
+extension SignUpStateSequence {
+    init(oldState: MyState,
+         event: MyEvent,
+         expected: MyState
+    ) {
+        self.resolver = SignUpState.Resolver().logging().eraseToAnyResolver()
+        self.oldState = oldState
+        self.event = event
+        self.expected = expected
+    }
+}
+
+class SignUpSequenceTests: XCTestCase {
+    func testValidSignUpSequences() throws {
+        let validSequences: [SignUpStateSequence] = [
+            StateSequence(oldState: .notStarted,
+                          event: .initiateSignUpEvent,
+                          expected: .initiatingSigningUp),
+            StateSequence(oldState: .signingUpInitiated,
+                          event: .initiateSignUpSuccessEvent,
+                          expected: .signingUpInitiated),
+            StateSequence(oldState: .initiatingSigningUp,
+                          event: .initiateSignUpFailureEvent,
+                          expected: .error),
+            StateSequence(oldState: .confirmingSignUp,
+                          event: .confirmSignUpEvent,
+                          expected: .confirmingSignUp),
+            StateSequence(oldState: .confirmingSignUp,
+                          event: .confirmSignUpSuccessEvent,
+                          expected: .signedUp),
+            StateSequence(oldState: .confirmingSignUp,
+                          event: .confirmSignUpFailureEvent,
+                          expected: .error)
+        ]
+
+        for sequence in validSequences {
+            sequence.assertResolvesToExpected()
+        }
+    }
+
+    func testInvalidSignUpSequences() throws {
+        let invalidSequences: [SignUpStateSequence] = [
+            SignUpStateSequence(oldState: .notStarted,
+                                event: .confirmSignUpEvent,
+                                expected: .initiatingSigningUp),
+            SignUpStateSequence(oldState: .confirmingSignUp,
+                                event: .confirmSignUpSuccessEvent,
+                                expected: .error),
+            SignUpStateSequence(oldState: .confirmingSignUp,
+                                event: .confirmSignUpFailureEvent,
+                                expected: .signedUp)
+        ]
+
+        for sequence in invalidSequences {
+            sequence.assertNotResolvesToExpected()
+        }
+    }
+}
