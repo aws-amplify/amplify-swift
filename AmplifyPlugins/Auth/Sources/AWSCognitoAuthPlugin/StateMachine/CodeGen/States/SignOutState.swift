0767720a3d6decbd0138582cda7773c1fa9a75c6
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-02-09 11:30:26 -0700

    feat(auth): signOut (#45)

---
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/StateMachine/CodeGen/States/SignOutState.swift
@@ -0,0 +1,51 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import AWSCognitoIdentityProvider
+
+public enum SignOutState: State {
+    case notStarted
+    case signingOutGlobally
+    case revokingToken
+    case signingOutLocally(SignedInData)
+    case signedOut(SignedOutData)
+    case error(AuthenticationError)
+}
+
+public extension SignOutState {
+    var type: String {
+        switch self {
+        case .notStarted: return "SignOutState.notStarted"
+        case .signingOutGlobally: return "SignOutState.signingOutGlobally"
+        case .revokingToken: return "SignOutState.revokingToken"
+        case .signingOutLocally: return "SignOutState.clearingCredentialStore"
+        case .signedOut: return "SignOutState.signedOut"
+        case .error: return "SignOutState.error"
+        }
+    }
+
+    static func == (lhs: SignOutState, rhs: SignOutState) -> Bool {
+        switch (lhs, rhs) {
+        case (.notStarted, .notStarted):
+            return true
+        case (.signingOutGlobally, .signingOutGlobally):
+            return true
+        case (.revokingToken, .revokingToken):
+            return true
+        case (.signingOutLocally, .signingOutLocally):
+            return true
+        case (.signedOut(let lhsData), .signedOut(let rhsData)):
+            return lhsData == rhsData
+        case (.error(let lhsData), .error(let rhsData)):
+            return lhsData == rhsData
+        default:
+            return false
+        }
+    }
+}
+
+
