e277114ced0f4f04a0534209fd969ddf28834eea
Jithin Roy <51138777+royjit@users.noreply.github.com>
2022-02-10 07:25:28 -0800

    fix: Auth fixes for different use cases (#46)
    * fix: Auth fixes for different use cases
    
    * As per PR comments

---
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/StateMachine/ErrorMapping/SRPSignInError+Helper.swift
@@ -0,0 +1,77 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+import Amplify
+import ClientRuntime
+import AWSCognitoIdentityProvider
+
+extension SRPSignInError {
+
+    var isUserUnConfirmed: Bool {
+        switch self {
+        case .service(error: let serviceError):
+            if let cognitoError = serviceError as? SdkError<InitiateAuthOutputError>,
+               case .service(let serviceError, _) = cognitoError,
+               case .userNotConfirmedException = serviceError {
+                return true
+            }
+            else if let cognitoError = serviceError as? SdkError<RespondToAuthChallengeOutputError>,
+                    case .service(let serviceError, _) = cognitoError,
+                    case .userNotConfirmedException = serviceError {
+                return true
+            }
+            return false
+        default:
+            return false
+        }
+    }
+
+    var isResetPassword: Bool {
+        switch self {
+        case .service(error: let serviceError):
+            if let cognitoError = serviceError as? SdkError<InitiateAuthOutputError>,
+               case .service(let serviceError, _) = cognitoError,
+               case .passwordResetRequiredException = serviceError {
+                return true
+            }
+            else if let cognitoError = serviceError as? SdkError<RespondToAuthChallengeOutputError>,
+                    case .service(let serviceError, _) = cognitoError,
+                    case .passwordResetRequiredException = serviceError {
+                return true
+            }
+            return false
+        default:
+            return false
+        }
+    }
+}
+
+extension SRPSignInError: AuthErrorConvertible {
+    var authError: AuthError {
+        switch self {
+        case .configuration(let message):
+            return AuthError.configuration(message, "")
+        case .service(let error):
+            if let initiateAuthError = error as? AuthErrorConvertible {
+                return initiateAuthError.authError
+            } else {
+                return AuthError.unknown("", error)
+            }
+        case . invalidServiceResponse(message: let message):
+            return AuthError.service(message, "")
+        case .calculation:
+            return AuthError.unknown("SignIn calculation returned an error")
+        case .inputValidation(let field):
+            return AuthError.validation(
+                field,
+                AuthPluginErrorConstants.signInUsernameError.errorDescription,
+                AuthPluginErrorConstants.signInUsernameError.recoverySuggestion)
+        }
+    }
+
+}
