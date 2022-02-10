e3e5756accb77435e8a9cd3c87358a29cb674434
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2022-02-10 09:13:12 -0800

    feat(auth): implements confirm signup behavior (#49)

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Operations/AWSAuthConfirmSignUpOperation.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Operations/AWSAuthConfirmSignUpOperation.swift
@@ -10,14 +10,15 @@ import Amplify
 import ClientRuntime
 import AWSCognitoIdentityProvider
 
-public typealias AmplifySignUpOperation = AmplifyOperation<AuthSignUpRequest, AuthSignUpResult, AuthError>
-typealias AWSAuthSignUpOperationStateMachine = StateMachine<AuthState, AuthEnvironment>
+public typealias AmplifyConfirmSignUpOperation = AmplifyOperation<AuthConfirmSignUpRequest, AuthSignUpResult, AuthError>
+typealias AWSAuthConfirmSignUpOperationStateMachine = StateMachine<AuthState, AuthEnvironment>
 
-public class AWSAuthSignUpOperation: AmplifySignUpOperation, AuthSignUpOperation {
+public class AWSAuthConfirmSignUpOperation: AmplifyConfirmSignUpOperation, AuthConfirmSignUpOperation {
 
-    let stateMachine: AuthStateMachine
+    let stateMachine: AWSAuthConfirmSignUpOperationStateMachine
+    var statelistenerToken: AWSAuthSignUpOperationStateMachine.StateChangeListenerToken?
 
-    init(_ request: AuthSignUpRequest,
+    init(_ request: AuthConfirmSignUpRequest,
          stateMachine: AWSAuthSignUpOperationStateMachine,
          resultListener: ResultListener?) {
 
@@ -37,7 +38,7 @@ public class AWSAuthSignUpOperation: AmplifySignUpOperation, AuthSignUpOperation
     }
 
     func doInitialize() {
-        var token: AuthStateMachine.StateChangeListenerToken?
+        var token: AWSAuthSignInOperationStateMachine.StateChangeListenerToken?
         token = stateMachine.listen { [weak self] in
             guard let self = self else {
                 return
@@ -46,18 +47,18 @@ public class AWSAuthSignUpOperation: AmplifySignUpOperation, AuthSignUpOperation
             if case .configured(let authNState, _) = $0 {
                 guard case .signedOut = authNState else {
                     self.cancelListener(token)
-                    let message = "Auth state must be signed out to sign up"
+                    let message = "Auth state must be signed out to confirm signup"
                     self.dispatch(message: message, error: .invalidState(message: message))
                     self.finish()
                     return
                 }
                 self.cancelListener(token)
-                self.doSignUp()
+                self.doConfirmSignUp()
             }
         } onSubscribe: { }
     }
 
-    func doSignUp() {
+    func doConfirmSignUp() {
         var token: AWSAuthSignUpOperationStateMachine.StateChangeListenerToken?
         token = stateMachine.listen { [weak self] state in
             guard let self = self else {
@@ -75,43 +76,26 @@ public class AWSAuthSignUpOperation: AmplifySignUpOperation, AuthSignUpOperation
                 self.cancelListener(token)
 
                 switch signUpState {
-                case .signingUpInitiated:
-                    self.dispatch(result: .success(AuthSignUpResult(.confirmUser())))
+                case .signedUp:
+                    self.dispatch(result: .success(AuthSignUpResult(.done)))
                 case .error(let error):
-                    self.dispatch(message: "Failed while signing up", error: error)
+                    self.dispatch(message: "Failed while confirming signup", error: error)
                 default:
-                    self.dispatch(message: "Failed while signing up", error: .invalidState(message: "\(signUpState.type)"))
+                    self.dispatch(message: "Failed while confirming signup", error: .invalidState(message: "\(signUpState.type)"))
                 }
             case .error(_, let error):
                 self.cancelListener(token)
-                self.dispatch(message: "failed while signing up", error: .service(error: error))
+                self.dispatch(message: "Failed while confirming signing up", error: .service(error: error))
             default:
-                print("default for \(authNState)")
-                break
+                self.cancelListener(token)
             }
-        } onSubscribe: { [weak self] in
-            guard let self = self else {
-                return
-            }
-            
-            self.sendSignUpEvent()
-        }
+        } onSubscribe: { }
+        sendConfirmSignUpEvent()
     }
 
-    private func sendSignUpEvent() {
-        guard let password = request.password else {
-            let message = "password is nil"
-            dispatch(message: message, error: .missingPassword(message: message))
-            return
-        }
-
-        if let error = SignUpPasswordValidator.validate(password: password) {
-            dispatch(message: "invalid password", error: error)
-            return
-        }
-
-        let signUpData = SignUpEventData(username: request.username, password: password)
-        let event = SignUpEvent(eventType: .initiateSignUp(signUpData))
+    private func sendConfirmSignUpEvent() {
+        let confirmSignUpData = ConfirmSignUpEventData(username: request.username, confirmationCode: request.code)
+        let event = SignUpEvent(eventType: .confirmSignUp(confirmSignUpData))
         stateMachine.send(event)
     }
 
@@ -125,4 +109,5 @@ public class AWSAuthSignUpOperation: AmplifySignUpOperation, AuthSignUpOperation
             stateMachine.cancel(listenerToken: token)
         }
     }
+
 }
