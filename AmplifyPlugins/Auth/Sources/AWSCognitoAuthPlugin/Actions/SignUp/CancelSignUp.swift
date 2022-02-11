bc142c67698ad84b5a9131b795a28cedbb63bb26
Jithin Roy <51138777+royjit@users.noreply.github.com>
2022-02-11 15:48:51 -0800

    fix(auth): SignUp and ConfirmSignUp fixes (#55)

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Actions/SignUp/CancelSignUp.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Actions/SignUp/CancelSignUp.swift
@@ -8,14 +8,14 @@
 import Amplify
 import Foundation
 
-struct CancelSignIn: Action {
-    let identifier = "CancelSignIn"
+struct CancelSignUp: Action {
+    let identifier = "CancelSignUp"
     
     func execute(withDispatcher dispatcher: EventDispatcher,
                  environment: Environment) {
-
+        
         Amplify.Logging.verbose("Starting execution \(#fileID)")
-
+        
         guard let environment = environment as? AuthEnvironment else {
             let message = AuthPluginErrorConstants.configurationError
             let error = AuthenticationError.configuration(message: message)
@@ -24,16 +24,17 @@ struct CancelSignIn: Action {
             Amplify.Logging.verbose("Sending event \(#fileID)")
             return
         }
-        let event = AuthenticationEvent(eventType: .cancelSignIn(environment.configuration))
+        let event = AuthenticationEvent(
+            eventType: .cancelSignUp(environment.configuration))
         dispatcher.send(event)
         Amplify.Logging.verbose("Sending event \(#fileID)")
-
+        
     }
 }
 
-extension CancelSignIn: DefaultLogger { }
+extension CancelSignUp: DefaultLogger { }
 
-extension CancelSignIn: CustomDebugDictionaryConvertible {
+extension CancelSignUp: CustomDebugDictionaryConvertible {
     public var debugDictionary: [String: Any] {
         [
             "identifier": identifier
@@ -41,7 +42,7 @@ extension CancelSignIn: CustomDebugDictionaryConvertible {
     }
 }
 
-extension CancelSignIn: CustomDebugStringConvertible {
+extension CancelSignUp: CustomDebugStringConvertible {
     public var debugDescription: String {
         debugDictionary.debugDescription
     }
