cfe70a2fa112caac77d2092c4236a1b3fcf2e512
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2022-02-11 18:04:03 -0500

    feat(Auth): Migrate credentials on configuration changes (#57)

---
+++ AmplifyPlugins/Auth/Tests/AuthHostApp/AuthHostApp/AuthHostAppApp.swift
@@ -0,0 +1,17 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import SwiftUI
+
+@main
+struct AuthHostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
