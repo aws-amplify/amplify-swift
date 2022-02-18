f94e096a9dff9a79e87e30ea56c05d757a038918
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2022-02-18 15:49:17 -0800

    feat(s3): implements adapter functions for multipart upload operations (#71)

---
+++ AmplifyPlugins/Storage/AWSS3StoragePlugin/Dependency/AWSS3CompleteMultipartUploadRequest.swift
@@ -0,0 +1,27 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+import Amplify
+
+struct AWSS3CompleteMultipartUploadRequest {
+    let bucket: String
+    let key: String
+    let uploadId: String
+    let parts: [AWSS3MultipartUploadRequestCompletedPart]
+    
+    init(bucket: String,
+         key: String,
+         uploadId: String,
+         parts: [AWSS3MultipartUploadRequestCompletedPart]) {
+        self.bucket = bucket
+        self.key = key
+        self.uploadId = uploadId
+        self.parts = parts
+    }
+}
