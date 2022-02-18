15dafdbf06ab6c17a4f0dbe6c5c07061bcf639dc
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2022-02-17 16:44:00 -0800

    feat(s3): implements AWS S3 service with new Swift SDK for list & delete operations (#70)

--- AmplifyPlugins/Storage/AWSS3StoragePlugin/Dependency/AWSS3Behavior.swift
+++ AmplifyPlugins/Storage/AWSS3StoragePlugin/Dependency/AWSS3Behavior.swift
@@ -5,20 +5,24 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-/*
 import Foundation
+
+import Amplify
+
 import AWSS3
+import ClientRuntime
+import AWSClientRuntime
 
 // Behavior that the implemenation class for AWSS3 will use.
 protocol AWSS3Behavior {
 
-    // Lists objects in a bucket.
-    func listObjectsV2(_ request: AWSS3ListObjectsV2Request) -> AWSTask<AWSS3ListObjectsV2Output>
-
     // Deletes object.
-    func deleteObject(_ request: AWSS3DeleteObjectRequest) -> AWSTask<AWSS3DeleteObjectOutput>
+    func deleteObject(_ request: AWSS3DeleteObjectRequest, completion: @escaping (Result<Void, StorageError>) -> Void)
+
+    // Lists objects in a bucket.
+    func listObjectsV2(_ request: AWSS3ListObjectsV2Request, completion: @escaping (Result<StorageListResult, StorageError>) -> Void)
 
     // Gets instance of AWS Service.
-    func getS3() -> AWSS3
+    func getS3() -> S3Client
+
 }
-*/
