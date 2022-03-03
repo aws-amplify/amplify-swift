cacb7db6b6af4a4db186ce92a592d632124f1b6d
Ian Saultz <52051793+atierian@users.noreply.github.com>
2022-03-03 13:18:37 -0500

    chore(ci): add jazzy doc gen script (#1658)

---
+++ CircleciScripts/jazzy_doc_gen.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This script generates API documentation using jazzy.
+# It requires jazzy.
+
+set -e
+
+REPO="aws-amplify/amplify-ios.git"
+
+git clone git@github.com:$REPO $(mktemp -d -t amplify-release)
+TEMP_DIR=$_
+
+echo "Temporary Directory: $TEMP_DIR"
+
+generate_docs() {
+    cd $TEMP_DIR
+    git checkout gh-pages
+    git reset --hard origin/release
+    pod install
+    jazzy && ln -s ../readme-images docs
+    git add docs
+    git commit -m "chore: update API docs [skip ci]"
+    git push --force
+}
+
+generate_docs
+
+set +e
--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ source 'https://rubygems.org'
 gem 'xcpretty'
 gem 'cocoapods'
 gem 'fastlane'
+gem 'jazzy'
 plugins_path = File.join(File.dirname(__FILE__), 'fastlane', 'Pluginfile')
 eval_gemfile(plugins_path) if File.exist?(plugins_path)
