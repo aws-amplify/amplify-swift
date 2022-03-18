a2e8115c74b1308dce6a257f04cb6ee40ebe44e6
Jithin Roy <51138777+royjit@users.noreply.github.com>
2022-03-18 10:03:24 -0700

    chore: Typo change in the error message (#1682)

--- AmplifyPlugins/Core/AWSPluginsCore/Auth/Configuration/AWSAuthorizationConfiguration.swift
+++ AmplifyPlugins/Core/AWSPluginsCore/Auth/Configuration/AWSAuthorizationConfiguration.swift
@@ -36,7 +36,7 @@ extension AWSAuthorizationConfiguration {
                 throw PluginError.pluginConfigurationError(
                     "Could not get `ApiKey` from plugin configuration",
                     """
-                    The specified configuration does not have a string with the key `ApiKey`. Review the \
+                    The specified configuration does not have a string with the key `apiKey`. Review the \
                     configuration and ensure it contains the expected values.
                     """
                 )
