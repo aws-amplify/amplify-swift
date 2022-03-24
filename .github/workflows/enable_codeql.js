6935dbc17946af4de0545fbc49e1380ab8679815
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-03-24 13:50:32 -0600

    testing code scanning

--- .github/workflows/enable_codeql.js
+++ .github/workflows/enable_codeql.js
@@ -2,3 +2,4 @@
 GitHub CodeQL scanning is used to scan for errors in GitHub Actions workflows (.yml), 
 but currently requires a non-empty JavaScript file to work.
 */
+const why = 'This is needed to run CodeQL.';
