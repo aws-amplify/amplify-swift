e8e9171f75eccf31f90a2da64027918f97e81fcc
Abhash Kumar Singh <thisisabhash@gmail.com>
2022-03-24 14:33:44 -0700

    chore(DataStore): Remove deprecated load() API and rename .paginatedList()to list() API (#1702)
    * chore(DataStore): Remove deprecated load() API and rename .paginatedList()to list() API
    
    * Address review comments

--- Amplify/Categories/DataStore/Model/Collection/List+Combine.swift
+++ Amplify/Categories/DataStore/Model/Collection/List+Combine.swift
@@ -11,26 +11,5 @@ import Combine
 extension List {
 
     public typealias LazyListPublisher = AnyPublisher<[Element], DataStoreError>
-
-    /// This method has been deprecated, Use load(completion:) instead.
-    ///
-    /// Lazy load the collection and expose the loaded `Elements` as a Combine `Publisher`. The List will retrieve the
-    /// data from the underlying data source before the publisher is returned. This is useful for integrating the
-    /// `List<ModelType>` with existing Combine code and/or SwiftUI. This method has been deprecated
-    /// and is currently not a supported API.
-    ///
-    /// - Returns: a type-erased Combine publisher
-    @available(*, deprecated, message: "Use `load(completion:)` instead.")
-    public func loadAsPublisher() -> LazyListPublisher {
-        return Future { promise in
-            self.load {
-                switch $0 {
-                case .success(let elements):
-                    promise(.success(elements))
-                case .failure(let error):
-                    promise(.failure(error))
-                }
-            }
-        }.eraseToAnyPublisher()
-    }
+    
 }
