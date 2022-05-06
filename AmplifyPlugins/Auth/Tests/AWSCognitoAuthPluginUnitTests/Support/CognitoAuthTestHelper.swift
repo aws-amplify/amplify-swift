4e37ac3bb55810f1674236f9988829136d7529a5
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2022-05-05 20:33:16 -0400

    bugfix(Auth): Fixing the user pool token expiring logic (#1770)
    * bugfix(Auth): Fixing the token expiring logic
    
    * minor fixes
    
    * Moving the logic to an extension
    
    * Updated the unit tests
    
    * revert the foundation import
    
    * worked on comments

---
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/Support/CognitoAuthTestHelper.swift
@@ -0,0 +1,49 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import CryptoKit
+import Foundation
+
+struct CognitoAuthTestHelper {
+    
+    /// Helper to build a JWT Token
+    static func buildToken(for payload: [String: String]) -> String {
+        
+        struct Header: Encodable {
+            let alg = "HS256"
+            let typ = "JWT"
+        }
+        
+        let secret = "256-bit-secret"
+        let privateKey = SymmetricKey(data: Data(secret.utf8))
+
+        let headerJSONData = try! JSONEncoder().encode(Header())
+        let headerBase64String = headerJSONData.urlSafeBase64EncodedString()
+
+        let payloadJSONData = try! JSONEncoder().encode(payload)
+        let payloadBase64String = payloadJSONData.urlSafeBase64EncodedString()
+
+        let toSign = Data((headerBase64String + "." + payloadBase64String).utf8)
+
+        let signature = HMAC<SHA256>.authenticationCode(for: toSign, using: privateKey)
+        let signatureBase64String = Data(signature).urlSafeBase64EncodedString()
+
+        let token = [headerBase64String, payloadBase64String, signatureBase64String].joined(separator: ".")
+        
+        return token
+    }
+}
+
+
+fileprivate extension Data {
+    func urlSafeBase64EncodedString() -> String {
+        return base64EncodedString()
+            .replacingOccurrences(of: "+", with: "-")
+            .replacingOccurrences(of: "/", with: "_")
+            .replacingOccurrences(of: "=", with: "")
+    }
+}
