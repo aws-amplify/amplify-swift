4306ccf876b340badbc9f75a688dbac7a7e8e11d
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2022-05-20 10:53:59 -0300

    feat(Analytics): Initial changes for the new plugin based on the Swift SDK (#1808)
    * feat(Analytics): Initial changes for the new plugin based on the Swift SDK.
    
    * feat(Analytics): Moving NSNumber to Double and adding convenience methods.

---
+++ AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Dependency/Pinpoint/Session/SessionTracker.swift
@@ -0,0 +1,16 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+class SessionTracker: InternalPinpointClient {
+
+    var currentSession: PinpointSession {
+        // TODO: Implement
+        fatalError("Not yet implemented")
+    }
+}
