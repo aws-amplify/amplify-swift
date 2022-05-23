35d2c182688dc72552ddc57aba57b7bf23e0ecf7
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2022-05-23 12:15:05 -0300

    feat(Analytics): Implementing AnalyticsClient and PinpointEvent. (#1814)

--- AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Dependency/Pinpoint/Analytics/AnalyticsClient.swift
+++ AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Dependency/Pinpoint/Analytics/AnalyticsClient.swift
@@ -9,87 +9,184 @@ import Foundation
 import StoreKit
 
 class AnalyticsClient: InternalPinpointClient {
-    private let eventRecorder: EventRecorder
-    private var globalAttributes: [String: String] = [:]
-    private var globalMetrics: [String: Double] = [:]
-        
-    override init(context: PinpointContext) {
-        eventRecorder = EventRecorder()
+    private let eventRecorder: AnalyticsEventRecording
+    private lazy var globalAttributes: [String: String] = [:]
+    private lazy var globalMetrics: [String: Double] = [:]
+    private lazy var eventTypeAttributes: [String: [String: String]] = [:]
+    private lazy var eventTypeMetrics: [String: [String: Double]] = [:]
+    
+    init(eventRecorder: AnalyticsEventRecording = EventRecorder(),
+         context: PinpointContext) {
+        self.eventRecorder = eventRecorder
         super.init(context: context)
     }
     
     // MARK: - Attributes & Metrics
     func addGlobalAttribute(_ attribute: String, forKey key: String) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        precondition(!key.isEmpty, "Attributes and metrics must have a valid key")
+        globalAttributes[key] = attribute
     }
     
     func addGlobalAttribute(_ attribute: String, forKey key: String, forEventType eventType: String) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        precondition(!key.isEmpty, "Attributes and metrics must have a valid key")
+        eventTypeAttributes[eventType]?[key] = attribute
     }
 
     func addGlobalMetric(_ metric: Double, forKey key: String) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        precondition(!key.isEmpty, "Attributes and metrics must have a valid key")
+        globalMetrics[key] = metric
     }
     
     func addGlobalMetric(_ metric: Double, forKey key: String, forEventType eventType: String) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        precondition(!key.isEmpty, "Attributes and metrics must have a valid key")
+        eventTypeMetrics[eventType]?[key] = metric
     }
     
     func removeGlobalAttribute(forKey key: String) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        globalAttributes[key] = nil
     }
     
     func removeGlobalAttribute(forKey key: String, forEventType eventType: String) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        eventTypeAttributes[eventType]?[key] = nil
     }
     
     func removeGlobalMetric(forKey key: String) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        globalMetrics[key] = nil
     }
     
     func removeGlobalMetric(forKey key: String, forEventType eventType: String) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        eventTypeMetrics[eventType]?[key] = nil
     }
-    
     
     // MARK: - Monetization events
     func createAppleMonetizationEvent(with transaction: SKPaymentTransaction,
                                       with product: SKProduct) -> PinpointEvent {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        let numberFormatter = NumberFormatter()
+        numberFormatter.locale = product.priceLocale
+        numberFormatter.numberStyle = .currency
+        numberFormatter.isLenient = true
+
+        return createMonetizationEvent(withStore: Constants.PurchaseEvent.appleStore,
+                                       productId: product.productIdentifier,
+                                       quantity: transaction.payment.quantity,
+                                       itemPrice: product.price.doubleValue,
+                                       currencyCode: product.priceLocale.currencyCode,
+                                       formattedItemPrice: numberFormatter.string(from: product.price),
+                                       transactionId: transaction.transactionIdentifier)
     }
 
-    func createVirtualMonetizationEvent(withProductId theProductId: String,
-                                        withItemPrice theItemPrice: Double,
-                                        withQuantity theQuantity: Int,
-                                        withCurrency theCurrency: String) -> PinpointEvent {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+    func createVirtualMonetizationEvent(withProductId productId: String,
+                                        withItemPrice itemPrice: Double,
+                                        withQuantity quantity: Int,
+                                        withCurrency currency: String) -> PinpointEvent {
+        return createMonetizationEvent(withStore: Constants.PurchaseEvent.virtual,
+                                       productId: productId,
+                                       quantity: quantity,
+                                       itemPrice: itemPrice,
+                                       currencyCode: currency)
+    }
+    
+    private func createMonetizationEvent(withStore store: String,
+                                         productId: String,
+                                         quantity: Int,
+                                         itemPrice: Double,
+                                         currencyCode: String?,
+                                         formattedItemPrice: String? = nil,
+                                         priceLocale: Locale? = nil,
+                                         transactionId: String? = nil) -> PinpointEvent {
+        let monetizationEvent = PinpointEvent(eventType: Constants.PurchaseEvent.name,
+                                              eventTimestamp: Date.utcTimeMillisNow,
+                                              session: context.sessionTracker.currentSession)
+        monetizationEvent.addAttribute(store,
+                                       forKey: Constants.PurchaseEvent.Keys.store)
+        monetizationEvent.addAttribute(productId,
+                                       forKey: Constants.PurchaseEvent.Keys.productId)
+        monetizationEvent.addMetric(quantity,
+                                    forKey: Constants.PurchaseEvent.Keys.quantity)
+        monetizationEvent.addMetric(itemPrice,
+                                    forKey: Constants.PurchaseEvent.Keys.itemPrice)
+
+        if let currencyCode = currencyCode {
+            monetizationEvent.addAttribute(currencyCode,
+                                           forKey: Constants.PurchaseEvent.Keys.currency)
+        }
+
+        if let formattedItemPrice = formattedItemPrice {
+            monetizationEvent.addAttribute(formattedItemPrice,
+                                           forKey: Constants.PurchaseEvent.Keys.priceFormatted)
+        }
+
+        if let transactionId = transactionId {
+            monetizationEvent.addAttribute(transactionId,
+                                           forKey: Constants.PurchaseEvent.Keys.transactionId)
+        }
+
+        return monetizationEvent
     }
 
     // MARK: - Event recording
-
     func createEvent(withEventType eventType: String) -> PinpointEvent {
+        precondition(!eventType.isEmpty, "Event types must be at least 1 character in length.")
         return PinpointEvent(eventType: eventType,
-                             eventTyimestamp: Date().timeIntervalSince1970 * 1000,
+                             eventTimestamp: Date.utcTimeMillisNow,
                              session: context.sessionTracker.currentSession)
     }
     
-    func record(_ event: PinpointEvent) {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+    func record(_ event: PinpointEvent) async throws {
+        // Add event type attributes
+        if let eventAttributes = eventTypeAttributes[event.eventType] {
+            for (key, attribute) in eventAttributes {
+                event.addAttribute(attribute, forKey: key)
+            }
+        }
+
+        // Add event type metrics
+        if let eventMetrics = eventTypeMetrics[event.eventType] {
+            for (key, metric) in eventMetrics {
+                event.addMetric(metric, forKey: key)
+            }
+        }
+
+        // Add global attributes
+        for (key, attribute) in globalAttributes {
+            event.addAttribute(attribute, forKey: key)
+        }
+
+        // Add global metrics
+        for (key, metric) in globalMetrics {
+            event.addMetric(metric, forKey: key)
+        }
+
+        try await eventRecorder.save(event)
     }
     
     func submitEvents() async throws -> [PinpointEvent] {
-        // TODO: Implement
-        fatalError("Not yet implemented")
+        return try await eventRecorder.submitAllEvents()
+    }
+}
+
+extension AnalyticsClient {
+    private struct Constants {
+        struct PurchaseEvent {
+            static let name = "_monetization.purchase"
+            static let appleStore = "Apple"
+            static let virtual = "Virtual"
+
+            struct Keys {
+                static let productId = "_product_id"
+                static let quantity = "_quantity"
+                static let itemPrice = "_item_price"
+                static let priceFormatted = "_item_price_formatted"
+                static let currency = "_currency"
+                static let store = "_store"
+                static let transactionId = "_transaction_id"
+            }
+        }
+    }
+}
+
+extension Date {
+    static var utcTimeMillisNow: TimeInterval {
+        return Date().timeIntervalSince1970 * 1000
     }
 }
