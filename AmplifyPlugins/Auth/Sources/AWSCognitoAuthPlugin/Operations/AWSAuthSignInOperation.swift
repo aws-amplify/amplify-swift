a950de314848c75b74dd739e213549731f8f9130
Jithin Roy <51138777+royjit@users.noreply.github.com>
2022-05-26 16:51:58 -0700

    chore: Cleanup operations to stop relying on configuration state

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Operations/AWSAuthSignInOperation.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Operations/AWSAuthSignInOperation.swift
@@ -17,8 +17,7 @@ public typealias AmplifySignInOperation = AmplifyOperation<
     AuthError>
 
 public class AWSAuthSignInOperation: AmplifySignInOperation,
-                                     AuthSignInOperation
-{
+                                     AuthSignInOperation {
 
     let authStateMachine: AuthStateMachine
     let credentialStoreStateMachine: CredentialStoreStateMachine
@@ -26,8 +25,7 @@ public class AWSAuthSignInOperation: AmplifySignInOperation,
     init(_ request: AuthSignInRequest,
          authStateMachine: AuthStateMachine,
          credentialStoreStateMachine: CredentialStoreStateMachine,
-         resultListener: ResultListener?)
-    {
+         resultListener: ResultListener?) {
 
         self.authStateMachine = authStateMachine
         self.credentialStoreStateMachine = credentialStoreStateMachine
@@ -42,40 +40,20 @@ public class AWSAuthSignInOperation: AmplifySignInOperation,
             finish()
             return
         }
-        doInitialize { [weak self] authenticationState in
-            self?.tryToInvokeSignIn(authenticationState)
-        }
-    }
-    
-    func doInitialize(_ callback: @escaping (AuthenticationState) -> Void) {
-        var token: AuthStateMachineToken?
-        token = authStateMachine.listen { [weak self] in
-            guard let self = self else {
+        authStateMachine.getCurrentState { [weak self] in
+            guard case .configured(let authenticationState, _) = $0 else {
                 return
             }
-            // Auth should be configured to start the signIn process
-            if case .configured(let authenticationState, _) = $0 {
-                callback(authenticationState)
-                self.cancelToken(token)
-            }
-        } onSubscribe: { }
-    }
 
-    func tryToInvokeSignIn(_ authenticationState: AuthenticationState) {
-        switch authenticationState {
-        case .signedOut:
-            self.doSignIn()
-        case .signingUp(let configuration, _):
-            self.cancelSignUp(configuration)
-        case .signedIn:
-            self.dispatch(AuthError.invalidState(
-                "There is already a user in signedIn state. SignOut the user first before calling signIn",
-                AuthPluginErrorConstants.invalidStateError, nil))
-            self.finish()
-        default:
-            self.dispatch(AuthError.invalidState("Sign in reached an invalid state: \(authenticationState)",
-                                                 AuthPluginErrorConstants.invalidStateError, nil))
-            self.finish()
+            switch authenticationState {
+            case .signedIn:
+                self?.dispatch(AuthError.invalidState(
+                    "There is already a user in signedIn state. SignOut the user first before calling signIn",
+                    AuthPluginErrorConstants.invalidStateError, nil))
+                self?.finish()
+            default:
+                self?.doSignIn()
+            }
         }
     }
 
@@ -95,14 +73,22 @@ public class AWSAuthSignInOperation: AmplifySignInOperation,
             }
 
             switch authNState {
+            case .signedOut:
+                self.sendSignInEvent()
+
+            case .signingUp(let configuration, _):
+                self.sendCancelSignUpEvent(configuration)
+
             case .signedIn(_, let signedInData):
                 let cognitoTokens = signedInData.cognitoUserPoolTokens
                 self.storeUserPoolTokens(cognitoTokens)
                 self.cancelToken(token)
+
             case .error(_, let error):
                 self.dispatch(AuthError.unknown("Sign in reached an error state", error))
                 self.cancelToken(token)
                 self.finish()
+
             case .signingIn(_, let signInState):
                 if case .signingInWithSRP(let srpState, _) = signInState,
                    case .error(let signInError) = srpState
@@ -121,47 +107,7 @@ public class AWSAuthSignInOperation: AmplifySignInOperation,
             default:
                 break
             }
-        } onSubscribe: { [weak self] in
-            guard let self = self else {
-                return
-            }
-            self.sendSignInEvent()
-        }
-
-    }
-
-    func cancelSignUp(_ configuration: AuthConfiguration) {
-        if isCancelled {
-            finish()
-            return
-        }
-
-        var token: AuthStateMachine.StateChangeListenerToken?
-        token = authStateMachine.listen { [weak self] in
-            guard let self = self else {
-                return
-            }
-            guard case .configured(let authNState, _) = $0 else {
-                return
-            }
-
-            switch authNState {
-            case .signedOut:
-                self.tryToInvokeSignIn(authNState)
-                self.cancelToken(token)
-            case .error(_, let error):
-                self.dispatch(AuthError.unknown("Unable to cancel sign up", error))
-                self.cancelToken(token)
-                self.finish()
-            default:
-                break
-            }
-        } onSubscribe: { [weak self] in
-            guard let self = self else {
-                return
-            }
-            self.sendCancelSignUpEvent(configuration)
-        }
+        } onSubscribe: { }
     }
 
     func storeUserPoolTokens(_ tokens: AWSCognitoUserPoolTokens) {
