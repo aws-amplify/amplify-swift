f406b5de17316c9dddc1180c1fced5330cc84ed5
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-05-31 13:51:36 -0600

    finished storage updates. removed work-around files.

--- AmplifyPlugins/Storage/AWSS3StoragePlugin/Dependency/AWSS3Adapter.swift
+++ AmplifyPlugins/Storage/AWSS3StoragePlugin/Dependency/AWSS3Adapter.swift
@@ -31,13 +31,13 @@ class AWSS3Adapter: AWSS3Behavior {
     ///   - request:: request identifying object
     ///   - completion: handle indicates when the operation is done
     func deleteObject(_ request: AWSS3DeleteObjectRequest, completion: @escaping (Result<Void, StorageError>) -> Void) {
-        let input = DeleteObjectInput(bucket: request.bucket, key: request.key)
-        awsS3.deleteObject(input: input) { result in
-            switch(result) {
-            case .success:
+        Task {
+            let input = DeleteObjectInput(bucket: request.bucket, key: request.key)
+            do {
+                _ = try await awsS3.deleteObject(input: input)
                 completion(.success(()))
-            case .failure(let error):
-                completion(.failure(error.storageError))
+            } catch {
+                completion(.failure(StorageError(error: error)))
             }
         }
     }
@@ -47,27 +47,23 @@ class AWSS3Adapter: AWSS3Behavior {
     ///   - request: request identifying bucket and options
     ///   - completion: handle which return a result with list of items
     func listObjectsV2(_ request: AWSS3ListObjectsV2Request, completion: @escaping (Result<StorageListResult, StorageError>) -> Void) {
-        let input = ListObjectsV2Input(bucket: request.bucket,
-                                       continuationToken: request.continuationToken,
-                                       delimiter: request.delimiter,
-                                       maxKeys: request.maxKeys,
-                                       prefix: request.prefix,
-                                       startAfter: request.startAfter)
-        awsS3.listObjectsV2(input: input) { result in
-            switch(result) {
-            case .success(let response):
+        Task {
+            let input = ListObjectsV2Input(bucket: request.bucket,
+                                           continuationToken: request.continuationToken,
+                                           delimiter: request.delimiter,
+                                           maxKeys: request.maxKeys,
+                                           prefix: request.prefix,
+                                           startAfter: request.startAfter)
+            do {
+                let response = try await awsS3.listObjectsV2(input: input)
                 let contents: S3BucketContents = response.contents ?? []
-                do {
-                    let items = try contents.map {
-                        try StorageListResult.Item(s3Object: $0, prefix: request.prefix ?? "")
-                    }
-                    let listResult = StorageListResult(items: items)
-                    completion(.success(listResult))
-                } catch {
-                    completion(.failure(StorageError(error: error)))
+                let items = try contents.map {
+                    try StorageListResult.Item(s3Object: $0, prefix: request.prefix ?? "")
                 }
-            case .failure(let error):
-                completion(.failure(error.storageError))
+                let listResult = StorageListResult(items: items)
+                completion(.success(listResult))
+            } catch {
+                completion(.failure(StorageError(error: error)))
             }
         }
     }
@@ -77,42 +73,42 @@ class AWSS3Adapter: AWSS3Behavior {
     ///   - request: request
     ///   - completion: handler which returns a result with uploadId
     func createMultipartUpload(_ request: CreateMultipartUploadRequest, completion: @escaping (Result<AWSS3CreateMultipartUploadResponse, StorageError>) -> Void) {
-        let input = CreateMultipartUploadInput(bucket: request.bucket,
-                                               cacheControl: request.cacheControl,
-                                               contentDisposition: request.contentDisposition,
-                                               contentEncoding: request.contentEncoding,
-                                               contentLanguage: request.contentLanguage,
-                                               contentType: request.contentType,
-                                               expires: request.expires,
-                                               key: request.key,
-                                               metadata: request.metadata)
-        
-        awsS3.createMultipartUpload(input: input) { result in
-            switch result {
-            case .success(let response):
+        Task {
+            let input = CreateMultipartUploadInput(bucket: request.bucket,
+                                                   cacheControl: request.cacheControl,
+                                                   contentDisposition: request.contentDisposition,
+                                                   contentEncoding: request.contentEncoding,
+                                                   contentLanguage: request.contentLanguage,
+                                                   contentType: request.contentType,
+                                                   expires: request.expires,
+                                                   key: request.key,
+                                                   metadata: request.metadata)
+            
+            do {
+                let response = try await awsS3.createMultipartUpload(input: input)
                 guard let bucket = response.bucket, let key = response.key, let uploadId = response.uploadId else {
                     completion(.failure(StorageError.unknown("Invalid response for creating multipart upload", nil)))
                     return
                 }
                 completion(.success(AWSS3CreateMultipartUploadResponse(bucket: bucket, key: key, uploadId: uploadId)))
-            case .failure(let error):
-                completion(.failure(error.storageError))
+            } catch {
+                completion(.failure(StorageError(error: error)))
             }
         }
     }
-
+    
     func listParts(bucket: String, key: String, uploadId: UploadID, completion: @escaping (Result<AWSS3ListUploadPartResponse, StorageError>) -> Void) {
-        let input = ListPartsInput(bucket: bucket, key: key, uploadId: uploadId)
-        awsS3.listParts(input: input) { result in
-            switch result {
-            case .success(let sdkResponse):
+        Task {
+            let input = ListPartsInput(bucket: bucket, key: key, uploadId: uploadId)
+            do {
+                let sdkResponse = try await awsS3.listParts(input: input)
                 guard let response = AWSS3ListUploadPartResponse(response: sdkResponse) else {
                     completion(.failure(StorageError.unknown("ListParts response is invalid", nil)))
                     return
                 }
                 completion(.success(response))
-            case .failure(let error):
-                completion(.failure(error.storageError))
+            } catch {
+                completion(.failure(StorageError(error: error)))
             }
         }
     }
@@ -122,21 +118,21 @@ class AWSS3Adapter: AWSS3Behavior {
     ///   - request: request which includes uploadId
     ///   - completion: handler which returns a result with object details
     func completeMultipartUpload(_ request: AWSS3CompleteMultipartUploadRequest, completion: @escaping (Result<AWSS3CompleteMultipartUploadResponse, StorageError>) -> Void) {
-        let parts = request.parts.map {
-            S3ClientTypes.CompletedPart(eTag: $0.eTag, partNumber: $0.partNumber)
-        }
-        let completedMultipartUpload = S3ClientTypes.CompletedMultipartUpload(parts: parts)
-        let input = CompleteMultipartUploadInput(bucket: request.bucket, key: request.key, multipartUpload: completedMultipartUpload, uploadId: request.uploadId)
-        awsS3.completeMultipartUpload(input: input) { result in
-            switch result {
-            case .success(let response):
+        Task {
+            let parts = request.parts.map {
+                S3ClientTypes.CompletedPart(eTag: $0.eTag, partNumber: $0.partNumber)
+            }
+            let completedMultipartUpload = S3ClientTypes.CompletedMultipartUpload(parts: parts)
+            let input = CompleteMultipartUploadInput(bucket: request.bucket, key: request.key, multipartUpload: completedMultipartUpload, uploadId: request.uploadId)
+            do {
+                let response = try await awsS3.completeMultipartUpload(input: input)
                 guard let eTag = response.eTag else {
                     completion(.failure(StorageError.unknown("Invalid response for completing multipart upload", nil)))
                     return
                 }
                 completion(.success(AWSS3CompleteMultipartUploadResponse(bucket: request.bucket, key: request.key, eTag: eTag)))
-            case .failure(let error):
-                completion(.failure(error.storageError))
+            } catch {
+                completion(.failure(StorageError(error: error)))
             }
         }
     }
@@ -146,13 +142,13 @@ class AWSS3Adapter: AWSS3Behavior {
     ///   - request: request which includes uploadId
     ///   - completion: handler which indicates when the operation is done
     func abortMultipartUpload(_ request: AWSS3AbortMultipartUploadRequest, completion: @escaping (Result<Void, StorageError>) -> Void) {
-        let input = AbortMultipartUploadInput(bucket: request.bucket, key: request.key, uploadId: request.uploadId)
-        awsS3.abortMultipartUpload(input: input) { result in
-            switch result {
-            case .success:
+        Task {
+            let input = AbortMultipartUploadInput(bucket: request.bucket, key: request.key, uploadId: request.uploadId)
+            do {
+                _ = try await awsS3.abortMultipartUpload(input: input)
                 completion(.success(()))
-            case .failure(let error):
-                completion(.failure(error.storageError))
+            } catch {
+                completion(.failure(StorageError(error: error)))
             }
         }
     }
