d2e172d5294dc1aa6e56b9188c3437fe4d9b7ee8
Jithin Roy <51138777+royjit@users.noreply.github.com>
2022-06-01 13:07:15 -0700

    Update AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Operations/AuthConfigureOperation.swift
    Co-authored-by: Harshdeep Singh <6162866+harsh62@users.noreply.github.com>

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Operations/AuthConfigureOperation.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Operations/AuthConfigureOperation.swift
@@ -1,8 +1,8 @@
 //
-//  File.swift
-//  
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
 //
-//  Created by Roy, Jithin on 5/25/22.
+// SPDX-License-Identifier: Apache-2.0
 //
 
 import Foundation
