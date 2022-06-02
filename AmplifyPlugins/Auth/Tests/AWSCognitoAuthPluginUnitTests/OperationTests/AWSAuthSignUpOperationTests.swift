35d46ae7ccdcd65f9261305c31e3ae3c756e5790
Jithin Roy <51138777+royjit@users.noreply.github.com>
2022-06-02 14:30:49 -0700

    chore: Cleanup operations to stop relying on configuration state (#1833)

--- AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/OperationTests/AWSAuthSignUpOperationTests.swift
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/OperationTests/AWSAuthSignUpOperationTests.swift
@@ -68,7 +68,11 @@ class AWSAuthSignUpOperationTests: XCTestCase {
         wait(for: [exp, functionExpectation], timeout: 1)
         
     }
-    
+
+    /// Given: Configured AuthState machine
+    /// When: A new SignUp operation is added to the queue and mock a service failure
+    /// Then: Should complete the signUp flow with an error
+    ///
     func testSignUpOperationFailure() throws {
         let exp = expectation(description: #function)
         let functionExpectation = expectation(description: "API call should be invoked")
@@ -90,6 +94,42 @@ class AWSAuthSignUpOperationTests: XCTestCase {
                 XCTFail("Should not produce success response")
             case .failure(let error):
                 print(error)
+            }
+            exp.fulfill()
+        }
+        queue?.addOperation(operation)
+        wait(for: [exp, functionExpectation], timeout: 1)
+    }
+
+    /// Given: Configured AuthState machine with existing signUp flow
+    /// When: A new SignUp operation is added to the queue
+    /// Then: Should cancel the existing signUp flow and start a new flow and complete
+    ///
+    func testCancelExistingSignUp() throws {
+        Amplify.Logging.logLevel = .verbose
+        let exp = expectation(description: #function)
+        let functionExpectation = expectation(description: "API call should be invoked")
+        let signUp: MockIdentityProvider.MockSignUpResponse = { _ in
+            functionExpectation.fulfill()
+            return .init(codeDeliveryDetails: nil, userConfirmed: true, userSub: UUID().uuidString)
+        }
+
+        let request = AuthSignUpRequest(username: "jeffb",
+                                        password: "Valid&99",
+                                        options: AuthSignUpRequest.Options())
+
+        let initialState = AuthState.configured(
+            .signingUp(Defaults.makeDefaultAuthConfigData(), .notStarted),
+            .configured)
+        let statemachine = Defaults.makeDefaultAuthStateMachine(
+            initialState: initialState,
+            userPoolFactory: {MockIdentityProvider(mockSignUpResponse: signUp)})
+        let operation = AWSAuthSignUpOperation(request, stateMachine: statemachine) {  result in
+            switch result {
+            case .success(let signUpResult):
+                print("Sign Up Result: \(signUpResult)")
+            case .failure(let error):
+                XCTAssertNil(error, "Error should not be returned")
             }
             exp.fulfill()
         }
