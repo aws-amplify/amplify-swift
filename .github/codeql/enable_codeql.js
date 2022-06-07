92e9ce52df1a1903ca17ca6de925c57a6cf00e8c
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-06-07 10:35:00 -0600

    chore: enable code scanning (#1871)

---
+++ .github/codeql/enable_codeql.js
@@ -0,0 +1,5 @@
+/* 
+GitHub CodeQL scanning is used to scan for errors in GitHub Actions workflows (.yml), 
+but currently requires a non-empty JavaScript file to work.
+*/
+const why = 'This is needed to run CodeQL on GitHub Actions files.';
