520df6d8e1784a39b3c313e346454454d4dfdefa
Diego Costantino <diegoco@amazon.com>
2022-06-09 18:25:00 -0700

    feat: intercept remote notification + didFinishLaunchingWithOptions

--- AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Notifications/AWSPinpointAnalyticsNotifications.swift
+++ AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Notifications/AWSPinpointAnalyticsNotifications.swift
@@ -6,15 +6,12 @@
 //
 
 import Foundation
-import Amplify
 #if canImport(UIKit)
 import UIKit
 #endif
+import Amplify
 
-class AWSPinpointAnalyticsNotifications: AWSPinpointAnalyticsNotificationsBehavior {
-    private static let AWSDataKey = "data"
-    private static let AWSPinpointKey = "pinpoint"
-    
+public class AWSPinpointAnalyticsNotifications: AWSPinpointAnalyticsNotificationsBehavior {
     private var previousEventSource: EventSource = .unknown
     private let context: PinpointContext
     
@@ -22,77 +19,145 @@ class AWSPinpointAnalyticsNotifications: AWSPinpointAnalyticsNotificationsBehavi
         self.context = context
     }
     
-    func interceptDidFinishLaunchingWithOptions(launchOptions: LaunchOptions) -> Bool {
+    // MARK: - Public APIs
+    public func interceptDidFinishLaunchingWithOptions(launchOptions: LaunchOptions) -> Bool {
         guard let notificationPayload = remoteNotificationPayload(fromLaunchOptions: launchOptions),
               isValidPinpointNotification(payload: notificationPayload) else {
             return true
         }
         
         let (eventSource, pinpointMetadata) = pinpointMetadata(fromPayload: notificationPayload)
-        
-        // TODO: record launch because of notification
+        Task {
+            await self.addGlobalEventMetadata(eventMetadata: pinpointMetadata, eventSource: eventSource)
+            recordEventForNotification(eventSource: eventSource,
+                                       pushEvent: .opened,
+                                       pushAction: .openedNotification)
+        }
         
         return true
     }
     
-    func interceptDidRegisterForRemoteNotificationsWithDeviceToken(deviceToken: Data) {
+    public func interceptDidRegisterForRemoteNotificationsWithDeviceToken(deviceToken: Data) {
+        let currentToken = self.context.userDefaults.data(forKey: PinpointContext.Constants.Notifications.deviceTokenKey)
+        guard currentToken != deviceToken else {
+            return
+        }
         
+        self.context.userDefaults.save(deviceToken, forKey: PinpointContext.Constants.Notifications.deviceTokenKey)
+        Task {
+            do {
+                try await self.context.targetingClient.updateEndpointProfile()
+            } catch {
+                log.error("Failed updating endpoint profile with error: \(error)")
+            }
+        }
     }
     
-    func interceptDidReceiveRemoteNotification(userInfo: UserInfo, pushEvent: AWSPinpointPushEvent) {
+    public func interceptDidReceiveRemoteNotification(userInfo: UserInfo,
+                                                      pushEvent: AWSPinpointPushEvent = .received,
+                                                      shouldHandleNotificationDeepLink: Bool) {
+        let (eventSource, metadata) = pinpointMetadata(fromPayload: userInfo)
+        let pushAction = makePinpointPushAction(fromEvent: pushEvent)
         
-    }
-    
-    func interceptDidReceiveRemoteNotification(userInfo: UserInfo, pushEvent: AWSPinpointPushEvent, shouldHandleNotificationDeepLink: Bool) {
+        switch pushAction {
+        case .openedNotification:
+            log.verbose("App launched from received notification.")
+            Task {
+                await self.addGlobalEventMetadata(eventMetadata: metadata, eventSource: eventSource)
+                self.recordEventForNotification(eventSource: eventSource, pushEvent: .opened, pushAction: pushAction)
+                
+                if shouldHandleNotificationDeepLink {
+                    self.handleDeepLinkForNotification(userInfo: userInfo)
+                }
+            }
+            
+        case .receivedBackground:
+            log.verbose("Received notification with app in background.")
+            Task {
+                await self.addGlobalEventMetadata(eventMetadata: metadata, eventSource: eventSource)
+                self.recordEventForNotification(eventSource: eventSource, pushEvent: .received, pushAction: pushAction)
+            }
         
+        case .receivedForeground:
+            log.verbose("Received notification with app in foreground.")
+            
+            // Not adding global event source metadata because if the app session is already running,
+            // the session should not contribute to the new push notification that is being received
+            self.recordEventForNotification(eventSource: eventSource, pushEvent: .received, pushAction: pushAction)
+        
+        case .unknown:
+            log.verbose("Received notification with app in unknown state.")
+        }
     }
     
     
-    private func isValidPinpointNotification(payload: NotificationMetadata) -> Bool {
-        pinpointPayloadFromNotificationPayload(notification: payload) != nil
-    }
     
-    private func pinpointPayloadFromNotificationPayload(notification: NotificationMetadata) -> [String: Any]? {
-        guard let dataPayload = notification[Self.AWSDataKey] as? [String: Any],
-              let pinpointMetadata = dataPayload[Self.AWSPinpointKey] as? [String: Any] else {
+    private func pinpointPayloadFromNotificationPayload(notification: UserInfo) -> [String: Any]? {
+        guard let dataPayload = notification[PinpointContext.Constants.Notifications.dataKey] as? [String: Any],
+              let pinpointMetadata = dataPayload[PinpointContext.Constants.Notifications.pinpointKey] as? [String: Any] else {
             return nil
         }
         return pinpointMetadata
     }
     
-    private func pinpointMetadata(fromPayload payload: NotificationMetadata) -> (EventSource, NotificationMetadata?) {
-        var metadata: (EventSource, NotificationMetadata?) = (.unknown, nil)
-        
-        guard let pinpointPayload = pinpointPayloadFromNotificationPayload(notification: payload) else {
-            return metadata
+    private func recordEventForNotification(eventSource: EventSource,
+                                            pushEvent: AWSPinpointPushEvent,
+                                            pushAction: AWSPinpointPushAction,
+                                            withIdentifier identifier: String? = nil) {
+        guard let pushNotificationEvent = PinpointEvent.makeEvent(eventSource: eventSource,
+                                                                  pushAction: pushAction,
+                                                                  usingClient: self.context.analyticsClient) else {
+            log.error("Invalid Pinpoint push notification event.")
+            return
         }
         
-        if let campaignMetadata = pinpointPayload[EventSource.campaign.rawValue] as? NotificationMetadata {
-            metadata = (.campaign, campaignMetadata)
-            self.log.verbose("Found Pinpoint campaign with attributes: \(campaignMetadata)")
-        
-        } else if let journeyMetadata = pinpointPayload[EventSource.journey.rawValue] as? NotificationMetadata {
-            metadata = (.journey, journeyMetadata)
-            self.log.verbose("Found Pinpoint journey with attributes: \(journeyMetadata)")
+        if let identifier = identifier {
+            pushNotificationEvent.addAttribute(identifier, forKey: PinpointContext.Constants.Notifications.actionIdentifierKey)
         }
         
-        if metadata.1 == nil {
-            fatalError("Pinpoint push payload not found")
-        }
-        
-        return metadata
     }
     
-    private func remoteNotificationPayload(fromLaunchOptions launchOptions: LaunchOptions) -> NotificationMetadata? {
+    private func remoteNotificationPayload(fromLaunchOptions launchOptions: LaunchOptions) -> UserInfo? {
 #if canImport(UIKit)
-        return launchOptions[UIApplication.LaunchOptionsKey.remoteNotification] as? NotificationMetadata
+        return launchOptions[UIApplication.LaunchOptionsKey.remoteNotification] as? UserInfo
 #else
         return nil
 #endif
     }
     
-    private func addGlobalEventMetadata(eventMetadata: AWSPinpointAnalyticsNotifications.NotificationMetadata?,
-                                eventSource: AWSPinpointAnalyticsNotifications.EventSource) {
+}
+
+// MARK: - AWSPinpointNotifications + metadata helpers
+extension AWSPinpointAnalyticsNotifications {
+    private func isValidPinpointNotification(payload: UserInfo) -> Bool {
+        pinpointPayloadFromNotificationPayload(notification: payload) != nil
+    }
+    
+    private func pinpointMetadata(fromPayload payload: UserInfo) -> (EventSource, UserInfo?) {
+        var metadata: (EventSource, UserInfo?) = (.unknown, nil)
+        
+        guard let pinpointPayload = pinpointPayloadFromNotificationPayload(notification: payload) else {
+            return metadata
+        }
+        
+        if let campaignMetadata = pinpointPayload[EventSource.campaign.rawValue] as? UserInfo {
+            metadata = (.campaign, campaignMetadata)
+            self.log.verbose("Found Pinpoint campaign with attributes: \(campaignMetadata)")
+        
+        } else if let journeyMetadata = pinpointPayload[EventSource.journey.rawValue] as? UserInfo {
+            metadata = (.journey, journeyMetadata)
+            self.log.verbose("Found Pinpoint journey with attributes: \(journeyMetadata)")
+        }
+        
+        if metadata.1 == nil {
+            fatalError("Pinpoint push notification payload not found.")
+        }
+        
+        return metadata
+    }
+    
+    private func addGlobalEventMetadata(eventMetadata: UserInfo?,
+                                        eventSource: EventSource) async {
         guard eventSource != previousEventSource,
               let pinpointMetadata = eventMetadata,
               !pinpointMetadata.isEmpty else {
@@ -105,13 +170,61 @@ class AWSPinpointAnalyticsNotifications: AWSPinpointAnalyticsNotificationsBehavi
         // [self.context.analyticsClient setEventSourceAttributes:metadata];
         
         for (key, value) in pinpointMetadata {
-            self.context.analyticsClient.addGlobalAttribute(value, forKey: key)
+            guard let value = value as? String, let key = key as? String else {
+                log.debug("Skipping metadata with key \(key) because has value of type \(type(of: value)).")
+                continue
+            }
+            await self.context.analyticsClient.addGlobalAttribute(value, forKey: key)
         }
         
     }
 }
 
-// MARK: - AWSPinpointNotifications + EventSourceType
+// MARK - AWSPinpointAnalyticsNotifications + handleDeepLink
+extension AWSPinpointAnalyticsNotifications {
+    func handleDeepLinkForNotification(userInfo: UserInfo) {
+        guard let payload = pinpointPayloadFromNotificationPayload(notification: userInfo) as? [String: String],
+              let deepLink = payload[PinpointContext.Constants.Notifications.deeplinkKey],
+              let deepLinkURL = URL(string: deepLink) else {
+            return
+        }
+        
+#if canImport(UIKit)
+        if UIApplication.shared.canOpenURL(deepLinkURL) {
+            DispatchQueue.main.async {
+                UIApplication.shared.open(deepLinkURL)
+            }
+        }
+#endif
+        
+    }
+}
+
+// MARK: - AWSPinpointAnalyticsNotifications + AWSPinpointPushAction
+extension AWSPinpointAnalyticsNotifications {
+    func makePinpointPushAction(fromEvent pushEvent: AWSPinpointPushEvent) -> AWSPinpointPushAction {
+        var pushActionType: AWSPinpointPushAction
+
+#if canImport(UIKit)
+        let appState = UIApplication.shared.applicationState
+        switch appState {
+        case .active:
+            pushActionType = pushEvent == .received ? .receivedForeground : .openedNotification
+        case .inactive:
+            pushActionType = .openedNotification
+        case .background:
+            pushActionType = .receivedBackground
+        @unknown default:
+            pushActionType = .unknown
+        }
+#else
+        pushActionType = .unknown
+#endif
+        return pushActionType
+    }
+}
+
+// MARK: - AWSPinpointAnalyticsNotifications + EventSourceType
 extension AWSPinpointAnalyticsNotifications {
     enum EventSource: String {
         case campaign
@@ -122,4 +235,43 @@ extension AWSPinpointAnalyticsNotifications {
 
 // MARK: - AWSPinpointAnalyticsNotifications + DefaultLogger
 extension AWSPinpointAnalyticsNotifications: DefaultLogger {}
+
+
+// MARK: - PinpointEvent + makeWithActionType
+extension PinpointEvent {
+    static func makeEvent(eventSource: AWSPinpointAnalyticsNotifications.EventSource,
+                          pushAction: AWSPinpointPushAction,
+                          usingClient analyticClient: AnalyticsClient) -> PinpointEvent? {
+        
+        guard pushAction != .unknown else {
+            return nil
+        }
+        
+        let eventPrefix = eventSource.rawValue
+        let eventSuffix = pushAction.rawValue
+        let eventType = "_\(eventPrefix).\(eventSuffix)"
+        
+        return analyticClient.createEvent(withEventType: eventType)
+    }
+}
+
+
+// MARK: - Constants + notifications
+fileprivate extension PinpointContext.Constants {
+    enum Notifications {
+        static let actionIdentifierKey = "actionIdentifier"
+        static let deviceTokenKey = "com.amazonaws.AWSDeviceTokenKey"
+        static let dataKey = "data"
+        static let pinpointKey = "pinpoint"
+        static let deeplinkKey = "deeplink"
+    }
+}
+
+// MARK: - AWSPinpointPushAction
+enum AWSPinpointPushAction: String {
+    case openedNotification = "opened_notification"
+    case receivedForeground = "received_foreground"
+    case receivedBackground = "received_background"
+    case unknown
+}
 
--- AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Notifications/AWSPinpointAnalyticsNotificationsBehavior.swift
+++ AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin/Notifications/AWSPinpointAnalyticsNotificationsBehavior.swift
@@ -11,11 +11,8 @@ import Foundation
 import UIKit
 #endif
 
-protocol AWSPinpointAnalyticsNotificationsBehavior {
+public protocol AWSPinpointAnalyticsNotificationsBehavior {
     typealias UserInfo = [AnyHashable: Any]
-    
-    // TODO: review this type
-    typealias NotificationMetadata = [String: Any]
     
     func interceptDidFinishLaunchingWithOptions(launchOptions: LaunchOptions) -> Bool
     
@@ -25,22 +22,6 @@ protocol AWSPinpointAnalyticsNotificationsBehavior {
     /// The Pinpoint targeting client must intercept this callback in order to report campaign analytics correctly.
     /// - Parameter deviceToken: A token that identifies the device to APNs.
     func interceptDidRegisterForRemoteNotificationsWithDeviceToken(deviceToken: Data)
-    
-    
-    /// For iOS 9 and below, invoke this method from the `application(_:didReceiveRemoteNotification:fetchCompletionHandler:)`
-    /// application delegate method.
-
-    /// For iOS 10 and above, invoke this method from the `userNotificationCenter(_:willPresent:withCompletionHandler:)`
-    /// and `userNotificationCenter(_:didReceive:withCompletionHandler:)` UserNotificationCenter methods. When invoking this method from `willPresent`, pass in `notification.request.content.userInfo` as userInfo.
-    /// When invoking this method on `didReceive`, pass in `response.notification.request.content.userInfo` as `userInfo`.
-    ///
-    /// The Pinpoint targeting client must intercept this callback in order to report campaign analytics correctly.
-    ///
-    /// - Parameter userInfo: A dictionary that contains information related to the remote notification, potentially including a badge number for the app icon, an alert sound, an alert message to display to the user, a notification identifier, and custom data.
-    /// The provider originates it as a JSON-defined dictionary that the system converts to an `Dictionary`.
-    ///
-    /// - Parameter pushEvent: Event for the push notification which is either `received` or `opened`.
-    func interceptDidReceiveRemoteNotification(userInfo: UserInfo, pushEvent: AWSPinpointPushEvent)
     
     
     /// For iOS 9 and below, intercept the `application(_:didReceiveRemoteNotification:fetchCompletionHandler:)` application
@@ -57,7 +38,7 @@ protocol AWSPinpointAnalyticsNotificationsBehavior {
     ///
     /// - Parameter userInfo: A dictionary that contains information related to the remote notification, potentially including a badge number for the app icon, an alert sound, an alert message to display to the user, a notification identifier, and custom data.
     /// The provider originates it as a JSON-defined dictionary that the system converts to an `Dictionary`.
-    /// - Parameter pushEvent: Event for the push notification which is either `received` or `opened`.
+    /// - Parameter pushEvent: Event for the push notification which is either `received` or `opened`. Defaults to `received`.
     /// - Parameter shouldHandleNotificationDeepLink: Whether or not notification manager should attempt to open the remote notification deeplink, if present.
     func interceptDidReceiveRemoteNotification(userInfo: UserInfo,
                                                pushEvent: AWSPinpointPushEvent,
@@ -65,7 +46,7 @@ protocol AWSPinpointAnalyticsNotificationsBehavior {
 }
 
 // MARK: - AWSPinpointNotifications + LaunchOptions
-extension AWSPinpointAnalyticsNotificationsBehavior {
+public extension AWSPinpointAnalyticsNotificationsBehavior {
 #if canImport(UIKit)
     typealias LaunchOptions = [UIApplication.LaunchOptionsKey: Any]
 #else
@@ -73,11 +54,10 @@ extension AWSPinpointAnalyticsNotificationsBehavior {
 #endif
 }
 
-enum AWSPinpointPushEvent {
+public enum AWSPinpointPushEvent {
     case opened
     case received
 }
-
 
 extension AWSPinpointAnalyticsClientBehavior {
     public var areNotificationsEnabled: Bool {
