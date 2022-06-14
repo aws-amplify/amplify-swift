63769b2921354bad25111f980f9353bd4e5a04c8
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2022-06-14 10:13:40 -0400

    feat(Auth): Adding FetchUserAttribute API implementation

--- AmplifyPlugins/Auth/Tests/AuthHostApp/AuthIntegrationTests/AuthUserAttributesTests/AuthUserAttributesTests.swift
+++ AmplifyPlugins/Auth/Tests/AuthHostApp/AuthIntegrationTests/AuthUserAttributesTests/AuthUserAttributesTests.swift
@@ -35,6 +35,49 @@ class AuthUserAttributesTests: AWSAuthBaseTest {
     /// - Then:
     ///    - The request should be successful and the email specified should receive a confirmation code
     ///
+    func testSuccessfulFetchAttribute() throws {
+        let username = "integTest\(UUID().uuidString)"
+        let password = "P123@\(UUID().uuidString)"
+
+        let signInExpectation = expectation(description: "SignIn operation should complete")
+        AuthSignInHelper.registerAndSignInUser(username: username,
+                                               password: password,
+                                               email: defaultTestEmail) { didSucceed, error in
+            signInExpectation.fulfill()
+            XCTAssertTrue(didSucceed, "SignIn operation failed - \(String(describing: error))")
+        }
+        wait(for: [signInExpectation], timeout: networkTimeout)
+
+        let fetchUserAttributeExpectation = expectation(description: "Fetch User Attribute operation should complete")
+        
+        _ = Amplify.Auth.fetchUserAttributes(listener: { result in
+            switch result {
+            case .success(let attributes):
+                if let emailAttribute = attributes.filter({ $0.key == .email }).first {
+                    XCTAssertEqual(emailAttribute.value, self.defaultTestEmail)
+                } else {
+                    XCTFail("Email attribute not found")
+                }
+                fetchUserAttributeExpectation.fulfill()
+            case .failure(let error):
+                XCTFail("Failed to fetch user attribute with \(error)")
+            }
+        })
+        wait(for: [fetchUserAttributeExpectation], timeout: networkTimeout)
+    }
+    
+    /// Test updating the user's email attribute.
+    /// Internally, Cognito's `UpdateUserAttributes` API will be called with metadata as clientMetadata.
+    /// The configured lambda trigger will invoke the custom message lambda with the client metadata payload. See
+    /// https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_UpdateUserAttributes.html
+    /// for more details.
+    ///
+    /// - Given: A confirmed user
+    /// - When:
+    ///    - I invoke Amplify.Auth.update with email attribute
+    /// - Then:
+    ///    - The request should be successful and the email specified should receive a confirmation code
+    ///
     func testSuccessfulUpdateEmailAttribute() throws {
         let username = "integTest\(UUID().uuidString)"
         let password = "P123@\(UUID().uuidString)"
