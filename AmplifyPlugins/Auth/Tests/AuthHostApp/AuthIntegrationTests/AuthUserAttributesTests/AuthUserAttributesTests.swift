5d3a158dc7f8913bcfb243e558a431614a125e78
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2022-06-15 12:42:53 -0400

    feat(Auth): Adding UpdateUserAttributes API implementation

--- AmplifyPlugins/Auth/Tests/AuthHostApp/AuthIntegrationTests/AuthUserAttributesTests/AuthUserAttributesTests.swift
+++ AmplifyPlugins/Auth/Tests/AuthHostApp/AuthIntegrationTests/AuthUserAttributesTests/AuthUserAttributesTests.swift
@@ -118,6 +118,76 @@ class AuthUserAttributesTests: AWSAuthBaseTest {
         })
         wait(for: [fetchUserAttributeExpectation], timeout: networkTimeout)
     }
+    
+    
+    /// Test updating the user's email attribute.
+    /// Internally, Cognito's `UpdateUserAttributes` API will be called with metadata as clientMetadata.
+    /// The configured lambda trigger will invoke the custom message lambda with the client metadata payload. See
+    /// https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_UpdateUserAttributes.html
+    /// for more details.
+    ///
+    /// - Given: A confirmed user
+    /// - When:
+    ///    - I invoke Amplify.Auth.update with email attribute
+    /// - Then:
+    ///    - The request should be successful and the email specified should receive a confirmation code
+    ///
+    func testSuccessfulUpdateOfMultipleAttributes() throws {
+        let username = "integTest\(UUID().uuidString)"
+        let password = "P123@\(UUID().uuidString)"
+        let updatedEmail = "\(username)@amazon.com"
+        let updatedName = "Name\(UUID().uuidString)"
+
+        let signInExpectation = expectation(description: "SignIn operation should complete")
+        AuthSignInHelper.registerAndSignInUser(username: username,
+                                               password: password,
+                                               email: defaultTestEmail) { didSucceed, error in
+            signInExpectation.fulfill()
+            XCTAssertTrue(didSucceed, "SignIn operation failed - \(String(describing: error))")
+        }
+        wait(for: [signInExpectation], timeout: networkTimeout)
+
+        let updateExpectation = expectation(description: "Update operation should complete")
+        let pluginOptions = AWSUpdateUserAttributesOptions(metadata: ["mydata": "myvalue"])
+        let options = AuthUpdateUserAttributesRequest.Options(pluginOptions: pluginOptions)
+        let attributes = [
+            AuthUserAttribute(.email, value: updatedEmail),
+            AuthUserAttribute(.name, value: updatedName)
+        ]
+        _ = Amplify.Auth.update(
+            userAttributes: attributes, options: options) { result in
+            switch result {
+            case .success:
+                updateExpectation.fulfill()
+            case .failure(let error):
+                XCTFail("Failed to update user attribute with \(error)")
+            }
+        }
+        wait(for: [updateExpectation], timeout: networkTimeout)
+        
+        let fetchUserAttributeExpectation = expectation(description: "Fetch User Attribute operation should complete")
+        
+        _ = Amplify.Auth.fetchUserAttributes(listener: { result in
+            switch result {
+            case .success(let attributes):
+                if let emailAttribute = attributes.filter({ $0.key == .email }).first {
+                    XCTAssertEqual(emailAttribute.value, updatedEmail)
+                } else {
+                    XCTFail("Email attribute not found")
+                }
+                
+                if let emailAttribute = attributes.filter({ $0.key == .name }).first {
+                    XCTAssertEqual(emailAttribute.value, updatedName)
+                } else {
+                    XCTFail("Email attribute not found")
+                }
+                fetchUserAttributeExpectation.fulfill()
+            case .failure(let error):
+                XCTFail("Failed to fetch user attribute with \(error)")
+            }
+        })
+        wait(for: [fetchUserAttributeExpectation], timeout: networkTimeout)
+    }
 
     /// Test resending code for the user's updated email attribute.
     /// Internally, Cognito's `GetUserAttributeVerificationCode` API will be called with metadata as clientMetadata.
