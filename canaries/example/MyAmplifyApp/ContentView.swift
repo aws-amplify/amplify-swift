6dc6e1f3df322d172b5ca8015cc37c2bba2532d7
yeung-wah <94481720+yeung-wah@users.noreply.github.com>
2022-06-20 15:18:25 -0400

    chore(ci): added getting started canary workflow (#1884)

---
+++ canaries/example/MyAmplifyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAmplifyApp
+//
+//  Created by Tsui, Yeung Wah on 4/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
