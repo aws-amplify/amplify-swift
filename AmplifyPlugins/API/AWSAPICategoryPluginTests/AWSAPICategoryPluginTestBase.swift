d1d19e13faa7d18d9bc1d2cfd2792df4f1dd8c70
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-06-27 08:02:00 -0600

    fix(api): fix race conditions in API tests (#1962)

--- AmplifyPlugins/API/AWSAPICategoryPluginTests/AWSAPICategoryPluginTestBase.swift
+++ AmplifyPlugins/API/AWSAPICategoryPluginTests/AWSAPICategoryPluginTestBase.swift
@@ -64,9 +64,7 @@ class AWSAPICategoryPluginTestBase: XCTestCase {
         }
 
         await Amplify.reset()
-        // await Amplify.reset() doesn't immediately stop all in-progress tasks, so we sleep here to
-        // give everything a chance to complete. This prevents sporadic failures when running many tests.
-        sleep(2)
+
         let config = AmplifyConfiguration()
         do {
             try Amplify.configure(config)
