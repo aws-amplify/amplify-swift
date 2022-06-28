9b901bea3692cbf4dddd9520ef3c7fe27207cd26
yeung-wah <94481720+yeung-wah@users.noreply.github.com>
2022-06-28 11:41:52 -0400

    chore: Added license headers to canary sample app (#1966)

--- canaries/example/MyAmplifyAppUITests/MyAmplifyAppUITests.swift
+++ canaries/example/MyAmplifyAppUITests/MyAmplifyAppUITests.swift
@@ -1,8 +1,8 @@
 //
-//  MyAmplifyAppUITests.swift
-//  MyAmplifyAppUITests
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
 //
-//  Created by Tsui, Yeung Wah on 4/12/22.
+// SPDX-License-Identifier: Apache-2.0
 //
 
 import XCTest
