96afb12ee09a4eb521e3c64d4624d81dda03f60d
Tuan Pham <phantum@amazon.com>
2022-07-19 10:02:02 -0400

    chore(Analytics): revert new analytics host app

---
+++ AmplifyPlugins/Analytics/HostApp/Source/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene
+        // `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see
+        // `application:configurationForConnectingSceneSession` instead).
+        guard scene is UIWindowScene else { return }
+    }
+}
---
+++ AmplifyPlugins/Analytics/HostApp/Source/ViewController.swift
@@ -0,0 +1,15 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
