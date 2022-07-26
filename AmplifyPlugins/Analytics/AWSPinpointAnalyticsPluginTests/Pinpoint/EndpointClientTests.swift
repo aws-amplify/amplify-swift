8bf1df66e58ad6febd8240d689d6738e995ecd78
Tuan Pham <103537251+phantumcode@users.noreply.github.com>
2022-07-26 12:51:34 -0500

    chore(Analytics): migrate key-value Userdefaults to Keychain (#2021)
    * chore(Analytics): migrate key-value Userdefaults to Keychain
    
    * chore(Analytics): fix unit tests
    
    * chore(Analytics): add key-value  migration unit tests
    
    * chore(Analytics): revert new analytics host app
    
    * chore(Analytics): update migration logic to be a static method
    
    * chore(Analytics): updates to resolve review comments
    
    * chore(Analytics): add fallback logic for error handling
    
    * chore(Analytics): specify keychain in PinpointContext constructor
    
    * chore(Analytics): updates per review suggestions
    
    * chore(Analytics): fix line length error
    
    * chore(Analytics): fix line length error
    
    * chore(Analytics): fix line length error

--- AmplifyPlugins/Analytics/AWSPinpointAnalyticsPluginTests/Pinpoint/EndpointClientTests.swift
+++ AmplifyPlugins/Analytics/AWSPinpointAnalyticsPluginTests/Pinpoint/EndpointClientTests.swift
@@ -17,12 +17,13 @@ class EndpointClientTests: XCTestCase {
     private var device: MockDevice!
     private var currentApplicationId = "applicationId"
     private var currentEndpointId = "endpointId"
-
+    private var keychain: MockKeychainStore!
     override func setUp() {
         archiver = MockArchiver()
         userDefaults = MockUserDefaults()
         pinpointClient = MockPinpointClient()
         device = MockDevice()
+        keychain = MockKeychainStore()
         endpointClient = EndpointClient(configuration: .init(appId: currentApplicationId,
                                                              uniqueDeviceId: currentEndpointId,
                                                              isDebug: false,
@@ -30,7 +31,8 @@ class EndpointClientTests: XCTestCase {
                                         pinpointClient: pinpointClient,
                                         archiver: archiver,
                                         currentDevice: device,
-                                        userDefaults: userDefaults)
+                                        userDefaults: userDefaults,
+                                        keychain: keychain)
     }
 
     override func tearDown() {
@@ -39,6 +41,7 @@ class EndpointClientTests: XCTestCase {
         pinpointClient = nil
         device = nil
         endpointClient = nil
+        keychain = nil
     }
 
     func testCurrentEndpointProfile_withValidStoredProfile_shouldReturnUpdatedStored() async {
@@ -49,9 +52,14 @@ class EndpointClientTests: XCTestCase {
                                                             deviceToken: "oldToken",
                                                             effectiveDate: oldEffectiveDate,
                                                             demographic: oldDemographic)
-        userDefaults.addMockValue(Data(), forKey: EndpointClient.Constants.endpointProfileKey)
         let newToken = "newToken".data(using: .utf8)
-        userDefaults.addMockValue(newToken, forKey: EndpointClient.Constants.deviceTokenKey)
+        do {
+            try keychain.set(Data(), key: EndpointClient.Constants.endpointProfileKey)
+            try keychain.set(newToken!, key: EndpointClient.Constants.deviceTokenKey)
+        } catch {
+            XCTFail("Fail to setup test data")
+        }
+
         archiver.decoded = storedEndpointProfile
 
         let endpointProfile = await endpointClient.currentEndpointProfile()
@@ -59,6 +67,9 @@ class EndpointClientTests: XCTestCase {
         XCTAssertEqual(userDefaults.dataForKeyCount, 2)
         XCTAssertEqual(userDefaults.dataForKeyCountMap[EndpointClient.Constants.endpointProfileKey], 1)
         XCTAssertEqual(userDefaults.dataForKeyCountMap[EndpointClient.Constants.deviceTokenKey], 1)
+        XCTAssertEqual(keychain.dataValues.count, 2)
+        XCTAssertNotNil(keychain.dataValues[EndpointClient.Constants.endpointProfileKey])
+        XCTAssertNotNil(keychain.dataValues[EndpointClient.Constants.deviceTokenKey])
         XCTAssertEqual(archiver.decodeCount, 1)
         XCTAssertTrue(endpointProfile === storedEndpointProfile, "Expected stored PinpointEndpointProfile object")
         XCTAssertEqual(endpointProfile.applicationId, currentApplicationId)
@@ -80,17 +91,21 @@ class EndpointClientTests: XCTestCase {
                                                             deviceToken: "oldToken",
                                                             effectiveDate: oldEffectiveDate,
                                                             demographic: oldDemographic)
+        keychain.resetCounters()
         let newToken = "newToken".data(using: .utf8)
-        userDefaults.addMockValue(newToken, forKey: EndpointClient.Constants.deviceTokenKey)
+        do {
+            try keychain.set(newToken!, key: EndpointClient.Constants.deviceTokenKey)
+        } catch {
+            XCTFail("Fail to setup test data")
+        }
         archiver.decoded = storedEndpointProfile
 
         let endpointProfile = await endpointClient.currentEndpointProfile()
 
-        XCTAssertEqual(userDefaults.dataForKeyCount, 2)
-        XCTAssertEqual(userDefaults.dataForKeyCountMap[EndpointClient.Constants.endpointProfileKey], 1)
-        XCTAssertEqual(userDefaults.dataForKeyCountMap[EndpointClient.Constants.deviceTokenKey], 1)
+        XCTAssertEqual(keychain.dataForKeyCount, 2)
+        XCTAssertEqual(keychain.dataForKeyCountMap[EndpointClient.Constants.endpointProfileKey], 1)
+        XCTAssertEqual(keychain.dataForKeyCountMap[EndpointClient.Constants.deviceTokenKey], 2)
         XCTAssertEqual(archiver.decodeCount, 0)
-        XCTAssertEqual(userDefaults.removeObjectCount, 1)
         XCTAssertFalse(endpointProfile === storedEndpointProfile, "Expected new PinpointEndpointProfile object")
         XCTAssertEqual(endpointProfile.applicationId, currentApplicationId)
         XCTAssertEqual(endpointProfile.endpointId, currentEndpointId)
@@ -123,14 +138,16 @@ class EndpointClientTests: XCTestCase {
     }
 
     func testUpdateEndpointProfile_shouldSendUpdateRequestAndSave() async {
+        keychain.resetCounters()
         try? await endpointClient.updateEndpointProfile()
 
         XCTAssertEqual(pinpointClient.updateEndpointCount, 1)
         XCTAssertEqual(archiver.encodeCount, 1)
-        XCTAssertEqual(userDefaults.saveCount, 1)
+        XCTAssertEqual(keychain.saveDataCount, 1)
     }
 
     func testUpdateEndpointProfile_withProfile_shouldUpdateandSendUpdateRequestAndSave() async {
+        keychain.resetCounters()
         await endpointClient.addAttributes(["value"], forKey: "attribute")
         await endpointClient.addMetric(1, forKey: "metric")
 
@@ -139,15 +156,17 @@ class EndpointClientTests: XCTestCase {
 
         XCTAssertTrue(pinpointProfile.attributes.isEmpty)
         XCTAssertTrue(pinpointProfile.metrics.isEmpty)
-        XCTAssertEqual(userDefaults.saveCount, 2)
-        userDefaults.resetCounters()
+        XCTAssertEqual(keychain.saveDataCount, 2)
+        XCTAssertEqual(archiver.encodeCount, 2)
+        keychain.resetCounters()
+        archiver.resetCounters()
 
         try? await endpointClient.updateEndpointProfile(with: pinpointProfile)
         XCTAssertEqual(pinpointProfile.attributes.count, 1)
         XCTAssertEqual(pinpointProfile.metrics.count, 1)
         XCTAssertEqual(pinpointClient.updateEndpointCount, 1)
         XCTAssertEqual(archiver.encodeCount, 1)
-        XCTAssertEqual(userDefaults.saveCount, 1)
+        XCTAssertEqual(keychain.saveDataCount, 1)
     }
 
     func testConvertToPublicEndpoint_shouldReturnPublicEndpoint() async {
