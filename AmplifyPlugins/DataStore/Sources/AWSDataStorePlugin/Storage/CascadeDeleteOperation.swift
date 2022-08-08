133ad3869cc1abd58184702ddafb4b75b76ef54d
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-08-08 15:56:26 -0600

    fix(datastore): fix data race in CascadeDeleteOperation (#2106)

--- AmplifyPlugins/DataStore/Sources/AWSDataStorePlugin/Storage/CascadeDeleteOperation.swift
+++ AmplifyPlugins/DataStore/Sources/AWSDataStorePlugin/Storage/CascadeDeleteOperation.swift
@@ -462,8 +462,8 @@ public class CascadeDeleteOperation<M: Model>: AsynchronousOperation {
     private func submitToSyncEngine(mutationEvent: MutationEvent,
                                     syncEngine: RemoteSyncEngineBehavior,
                                     completion: @escaping DataStoreCallback<MutationEvent>) {
-        var mutationQueueSink: AnyCancellable?
-        mutationQueueSink = syncEngine
+        let mutationQueueSink = AtomicValue<AnyCancellable?>(initialValue: nil)
+        mutationQueueSink.set(syncEngine
             .submit(mutationEvent)
             .sink(
                 receiveCompletion: { futureCompletion in
@@ -473,12 +473,12 @@ public class CascadeDeleteOperation<M: Model>: AsynchronousOperation {
                     case .finished:
                         self.log.verbose("\(#function) Received successful completion")
                     }
-                    mutationQueueSink?.cancel()
-                    mutationQueueSink = nil
+                    mutationQueueSink.get()?.cancel()
+                    mutationQueueSink.set(nil)
                 }, receiveValue: { mutationEvent in
                     self.log.verbose("\(#function) saved mutation event: \(mutationEvent)")
                     completion(.success(mutationEvent))
-                })
+                }))
     }
 }
 
