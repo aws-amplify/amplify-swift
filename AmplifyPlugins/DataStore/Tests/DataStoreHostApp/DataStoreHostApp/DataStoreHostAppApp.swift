32f7cc6ca1be5084facee974a09d219abb755328
Michael Law <1365977+lawmicha@users.noreply.github.com>
2022-08-10 13:24:26 -0400

    chore(datastore): Add github workflow for EndToEnd tests (#2115)
    * chore(datastore): Add github workflow for EndToEnd tests
    
    * test project path
    
    * optional project path
    
    * fix unittset
    
    * test with override rule in gitignore for host app package.resolved
    
    * iOS 14
    
    * fix workflow on.push.branch and enable observeQuery tests
    
    * set deployment target iOS 13
    
    * override on.push.branch to run integ test
    
    * fix iOS 13 compile errors
    
    * Update .github/workflows/integ_test_datastore.yml

--- AmplifyPlugins/DataStore/Tests/DataStoreHostApp/DataStoreHostApp/DataStoreHostAppApp.swift
+++ AmplifyPlugins/DataStore/Tests/DataStoreHostApp/DataStoreHostApp/DataStoreHostAppApp.swift
@@ -7,11 +7,42 @@
 
 import SwiftUI
 
+// Run project on iOS 13; https://stackoverflow.com/questions/62935053/use-main-in-xcode-12
+
 @main
+struct DataStoreHostAppAppWrapper {
+    static func main() {
+        if #available(iOS 14.0, *) {
+            DataStoreHostAppApp.main()
+        }
+        else {
+            UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(SceneDelegate.self))
+        }
+    }
+}
+
+@available(iOS 14.0, *)
 struct DataStoreHostAppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
         }
     }
 }
