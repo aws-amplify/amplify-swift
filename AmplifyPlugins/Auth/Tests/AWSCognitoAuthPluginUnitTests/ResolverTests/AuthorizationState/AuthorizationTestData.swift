22ddf1632adb0ae66128dabb93ccbe1dc4a7139e
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2022-08-12 12:45:59 -0400

    fix flaky tests due to expiration

--- AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/ResolverTests/AuthorizationState/AuthorizationTestData.swift
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/ResolverTests/AuthorizationState/AuthorizationTestData.swift
@@ -24,7 +24,7 @@ extension AuthAWSCognitoCredentials {
         AuthAWSCognitoCredentials(accessKey: "accessKey",
                                   secretKey: "secretKey",
                                   sessionKey: "sessionKey",
-                                  expiration: Date() + 121)
+                                  expiration: Date() + 10000)
     }
 
     static var expiredTestData: AuthAWSCognitoCredentials {
