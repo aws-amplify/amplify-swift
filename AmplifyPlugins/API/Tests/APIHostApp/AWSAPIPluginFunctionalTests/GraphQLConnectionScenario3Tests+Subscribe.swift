1271da1f7958e396e61ed3a2d13c1c1d7c116b57
Michael Law <1365977+lawmicha@users.noreply.github.com>
2022-08-17 10:59:58 -0400

    subscription async testing

--- AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginFunctionalTests/GraphQLConnectionScenario3Tests+Subscribe.swift
+++ AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginFunctionalTests/GraphQLConnectionScenario3Tests+Subscribe.swift
@@ -76,6 +76,72 @@ extension GraphQLConnectionScenario3Tests {
         XCTAssertTrue(operation.isFinished)
     }
 
+    func testOnCreatePostSubscriptionWithModel2() async throws {
+        let connectedInvoked = AsyncExpectation(description: "Connection established")
+        let disconnectedInvoked = AsyncExpectation(description: "Connection disconnected")
+        let completedInvoked = AsyncExpectation(description: "Completed invoked")
+        let progressInvoked = AsyncExpectation(description: "progress invoked", expectedFulfillmentCount: 2)
+        let uuid = UUID().uuidString
+        let uuid2 = UUID().uuidString
+        let testMethodName = String("\(#function)".dropLast(2))
+        let title = testMethodName + "Title"
+        print("1")
+        let task = try await Amplify.API.subscribe(request: .subscription(of: Post3.self, type: .onCreate))
+        print("2")
+        let subscription = await task.subscription
+        print("3")
+        Task {
+            for await subscriptionEvent in subscription {
+                print("Got subscriptionEvent \(subscriptionEvent)")
+                switch subscriptionEvent {
+                case .connection(let state):
+                    switch state {
+                    case .connecting:
+                        break
+                    case .connected:
+                        await connectedInvoked.fulfill()
+                    case .disconnected:
+                        await disconnectedInvoked.fulfill()
+                    }
+                case .data(let result):
+                    switch result {
+                    case .success(let post):
+                        if post.id == uuid || post.id == uuid2 {
+                            
+                            await progressInvoked.fulfill()
+                        }
+                    case .failure(let error):
+                        XCTFail("\(error)")
+                    }
+                }
+            }
+        }
+        Task {
+            try await task.value
+            print("12")
+            await completedInvoked.fulfill()
+        }
+        
+        print("5")
+        //let completion: Void = try await task.value
+        XCTAssertNotNil(task)
+        try await AsyncExpectation.waitForExpectations([connectedInvoked], timeout: TestCommonConstants.networkTimeout)
+        print("6")
+        let post = Post3(id: uuid, title: title)
+        let createdPost = try await Amplify.API.mutate(request: .create(post))
+        print("7")
+        let post2 = Post3(id: uuid2, title: title)
+        let createdPost2 = try await Amplify.API.mutate(request: .create(post2))
+        print("8")
+
+        try await AsyncExpectation.waitForExpectations([progressInvoked], timeout: TestCommonConstants.networkTimeout)
+        print("9")
+        await task.cancel()
+        print("10")
+        //try await AsyncExpectation.waitForExpectations([disconnectedInvoked, completedInvoked], timeout: TestCommonConstants.networkTimeout)
+        print("11")
+    }
+    
     func testOnUpdatePostSubscriptionWithModel() {
         let connectedInvoked = expectation(description: "Connection established")
         let disconnectedInvoked = expectation(description: "Connection disconnected")
