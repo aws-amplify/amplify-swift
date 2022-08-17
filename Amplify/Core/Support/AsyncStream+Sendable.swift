9d57d400281698f526170e073678a84b304eb31d
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2022-08-17 12:29:30 -0700

    declares AsyncStream and AsyncThrowingStream as unchecked Sendable

---
+++ Amplify/Core/Support/AsyncStream+Sendable.swift
@@ -0,0 +1,13 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+#if swift(<5.6)
+extension AsyncStream: @unchecked Sendable where Element: Sendable { }
+extension AsyncThrowingStream: @unchecked Sendable where Element: Sendable { }
+#endif
