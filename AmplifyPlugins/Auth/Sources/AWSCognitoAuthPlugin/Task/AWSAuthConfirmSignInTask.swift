b29a3b997697788eea34e729030ecd73767d28bc
Tuan Pham <phantum@amazon.com>
2022-08-18 13:36:02 -0500

    chore(Auth): update confirm sign in task to cancel state listener token

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/AWSAuthConfirmSignInTask.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/AWSAuthConfirmSignInTask.swift
@@ -39,7 +39,7 @@ class AWSAuthConfirmSignInTask: AuthConfirmSignInTask {
             stateListenerToken = authStateMachine.listen { [weak self] state in
                 guard let self = self, case .configured(let authNState, let authZState) = state else {
                     self?.dispatch(result: .failure(invalidStateError))
-                    self?.cancelToken(self?.stateListenerToken)
+                    self?.cancelToken()
                     continuation.resume(throwing: invalidStateError)
                     return
                 }
@@ -48,13 +48,13 @@ class AWSAuthConfirmSignInTask: AuthConfirmSignInTask {
                     if case .sessionEstablished = authZState {
                         let result = AuthSignInResult(nextStep: .done)
                         self.dispatch(result: .success(result))
-                        self.cancelToken(self.stateListenerToken)
+                        self.cancelToken()
                         continuation.resume(returning: result)
                     }
                 case .error(let error):
                     let authError = AuthError.unknown("Sign in reached an error state", error)
                     self.dispatch(result: .failure(authError))
-                    self.cancelToken(self.stateListenerToken)
+                    self.cancelToken()
                     continuation.resume(throwing: authError)
                 case .signingIn(let signInState):
                     if case .resolvingChallenge(let challengeState, _) = signInState,
@@ -65,18 +65,18 @@ class AWSAuthConfirmSignInTask: AuthConfirmSignInTask {
                            case .passwordResetRequired = cognitoError {
                             let result = AuthSignInResult(nextStep: .resetPassword(nil))
                             self.dispatch(result: .success(result))
-                            self.cancelToken(self.stateListenerToken)
+                            self.cancelToken()
                             continuation.resume(returning: result)
                         } else if case .service(_, _, let serviceError) = authError,
                                   let cognitoError = serviceError as? AWSCognitoAuthError,
                                   case .userNotConfirmed = cognitoError {
                             let result = AuthSignInResult(nextStep: .confirmSignUp(nil))
                             self.dispatch(result: .success(result))
-                            self.cancelToken(self.stateListenerToken)
+                            self.cancelToken()
                             continuation.resume(returning: result)
                         } else {
                             self.dispatch(result: .failure(authError))
-                            self.cancelToken(self.stateListenerToken)
+                            self.cancelToken()
                             continuation.resume(throwing: authError)
                         }
                     } else if case .resolvingChallenge(let challengeState, _) = signInState {
@@ -100,15 +100,15 @@ class AWSAuthConfirmSignInTask: AuthConfirmSignInTask {
                     }
                 default:
                     self.dispatch(result: .failure(invalidStateError))
-                    self.cancelToken(self.stateListenerToken)
+                    self.cancelToken()
                     continuation.resume(throwing: invalidStateError)
                 }
             } onSubscribe: { }
         }
     }
     
-    private func cancelToken(_ token: AuthStateMachineToken?) {
-        if let token = token {
+    private func cancelToken() {
+        if let token = self.stateListenerToken {
             authStateMachine.cancel(listenerToken: token)
         }
     }
