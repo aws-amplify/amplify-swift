e2e6f5a9b8e3e83560b92a1e4060fa2b906c6df1
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-08-20 23:33:24 -0600

    added withTaskCancellationHandler to immediately handle cancellation in the case of a slow iterator.

--- Amplify/Core/Support/Amplify+Publisher.swift
+++ Amplify/Core/Support/Amplify+Publisher.swift
@@ -69,25 +69,33 @@ public extension Amplify {
         ///
         /// - Parameter sequence: The AsyncSequence for which to create the Publisher.
         /// - Returns: The Publisher for the given AsyncSequence.
-        public static func create<Sequence: AsyncSequence>(
+        static func create<Sequence: AsyncSequence>(
             _ sequence: Sequence
         ) -> AnyPublisher<Sequence.Element, Error> {
             let subject = PassthroughSubject<Sequence.Element, Error>()
             let task = Task {
                 do {
-                    for try await value in sequence {
+                    // If the Task is cancelled, this will allow the onCancel closure to be called immediately.
+                    // This is necessary to prevent continuing to wait until another value is received from
+                    // the sequence before cancelling in the case of a slow Iterator.
+                    try await withTaskCancellationHandler {
+                        for try await value in sequence {
+                            // If the Task is cancelled, this will end the loop and send a CancellationError
+                            // via the publisher.
+                            // This is necessary to prevent the sequence from continuing to send values for a time
+                            // after cancellation in the case of a fast Iterator.
+                            try Task.checkCancellation()
+                            
+                            subject.send(value)
+                        }
+                        subject.send(completion: .finished)
+                    } onCancel: {
                         // If the Task is cancelled and the AsyncSequence is Cancellable, as
                         // is the case with AmplifyAsyncSequence, cancel the AsyncSequence.
-                        if Task.isCancelled {
-                            if let cancellable = sequence as? Cancellable {
-                                cancellable.cancel()
-                            }
-                            // This will end the loop and send a CancellationError to the publisher
-                            throw CancellationError()
+                        if let cancellable = sequence as? Cancellable {
+                            cancellable.cancel()
                         }
-                        subject.send(value)
                     }
-                    subject.send(completion: .finished)
                 } catch {
                     subject.send(completion: .failure(error))
                 }
