076ee9e5d672eb85b11d5ba2980039895b00e59a
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-08-22 14:37:17 -0600

    feat: add support for AsyncSequence to Amplify.Publisher (#2183)

--- Amplify/Core/Support/Amplify+Publisher.swift
+++ Amplify/Core/Support/Amplify+Publisher.swift
@@ -8,10 +8,33 @@
 #if canImport(Combine)
 import Combine
 
-extension Amplify {
+public extension Amplify {
+    
+    /// Get Combine Publishers for Amplify APIs.
+    ///
+    /// Provides static methods to create Combine Publishers from Tasks and
+    /// AsyncSequences.
+    ///
+    /// These can be used to get Combine Publishers for any Amplify API.
     enum Publisher {
-        static func create<Success>(
-            operation: @escaping @Sendable () async throws -> Success
+        /// Create a Combine Publisher for a given Task.
+        ///
+        /// Example Usage
+        /// ```
+        /// let sink = Amplify.Publisher.create {
+        ///     try await Amplify.Geo.search(for "coffee")
+        /// }
+        ///     .sink { completion in
+        ///         // handle completion
+        ///     } receiveValue: { value in
+        ///         // handle value
+        ///     }
+        /// ```
+        ///
+        /// - Parameter operation: The Task for which to create the Publisher.
+        /// - Returns: The Publisher for the given Task.
+        public static func create<Success>(
+            _ operation: @escaping @Sendable () async throws -> Success
         ) -> AnyPublisher<Success, Error> {
             let task = Task(operation: operation)
             return Future() { promise in
@@ -26,6 +49,60 @@ extension Amplify {
             }
             .handleEvents(receiveCancel: { task.cancel() } )
             .eraseToAnyPublisher()
+        }
+        
+        /// Create a Combine Publisher for a given AsyncSequence.
+        ///
+        /// Example Usage
+        /// ```
+        /// let subscription = try await Amplify.API.subscribe(
+        ///     request: .subscription(of: Todo.self, type: .onCreate)
+        /// )
+        ///
+        /// let sink = Amplify.Publisher.create(subscription)
+        ///     .sink { completion in
+        ///         // handle completion
+        ///     } receiveValue: { value in
+        ///         // handle value
+        ///     }
+        /// ```
+        ///
+        /// - Parameter sequence: The AsyncSequence for which to create the Publisher.
+        /// - Returns: The Publisher for the given AsyncSequence.
+        static func create<Sequence: AsyncSequence>(
+            _ sequence: Sequence
+        ) -> AnyPublisher<Sequence.Element, Error> {
+            let subject = PassthroughSubject<Sequence.Element, Error>()
+            let task = Task {
+                do {
+                    // If the Task is cancelled, this will allow the onCancel closure to be called immediately.
+                    // This is necessary to prevent continuing to wait until another value is received from
+                    // the sequence before cancelling in the case of a slow Iterator.
+                    try await withTaskCancellationHandler {
+                        for try await value in sequence {
+                            // If the Task is cancelled, this will end the loop and send a CancellationError
+                            // via the publisher.
+                            // This is necessary to prevent the sequence from continuing to send values for a time
+                            // after cancellation in the case of a fast Iterator.
+                            try Task.checkCancellation()
+                            
+                            subject.send(value)
+                        }
+                        subject.send(completion: .finished)
+                    } onCancel: {
+                        // If the Task is cancelled and the AsyncSequence is Cancellable, as
+                        // is the case with AmplifyAsyncSequence, cancel the AsyncSequence.
+                        if let cancellable = sequence as? Cancellable {
+                            cancellable.cancel()
+                        }
+                    }
+                } catch {
+                    subject.send(completion: .failure(error))
+                }
+            }
+            return subject
+                .handleEvents(receiveCancel: { task.cancel() })
+                .eraseToAnyPublisher()
         }
     }
 }
