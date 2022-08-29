3bbb10e9ffbe1235ae81e742bf6953b562e96fcc
Michael Law <1365977+lawmicha@users.noreply.github.com>
2022-08-29 12:33:06 -0400

    make V2 tests compile

--- AmplifyPlugins/DataStore/Tests/DataStoreHostApp/AWSDataStorePluginV2Tests/TestSupport/DataStoreTestBase.swift
+++ AmplifyPlugins/DataStore/Tests/DataStoreHostApp/AWSDataStorePluginV2Tests/TestSupport/DataStoreTestBase.swift
@@ -10,51 +10,15 @@ import XCTest
 import Amplify
 
 class DataStoreTestBase: XCTestCase {
-    func saveModel<M: Model>(_ model: M) async -> DataStoreResult<M> {
-        let saveFinished = expectation(description: "Save finished")
-        var result: DataStoreResult<M>?
-
-        Amplify.DataStore.save(model) { sResult in
-            result = sResult
-            saveFinished.fulfill()
-        }
-        await waitForExpectations(timeout: TestCommonConstants.networkTimeout)
-        guard let saveResult = result else {
-            return .failure(causedBy: "Save operation timed out")
-        }
-        return saveResult
+    func saveModel<M: Model>(_ model: M) async throws -> M {
+        return try await Amplify.DataStore.save(model)
     }
 
-    func queryModel<M: Model>(_ modelType: M.Type, byId id: String) async -> DataStoreResult<M?> {
-        let queryFinished = expectation(description: "Query Finished")
-        var result: DataStoreResult<M?>?
-
-        Amplify.DataStore.query(modelType, byId: id) { qResult in
-            result = qResult
-            queryFinished.fulfill()
-        }
-
-        await waitForExpectations(timeout: TestCommonConstants.networkTimeout)
-        guard let queryResult = result else {
-            return .failure(causedBy: "Query operation timed out")
-        }
-        return queryResult
+    func queryModel<M: Model>(_ modelType: M.Type, byId id: String) async throws -> M? {
+        return try await Amplify.DataStore.query(modelType, byId: id)
     }
 
-    func deleteModel<M: Model>(_ model: M) async -> DataStoreResult<Void> {
-        let deleteFinished = expectation(description: "Delete Finished")
-        var result: DataStoreResult<Void>?
-
-        Amplify.DataStore.delete(model,
-                                 completion: { dResult in
-                                    result = dResult
-                                    deleteFinished.fulfill()
-                                 })
-
-        await waitForExpectations(timeout: TestCommonConstants.networkTimeout)
-        guard let deleteResult = result else {
-            return .failure(causedBy: "Delete operation timed out")
-        }
-        return deleteResult
+    func deleteModel<M: Model>(_ model: M) async throws {
+        try await Amplify.DataStore.delete(model)
     }
 }
