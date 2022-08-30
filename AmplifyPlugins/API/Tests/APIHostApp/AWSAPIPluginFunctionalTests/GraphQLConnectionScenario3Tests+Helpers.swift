f2974b5d63e45222fa860d0eb90c22a5359649f5
Michael Law <1365977+lawmicha@users.noreply.github.com>
2022-08-30 15:58:34 -0400

    Fix GraphQLConnectionScenario3Tests

--- AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginFunctionalTests/GraphQLConnectionScenario3Tests+Helpers.swift
+++ AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginFunctionalTests/GraphQLConnectionScenario3Tests+Helpers.swift
@@ -14,51 +14,43 @@ extension GraphQLConnectionScenario3Tests {
     
     func createPost(id: String = UUID().uuidString, title: String) async throws -> Post3? {
         let post = Post3(id: id, title: title)
-        var result: Post3?
         let event = try await Amplify.API.mutate(request: .create(post))
-            switch event {
-                case .success(let post):
-                    result = post
-                case .failure(let graphQLResponseError):
-                    XCTFail("Failed with error: \(graphQLResponseError)")
-                }
-        return result
+        switch event {
+        case .success(let post):
+            return post
+        case .failure(let graphQLResponseError):
+            throw graphQLResponseError
+        }
     }
 
     func createComment(id: String = UUID().uuidString, postID: String, content: String) async throws -> Comment3? {
         let comment = Comment3(id: id, postID: postID, content: content)
-        var result: Comment3?
         let event = try await Amplify.API.mutate(request: .create(comment))
-            switch event {
-                case .success(let comment):
-                    result = comment
-                case .failure(let graphQLResponseError):
-                XCTFail("Failed with error: \(graphQLResponseError)")
-                }
-        return result
+        switch event {
+        case .success(let comment):
+            return comment
+        case .failure(let graphQLResponseError):
+            throw graphQLResponseError
+        }
     }
 
     func mutatePost(post: Post3) async throws -> Post3? {
-        var result: Post3?
         let event = try await Amplify.API.mutate(request: .update(post))
-            switch event {
-            case .success(let post):
-                result = post
-            case .failure(let graphQLResponseError):
-                XCTFail("Failed with error: \(graphQLResponseError)")
-            }
-        return result
+        switch event {
+        case .success(let post):
+            return post
+        case .failure(let graphQLResponseError):
+            throw graphQLResponseError
+        }
     }
 
     func deletePost(post: Post3) async throws -> Post3? {
-        var result: Post3?
         let event = try await Amplify.API.mutate(request: .delete(post))
-            switch event {
-            case .success(let post):
-                result = post
-            case .failure(let graphQLResponseError):
-                XCTFail("Failed with error: \(graphQLResponseError)")
-            }
-        return result
+        switch event {
+        case .success(let post):
+            return post
+        case .failure(let graphQLResponseError):
+            throw graphQLResponseError
+        }
     }
 }
