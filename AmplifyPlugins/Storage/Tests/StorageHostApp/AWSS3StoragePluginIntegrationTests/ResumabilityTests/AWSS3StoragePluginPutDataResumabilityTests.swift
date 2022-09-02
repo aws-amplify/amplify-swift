ae59b1055aa6680608bcffedb3f65e09a4a80162
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2022-09-02 17:17:28 -0400

    Fixing AWSS3StoragePluginUploadDataResumabilityTests

--- AmplifyPlugins/Storage/Tests/StorageHostApp/AWSS3StoragePluginIntegrationTests/ResumabilityTests/AWSS3StoragePluginPutDataResumabilityTests.swift
+++ AmplifyPlugins/Storage/Tests/StorageHostApp/AWSS3StoragePluginIntegrationTests/ResumabilityTests/AWSS3StoragePluginPutDataResumabilityTests.swift
@@ -9,6 +9,7 @@ import XCTest
 import Amplify
 import AWSS3StoragePlugin
 import AWSS3
+import Combine
 
 // swiftlint:disable:next type_name
 class AWSS3StoragePluginUploadDataResumabilityTests: AWSS3StoragePluginTestBase {
@@ -16,45 +17,49 @@ class AWSS3StoragePluginUploadDataResumabilityTests: AWSS3StoragePluginTestBase 
     /// Given: A large data object to upload
     /// When: Call the put API and pause the operation
     /// Then: The operation is stalled (no progress, completed, or failed event)
-    func testUploadLargeDataThenPause() {
+    func testUploadLargeDataThenPause() async throws {
+        var cancellables = Set<AnyCancellable>()
         let key = UUID().uuidString
         let progressInvoked = expectation(description: "Progress invoked")
         progressInvoked.assertForOverFulfill = false
-        let completeInvoked = expectation(description: "Completion invoked")
-        completeInvoked.isInverted = true
-        let failedInvoked = expectation(description: "Failed invoked")
-        failedInvoked.isInverted = true
-        let noProgressAfterPause = expectation(description: "Progress after pause is invoked")
-        noProgressAfterPause.isInverted = true
-        let operation = Amplify.Storage.uploadData(
+        let task = try await Amplify.Storage.uploadData(
             key: key,
             data: AWSS3StoragePluginTestBase.largeDataObject,
-            options: nil,
-            progressListener: { progress in
-                // To simulate a normal scenario, fulfill the progressInvoked expectation after some progress (30%)
-                if progress.fractionCompleted > 0.3 {
-                    progressInvoked.fulfill()
-                }
+            options: nil
+        )
 
-                // After pausing, progress events still trickle in, but should not exceed
-                if progress.fractionCompleted > 0.7 {
-                    noProgressAfterPause.fulfill()
-                }
-
-        }, resultListener: { result in
-            switch result {
-            case .success:
-                completeInvoked.fulfill()
-            case .failure:
-                failedInvoked.fulfill()
+        task.inProcessPublisher.sink { progress in
+            // To simulate a normal scenario, fulfill the progressInvoked expectation after some progress (30%)
+            if progress.fractionCompleted > 0.3 {
+                progressInvoked.fulfill()
             }
-        })
+        }.store(in: &cancellables)
 
-        XCTAssertNotNil(operation)
-        wait(for: [progressInvoked], timeout: TestCommonConstants.networkTimeout)
-        operation.pause()
-        wait(for: [completeInvoked, failedInvoked, noProgressAfterPause], timeout: 30)
-        operation.cancel()
+        await waitForExpectations(timeout: TestCommonConstants.networkTimeout)
+        task.pause()
+        cancellables.removeAll()
+
+        let noProgressAfterPause = expectation(description: "Progress after pause is invoked")
+        noProgressAfterPause.isInverted = true
+        noProgressAfterPause.assertForOverFulfill = false
+        task.inProcessPublisher.sink { progress in
+            // After pausing, progress events still trickle in, but should not exceed
+            if progress.fractionCompleted > 0.7 {
+                XCTFail("Task should have been paused")
+                noProgressAfterPause.fulfill()
+            }
+        }.store(in: &cancellables)
+
+        let completeInvoked = expectation(description: "Upload is completed")
+        completeInvoked.isInverted = true
+        task.resultPublisher.sink(receiveCompletion: { _ in }, receiveValue: { value in
+            XCTFail("Task should have been paused")
+            completeInvoked.fulfill()
+        })
+        .store(in: &cancellables)
+
+        await waitForExpectations(timeout: 30)
+        task.cancel()
         // A 5 second sleep has been added because, the cancelling runs async task to cancel anything existing that is still running, 
         // This gives ample time for operation to cancel, and then await Amplify.reset(), Amplify.configure works as expected.
         // If the sleep is not added, await Amplify.reset() will be trigerred in the tear down method which will remove all the plugins, 
@@ -63,71 +68,80 @@ class AWSS3StoragePluginUploadDataResumabilityTests: AWSS3StoragePluginTestBase 
     }
 
     /// Given: A large data object to upload
-    /// When: Call the put API, pause, and then resume tthe operation,
+    /// When: Call the put API, pause, and then resume the operation,
     /// Then: The operation should complete successfully
-    func testUploadLargeDataAndPauseThenResume() {
+    func testUploadLargeDataAndPauseThenResume() async throws {
+        var cancellables = Set<AnyCancellable>()
         let key = UUID().uuidString
-        let completeInvoked = expectation(description: "Completed is invoked")
         let progressInvoked = expectation(description: "Progress invoked")
         progressInvoked.assertForOverFulfill = false
-        let operation = Amplify.Storage.uploadData(
+
+        let task = try await Amplify.Storage.uploadData(
             key: key,
             data: AWSS3StoragePluginTestBase.largeDataObject,
-            options: nil,
-            progressListener: { progress in
-                // To simulate a normal scenario, fulfill the progressInvoked expectation after some progress (30%)
-                if progress.fractionCompleted > 0.3 {
-                    progressInvoked.fulfill()
-                }
-        }, resultListener: { result in
+            options: nil
+        )
+
+        task.inProcessPublisher.sink { progress in
+            // To simulate a normal scenario, fulfill the progressInvoked expectation after some progress (30%)
+            if progress.fractionCompleted > 0.3 {
+                progressInvoked.fulfill()
+            }
+        }.store(in: &cancellables)
+
+        XCTAssertNotNil(task)
+        await waitForExpectations(timeout: TestCommonConstants.networkTimeout)
+        task.pause()
+
+        let completeInvoked = expectation(description: "Upload is completed")
+        task.resultPublisher.sink(receiveCompletion: { result in
             switch result {
-            case .success:
+            case .finished:
                 completeInvoked.fulfill()
             case .failure(let error):
                 XCTFail("Failed with \(error)")
+                completeInvoked.fulfill()
             }
-        })
+        }, receiveValue: { _ in })
+        .store(in: &cancellables)
 
-        XCTAssertNotNil(operation)
-        wait(for: [progressInvoked], timeout: TestCommonConstants.networkTimeout)
-        operation.pause()
-        operation.resume()
-        wait(for: [completeInvoked], timeout: TestCommonConstants.networkTimeout)
+        task.resume()
+        await waitForExpectations(timeout: TestCommonConstants.networkTimeout)
     }
 
     /// Given: A large data object to upload
     /// When: Call the put API, pause, and then resume tthe operation,
     /// Then: The operation should complete successfully
-    func testUploadLargeDataAndCancel() {
+    func testUploadLargeDataAndCancel() async throws {
+        var cancellables = Set<AnyCancellable>()
         let key = UUID().uuidString
         let progressInvoked = expectation(description: "Progress invoked")
         progressInvoked.assertForOverFulfill = false
-        let completedInvoked = expectation(description: "Completion invoked")
-        completedInvoked.isInverted = true
-        let failedInvoked = expectation(description: "Failed invoked")
-        failedInvoked.isInverted = true
-        let operation = Amplify.Storage.uploadData(
+
+        let task = try await Amplify.Storage.uploadData(
             key: key,
             data: AWSS3StoragePluginTestBase.largeDataObject,
-            options: nil,
-            progressListener: { progress in
+            options: nil
+        )
+
+        task.inProcessPublisher.sink { progress in
             // To simulate a normal scenario, fulfill the progressInvoked expectation after some progress (30%)
             if progress.fractionCompleted > 0.3 {
                 progressInvoked.fulfill()
             }
-        }, resultListener: { result in
-            switch result {
-            case .success:
-                completedInvoked.fulfill()
-            case .failure:
-                failedInvoked.fulfill()
-            }
+        }.store(in: &cancellables)
+
+        await waitForExpectations(timeout: TestCommonConstants.networkTimeout)
+
+        let completeInvoked = expectation(description: "Upload is completed")
+        completeInvoked.isInverted = true
+        task.resultPublisher.sink(receiveCompletion: { _ in }, receiveValue: { value in
+            XCTFail("Task should have been cancelled")
+            completeInvoked.fulfill()
         })
+        .store(in: &cancellables)
 
-        XCTAssertNotNil(operation)
-        wait(for: [progressInvoked], timeout: TestCommonConstants.networkTimeout)
-        operation.cancel()
-        wait(for: [completedInvoked, failedInvoked], timeout: 30)
+        task.cancel()
+        await waitForExpectations(timeout: 30)
     }
-
 }
