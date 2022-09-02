84a6768e545ffa92ed74f1f6ee937bb0c3e9da65
Brennan Stehling <stehlib@amazon.com>
2022-09-02 11:47:03 -0700

    removes listener functions which were deprecated and removed

--- AmplifyTestCommon/Mocks/MockStorageCategoryPlugin.swift
+++ AmplifyTestCommon/Mocks/MockStorageCategoryPlugin.swift
@@ -10,79 +10,6 @@ import Foundation
 
 class MockStorageCategoryPlugin: MessageReporter, StorageCategoryPlugin {
 
-    func getURL(key: String,
-                options: StorageGetURLRequest.Options?,
-                resultListener: StorageGetURLOperation.ResultListener?) -> StorageGetURLOperation {
-        notify("getURL")
-        let options = options ?? StorageGetURLRequest.Options()
-        let request = StorageGetURLRequest(key: key, options: options)
-        return MockStorageGetURLOperation(request: request)
-    }
-
-    func downloadData(key: String,
-                      options: StorageDownloadDataRequest.Options?,
-                      progressListener: ProgressListener? = nil,
-                      resultListener: StorageDownloadDataOperation.ResultListener?
-    ) -> StorageDownloadDataOperation {
-        notify("downloadData")
-        let options = options ?? StorageDownloadDataRequest.Options()
-        let request = StorageDownloadDataRequest(key: key, options: options)
-        return MockStorageDownloadDataOperation(request: request)
-    }
-
-    func downloadFile(key: String,
-                      local: URL,
-                      options: StorageDownloadFileRequest.Options?,
-                      progressListener: ProgressListener? = nil,
-                      resultListener: StorageDownloadFileOperation.ResultListener?
-    ) -> StorageDownloadFileOperation {
-        notify("downloadFile")
-        let options = options ?? StorageDownloadFileRequest.Options()
-        let request = StorageDownloadFileRequest(key: key, local: local, options: options)
-        return MockStorageDownloadFileOperation(request: request)
-    }
-
-    func uploadData(key: String,
-                    data: Data,
-                    options: StorageUploadDataRequest.Options?,
-                    progressListener: ProgressListener? = nil,
-                    resultListener: StorageUploadDataOperation.ResultListener?
-    ) -> StorageUploadDataOperation {
-        notify("uploadData")
-        let options = options ?? StorageUploadDataRequest.Options()
-        let request = StorageUploadDataRequest(key: key, data: data, options: options)
-        return MockStorageUploadDataOperation(request: request)
-    }
-
-    func uploadFile(key: String,
-                    local: URL,
-                    options: StorageUploadFileRequest.Options?,
-                    progressListener: ProgressListener? = nil,
-                    resultListener: StorageUploadFileOperation.ResultListener?
-    ) -> StorageUploadFileOperation {
-        notify("uploadFile")
-        let options = options ?? StorageUploadFileRequest.Options()
-        let request = StorageUploadFileRequest(key: key, local: local, options: options)
-        return MockStorageUploadFileOperation(request: request)
-    }
-
-    func remove(key: String,
-                options: StorageRemoveRequest.Options?,
-                resultListener: StorageRemoveOperation.ResultListener?) -> StorageRemoveOperation {
-        notify("remove")
-        let options = options ?? StorageRemoveRequest.Options()
-        let request = StorageRemoveRequest(key: key, options: options)
-        return MockStorageRemoveOperation(request: request)
-    }
-
-    func list(options: StorageListRequest.Options?,
-              resultListener: StorageListOperation.ResultListener?) -> StorageListOperation {
-        notify("list")
-        let options = options ?? StorageListRequest.Options()
-        let request = StorageListRequest(options: options)
-        return MockStorageListOperation(request: request)
-    }
-
     var key: String {
         return "MockStorageCategoryPlugin"
     }
@@ -94,8 +21,6 @@ class MockStorageCategoryPlugin: MessageReporter, StorageCategoryPlugin {
     func reset() {
         notify("reset")
     }
-
-    // MARK: - Async API -
 
     @discardableResult
     func getURL(key: String,
