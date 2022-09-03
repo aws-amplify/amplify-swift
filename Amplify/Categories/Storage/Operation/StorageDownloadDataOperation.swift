ac239dbb92ffdb238a64b6a70ce744508176fcaa
Brennan Stehling <stehlib@amazon.com>
2022-09-02 18:16:06 -0700

    WIP

--- Amplify/Categories/Storage/Operation/StorageDownloadDataOperation.swift
+++ Amplify/Categories/Storage/Operation/StorageDownloadDataOperation.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+#if canImport(Combine)
+import Combine
+#endif
 
 public protocol StorageDownloadDataOperation: AmplifyInProcessReportingOperation<
     StorageDownloadDataRequest,
@@ -23,3 +26,62 @@ public typealias StorageDownloadDataTask = AmplifyInProcessReportingOperationTas
                                                                                                   Progress,
                                                                                                   Data,
                                                                                                   StorageError>
+
+public struct RealStorageDownloadDataTask<Request: AmplifyOperationRequest, InProcess, Success, Failure: AmplifyError>: AmplifyTask, AmplifyInProcessReportingTask {
+//    public typealias Request = StorageDownloadDataRequest
+//    public typealias InProcess = Progress
+//    public typealias Success = Data
+//    public typealias Failure = StorageError
+
+    let operation: AmplifyInProcessReportingOperation<Request, InProcess, Success, Failure>
+    let childTask: ChildTask<InProcess, Success, Failure>
+
+    // This type can take the operation and create the TaskAdapter to use internally
+    // Another initializer can take the active task context to support value and inProcess and the functions
+    public init(operation: AmplifyInProcessReportingOperation<Request, InProcess, Success, Failure>) {
+        self.operation = operation
+        self.childTask = ChildTask(parent: operation)
+    }
+
+    public var value: Success {
+        get async throws {
+            Fatal.notImplemented()
+        }
+    }
+
+    public var inProcess: AmplifyAsyncSequence<InProcess> {
+        get async {
+            Fatal.notImplemented()
+        }
+    }
+
+    public func pause() {
+        Fatal.notImplemented()
+    }
+
+    public func resume() {
+        Fatal.notImplemented()
+    }
+
+    public func cancel() {
+        Fatal.notImplemented()
+    }
+
+#if canImport(Combine)
+    public var resultPublisher: AnyPublisher<Success, Failure> {
+        Fatal.notImplemented()
+//        Amplify.Publisher.create {
+//            try await value
+//        }
+    }
+
+    public var inProcessPublisher: AnyPublisher<InProcess, Never> {
+//        Fatal.notImplemented()
+        let sequence = AmplifyAsyncSequence<InProcess>()
+//        return sequence
+        let p = Amplify.Publisher.create(sequence)
+        return p
+    }
+#endif
+
+}
