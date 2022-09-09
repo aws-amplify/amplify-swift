dabfa3b5ec6dc7f080560ec580c6563713fdc8a8
Jithin Roy <51138777+royjit@users.noreply.github.com>
2022-09-09 09:34:56 -0700

    test(auth): Added test for the signout task (#2283)

---
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/Mocks/MockData/RevokeTokenOutputResponse+Mock.swift
@@ -0,0 +1,16 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+import AWSCognitoIdentityProvider
+
+extension RevokeTokenOutputResponse {
+
+    static var testData: RevokeTokenOutputResponse {
+        return .init()
+    }
+}
