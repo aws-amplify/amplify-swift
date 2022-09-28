a2b03bb281387de099374eb5779e9a041afe6098
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2022-09-28 17:44:27 -0400

    fix(Auth): Remove expiration from user pool tokens

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Support/Helpers/AuthCognitoTokens+Validation.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Support/Helpers/AuthCognitoTokens+Validation.swift
@@ -18,7 +18,7 @@ extension AuthCognitoTokens {
               let accessTokenClaims = try? AWSAuthService().getTokenClaims(tokenString: accessToken).get(),
               let idTokenExpiration = idTokenClaims["exp"]?.doubleValue,
               let accessTokenExpiration = accessTokenClaims["exp"]?.doubleValue else {
-            return currentTime > expiration
+            return true
         }
 
         let idTokenExpiry = Date(timeIntervalSince1970: idTokenExpiration)
