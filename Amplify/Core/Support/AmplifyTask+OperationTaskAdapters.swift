9b410fda9ecc6191c9c52337abac579122672d25
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2022-10-14 17:11:02 -0400

    Update Amplify/Core/Support/AmplifyTask+OperationTaskAdapters.swift
    Co-authored-by: Ian Saultz <52051793+atierian@users.noreply.github.com>

--- Amplify/Core/Support/AmplifyTask+OperationTaskAdapters.swift
+++ Amplify/Core/Support/AmplifyTask+OperationTaskAdapters.swift
@@ -69,11 +69,11 @@ public class AmplifyInProcessReportingOperationTaskAdapter<Request: AmplifyOpera
     public init(operation: AmplifyInProcessReportingOperation<Request, InProcess, Success, Failure>) {
         self.operation = operation
         self.childTask = ChildTask(parent: operation)
-        resultToken = operation.subscribe(resultListener: {[weak self] result in
+        resultToken = operation.subscribe(resultListener: { [weak self] result in
             guard let self = self else { return }
             self.resultListener(result)
         })
-        inProcessToken = operation.subscribe(inProcessListener: {[weak self] inProcess in
+        inProcessToken = operation.subscribe(inProcessListener: { [weak self] inProcess in
             guard let self = self else { return }
             self.inProcessListener(inProcess)
         })
