f5b7b928af30d43a4d463f7ffe1beb6e897520bc
Jithin Roy <51138777+royjit@users.noreply.github.com>
2022-10-24 10:58:30 -0700

    chore: Update the useragent string to new repo name

--- AmplifyPlugins/Core/AWSPluginsCore/ServiceConfiguration/AmplifyAWSServiceConfiguration.swift
+++ AmplifyPlugins/Core/AWSPluginsCore/ServiceConfiguration/AmplifyAWSServiceConfiguration.swift
@@ -9,8 +9,9 @@ import Foundation
 import AWSClientRuntime
 
 public class AmplifyAWSServiceConfiguration {
+
     public static let amplifyVersion = "2.2.1"
-    public static let platformName = "amplify-ios"
+    public static let platformName = "amplify-swift"
 
     public static func frameworkMetaData() -> FrameworkMetadata {
 
