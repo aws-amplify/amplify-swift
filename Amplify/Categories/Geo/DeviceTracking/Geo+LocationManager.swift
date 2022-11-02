45ea24e78dafb7ede483be9517826a72272cb5a7
Tuan Pham <103537251+phantumcode@users.noreply.github.com>
2022-11-02 09:58:19 -0500

    chore(geo): update device tracking to support macos (#2521)
    * chore(geo): update device tracking to support macos
    
    * chore(geo): update code documentation for MacOS

--- Amplify/Categories/Geo/DeviceTracking/Geo+LocationManager.swift
+++ Amplify/Categories/Geo/DeviceTracking/Geo+LocationManager.swift
@@ -26,7 +26,9 @@ public extension Geo {
             allowsBackgroundLocationUpdates = options.allowsBackgroundLocationUpdates
             pausesLocationUpdatesAutomatically = options.pausesLocationUpdatesAutomatically
             activityType = options.activityType
+            #if !os(macOS)
             showsBackgroundLocationIndicator = options.showsBackgroundLocationIndicator
+            #endif
         }
     }
 
