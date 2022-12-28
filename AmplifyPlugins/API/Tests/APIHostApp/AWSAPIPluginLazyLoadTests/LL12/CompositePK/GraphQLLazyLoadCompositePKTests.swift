16fd0114c49e3afe81d0f988501cd86fe9b1b9f9
Michael Law <1365977+lawmicha@users.noreply.github.com>
2022-12-28 10:39:00 -0500

    API integ tests - refactor LL112 into 3 separate test classes

--- AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL12/CompositePK/GraphQLLazyLoadCompositePKTests.swift
+++ AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL12/CompositePK/GraphQLLazyLoadCompositePKTests.swift
@@ -12,184 +12,14 @@ import XCTest
 @testable import Amplify
 import AWSPluginsCore
 
-final class GraphQLLazyLoadL12Tests: GraphQLLazyLoadBaseTest {
+final class GraphQLLazyLoadCompositePKTests: GraphQLLazyLoadBaseTest {
 
     func testConfigure() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
+        await setup(withModels: CompositePKModels(), logLevel: .verbose)
     }
-    
-    // MARK: - HasOneParent / HasOneChild
-    
-    func testHasOneParentChild() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let hasOneChild = HasOneChild()
-        let savedChild = try await mutate(.create(hasOneChild))
-        let hasOneParent = HasOneParent(child: hasOneChild)
-        let savedParent = try await mutate(.create(hasOneParent))
-        
-        assertLazyReference(savedParent._child,
-                            state: .notLoaded(identifiers: [.init(name: "id", value: savedChild.id)]))
-        let loadedChild = try await savedParent.child
-        assertLazyReference(savedParent._child,
-                            state: .loaded(model: loadedChild))
-    }
-    
-    func testHasOneParentChildUpdate() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let hasOneChild = HasOneChild()
-        let savedChild = try await mutate(.create(hasOneChild))
-        let hasOneParent = HasOneParent(child: hasOneChild)
-        var savedParent = try await mutate(.create(hasOneParent))
-        
-        let newChild = HasOneChild()
-        let savedNewChild = try await mutate(.create(newChild))
-        savedParent.setChild(newChild)
-        var updatedParent = try await mutate(.update(savedParent))
-        
-        assertLazyReference(updatedParent._child,
-                            state: .notLoaded(identifiers: [.init(name: "id", value: newChild.id)]))
-        let loadedChild = try await updatedParent.child
-        assertLazyReference(updatedParent._child,
-                            state: .loaded(model: loadedChild))
-    }
-    
-    func testHasOneParentChildDelete() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let hasOneChild = HasOneChild()
-        let savedChild = try await mutate(.create(hasOneChild))
-        let hasOneParent = HasOneParent(child: hasOneChild)
-        var savedParent = try await mutate(.create(hasOneParent))
-        
-        try await mutate(.delete(savedParent))
-        try await assertModelDoesNotExist(savedParent)
-        try await assertModelExists(savedChild)
-        try await mutate(.delete(savedChild))
-        try await assertModelDoesNotExist(savedChild)
-    }
-    
-    func testHasOneParentChildGet() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let hasOneChild = HasOneChild()
-        let savedChild = try await mutate(.create(hasOneChild))
-        let hasOneParent = HasOneParent(child: hasOneChild)
-        let savedParent = try await mutate(.create(hasOneParent))
-        
-        let queriedParent = try await query(.get(HasOneParent.self, byId: savedParent.id))!
-        assertLazyReference(queriedParent._child,
-                            state: .notLoaded(identifiers: [.init(name: "id", value: savedChild.id)]))
-        let loadedChild = try await queriedParent.child
-        assertLazyReference(queriedParent._child,
-                            state: .loaded(model: loadedChild))
-        
-        let queriedChild = try await query(.get(HasOneChild.self, byId: savedChild.id))!
-    }
-    
-    func testHasOneParentChildList() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let hasOneChild = HasOneChild()
-        let savedChild = try await mutate(.create(hasOneChild))
-        let hasOneParent = HasOneParent(child: hasOneChild)
-        let savedParent = try await mutate(.create(hasOneParent))
-        
-        let queriedParents = try await listQuery(.list(HasOneParent.self,
-                                                       where: HasOneParent.keys.id == hasOneParent.id))
-        assertList(queriedParents, state: .isLoaded(count: 1))
-        
-        let queriedChildren = try await listQuery(.list(HasOneChild.self,
-                                                       where: HasOneChild.keys.id == hasOneChild.id))
-        assertList(queriedChildren, state: .isLoaded(count: 1))
-    }
-    
-    // MARK: - DefaultPKParent / DefaultPKChild
-    
-    func testDefaultPKParentChild() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let defaultPKParent = DefaultPKParent()
-        let savedParent = try await mutate(.create(defaultPKParent))
-        let defaultPKChild = DefaultPKChild(parent: savedParent)
-        let savedChild = try await mutate(.create(defaultPKChild))
-        
-        assertLazyReference(savedChild._parent,
-                            state: .notLoaded(identifiers: [.init(name: "id", value: savedParent.id)]))
-        let loadedParent = try await savedChild.parent
-        assertLazyReference(savedChild._parent,
-                            state: .loaded(model: loadedParent))
-    }
-    
-    func testDefaultParentChildUpdate() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let defaultPKParent = DefaultPKParent()
-        let savedParent = try await mutate(.create(defaultPKParent))
-        let defaultPKChild = DefaultPKChild(parent: savedParent)
-        var savedChild = try await mutate(.create(defaultPKChild))
-        
-        let newParent = DefaultPKParent()
-        let savedNewParent = try await mutate(.create(newParent))
-        savedChild.setParent(savedNewParent)
-        var updatedChild = try await mutate(.update(savedChild))
-                
-        assertLazyReference(updatedChild._parent,
-                            state: .notLoaded(identifiers: [.init(name: "id", value: newParent.id)]))
-        let loadedParent = try await updatedChild.parent
-        assertLazyReference(updatedChild._parent,
-                            state: .loaded(model: loadedParent))
-    }
-    
-    func testDefaultParentChildDelete() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let defaultPKParent = DefaultPKParent()
-        let savedParent = try await mutate(.create(defaultPKParent))
-        let defaultPKChild = DefaultPKChild(parent: savedParent)
-        let savedChild = try await mutate(.create(defaultPKChild))
-        
-        try await mutate(.delete(savedParent))
-        try await assertModelDoesNotExist(savedParent)
-        try await assertModelExists(savedChild)
-        try await mutate(.delete(savedChild))
-        try await assertModelDoesNotExist(savedChild)
-    }
-    
-    func testDefaultPKParentChildGet() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let defaultPKParent = DefaultPKParent()
-        let savedParent = try await mutate(.create(defaultPKParent))
-        let defaultPKChild = DefaultPKChild(parent: savedParent)
-        let savedChild = try await mutate(.create(defaultPKChild))
-        
-        let queriedParent = try await query(.get(DefaultPKParent.self, byId: savedParent.id))!
-        assertList(queriedParent.children!, state: .isNotLoaded(associatedIdentifiers: [queriedParent.id],
-                                                                associatedField: "parent"))
-        try await queriedParent.children?.fetch()
-        assertList(queriedParent.children!, state: .isLoaded(count: 1))
-        
-        let queriedChild = try await query(.get(DefaultPKChild.self, byId: savedChild.id))!
-        assertLazyReference(queriedChild._parent,
-                            state: .notLoaded(identifiers: [.init(name: "id", value: savedParent.id)]))
-        let loadedParent = try await queriedChild.parent
-        assertLazyReference(queriedChild._parent,
-                            state: .loaded(model: loadedParent))
-    }
-    
-    func testDefaultPKParentChildList() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
-        let defaultPKParent = DefaultPKParent()
-        let savedParent = try await mutate(.create(defaultPKParent))
-        let defaultPKChild = DefaultPKChild(parent: savedParent)
-        let savedChild = try await mutate(.create(defaultPKChild))
-        
-        let queriedParents = try await listQuery(.list(DefaultPKParent.self,
-                                                       where: DefaultPKParent.keys.id == defaultPKParent.id))
-        assertList(queriedParents, state: .isLoaded(count: 1))
-        
-        let queriedChildren = try await listQuery(.list(DefaultPKChild.self,
-                                                        where: DefaultPKChild.keys.id == defaultPKChild.id))
-        assertList(queriedChildren, state: .isLoaded(count: 1))
-    }
-    
-    // MARK: - CompositePKParent
     
     func testCompositePKParent() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
+        await setup(withModels: CompositePKModels(), logLevel: .verbose)
         
         let compositePKParent = CompositePKParent(customId: UUID().uuidString,
                                                   content: "content")
@@ -209,7 +39,7 @@ final class GraphQLLazyLoadL12Tests: GraphQLLazyLoadBaseTest {
     }
     
     func testCompositePKParentUpdate() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
+        await setup(withModels: CompositePKModels(), logLevel: .verbose)
         
         let compositePKParent = CompositePKParent(customId: UUID().uuidString,
                                                   content: "content")
@@ -230,7 +60,7 @@ final class GraphQLLazyLoadL12Tests: GraphQLLazyLoadBaseTest {
     }
     
     func testCompositePKParentDelete() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
+        await setup(withModels: CompositePKModels(), logLevel: .verbose)
         
         let compositePKParent = CompositePKParent(customId: UUID().uuidString,
                                                   content: "content")
@@ -264,7 +94,7 @@ final class GraphQLLazyLoadL12Tests: GraphQLLazyLoadBaseTest {
     }
     
     func testCompositePKParentGet() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
+        await setup(withModels: CompositePKModels(), logLevel: .verbose)
         
         let compositePKParent = CompositePKParent(customId: UUID().uuidString,
                                                   content: "content")
@@ -369,7 +199,7 @@ final class GraphQLLazyLoadL12Tests: GraphQLLazyLoadBaseTest {
     }
     
     func testCompositePKParentList() async throws {
-        await setup(withModels: LL12Models(), logLevel: .verbose)
+        await setup(withModels: CompositePKModels(), logLevel: .verbose)
         
         let compositePKParent = CompositePKParent(customId: UUID().uuidString,
                                                   content: "content")
@@ -405,17 +235,13 @@ final class GraphQLLazyLoadL12Tests: GraphQLLazyLoadBaseTest {
     }
 }
 
-extension GraphQLLazyLoadL12Tests: DefaultLogger { }
+extension GraphQLLazyLoadCompositePKTests: DefaultLogger { }
 
-extension GraphQLLazyLoadL12Tests {
+extension GraphQLLazyLoadCompositePKTests {
     
-    struct LL12Models: AmplifyModelRegistration {
+    struct CompositePKModels: AmplifyModelRegistration {
         public let version: String = "version"
         func registerModels(registry: ModelRegistry.Type) {
-            ModelRegistry.register(modelType: HasOneParent.self)
-            ModelRegistry.register(modelType: HasOneChild.self)
-            ModelRegistry.register(modelType: DefaultPKParent.self)
-            ModelRegistry.register(modelType: DefaultPKChild.self)
             ModelRegistry.register(modelType: CompositePKParent.self)
             ModelRegistry.register(modelType: CompositePKChild.self)
             ModelRegistry.register(modelType: ImplicitChild.self)
---
+++ AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL12/DefaultPK/GraphQLLazyLoadDefaultPKTests.swift
@@ -0,0 +1,117 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+import Combine
+import XCTest
+
+@testable import Amplify
+import AWSPluginsCore
+
+final class GraphQLLazyLoadDefaultPKTests: GraphQLLazyLoadBaseTest {
+
+    func testConfigure() async throws {
+        await setup(withModels: DefaultPKModels(), logLevel: .verbose)
+    }
+    
+    func testDefaultPKParentChild() async throws {
+        await setup(withModels: DefaultPKModels(), logLevel: .verbose)
+        let defaultPKParent = DefaultPKParent()
+        let savedParent = try await mutate(.create(defaultPKParent))
+        let defaultPKChild = DefaultPKChild(parent: savedParent)
+        let savedChild = try await mutate(.create(defaultPKChild))
+        
+        assertLazyReference(savedChild._parent,
+                            state: .notLoaded(identifiers: [.init(name: "id", value: savedParent.id)]))
+        let loadedParent = try await savedChild.parent
+        assertLazyReference(savedChild._parent,
+                            state: .loaded(model: loadedParent))
+    }
+    
+    func testDefaultParentChildUpdate() async throws {
+        await setup(withModels: DefaultPKModels(), logLevel: .verbose)
+        let defaultPKParent = DefaultPKParent()
+        let savedParent = try await mutate(.create(defaultPKParent))
+        let defaultPKChild = DefaultPKChild(parent: savedParent)
+        var savedChild = try await mutate(.create(defaultPKChild))
+        
+        let newParent = DefaultPKParent()
+        let savedNewParent = try await mutate(.create(newParent))
+        savedChild.setParent(savedNewParent)
+        var updatedChild = try await mutate(.update(savedChild))
+                
+        assertLazyReference(updatedChild._parent,
+                            state: .notLoaded(identifiers: [.init(name: "id", value: newParent.id)]))
+        let loadedParent = try await updatedChild.parent
+        assertLazyReference(updatedChild._parent,
+                            state: .loaded(model: loadedParent))
+    }
+    
+    func testDefaultParentChildDelete() async throws {
+        await setup(withModels: DefaultPKModels(), logLevel: .verbose)
+        let defaultPKParent = DefaultPKParent()
+        let savedParent = try await mutate(.create(defaultPKParent))
+        let defaultPKChild = DefaultPKChild(parent: savedParent)
+        let savedChild = try await mutate(.create(defaultPKChild))
+        
+        try await mutate(.delete(savedParent))
+        try await assertModelDoesNotExist(savedParent)
+        try await assertModelExists(savedChild)
+        try await mutate(.delete(savedChild))
+        try await assertModelDoesNotExist(savedChild)
+    }
+    
+    func testDefaultPKParentChildGet() async throws {
+        await setup(withModels: DefaultPKModels(), logLevel: .verbose)
+        let defaultPKParent = DefaultPKParent()
+        let savedParent = try await mutate(.create(defaultPKParent))
+        let defaultPKChild = DefaultPKChild(parent: savedParent)
+        let savedChild = try await mutate(.create(defaultPKChild))
+        
+        let queriedParent = try await query(.get(DefaultPKParent.self, byId: savedParent.id))!
+        assertList(queriedParent.children!, state: .isNotLoaded(associatedIdentifiers: [queriedParent.id],
+                                                                associatedField: "parent"))
+        try await queriedParent.children?.fetch()
+        assertList(queriedParent.children!, state: .isLoaded(count: 1))
+        
+        let queriedChild = try await query(.get(DefaultPKChild.self, byId: savedChild.id))!
+        assertLazyReference(queriedChild._parent,
+                            state: .notLoaded(identifiers: [.init(name: "id", value: savedParent.id)]))
+        let loadedParent = try await queriedChild.parent
+        assertLazyReference(queriedChild._parent,
+                            state: .loaded(model: loadedParent))
+    }
+    
+    func testDefaultPKParentChildList() async throws {
+        await setup(withModels: DefaultPKModels(), logLevel: .verbose)
+        let defaultPKParent = DefaultPKParent()
+        let savedParent = try await mutate(.create(defaultPKParent))
+        let defaultPKChild = DefaultPKChild(parent: savedParent)
+        let savedChild = try await mutate(.create(defaultPKChild))
+        
+        let queriedParents = try await listQuery(.list(DefaultPKParent.self,
+                                                       where: DefaultPKParent.keys.id == defaultPKParent.id))
+        assertList(queriedParents, state: .isLoaded(count: 1))
+        
+        let queriedChildren = try await listQuery(.list(DefaultPKChild.self,
+                                                        where: DefaultPKChild.keys.id == defaultPKChild.id))
+        assertList(queriedChildren, state: .isLoaded(count: 1))
+    }
+}
+
+extension GraphQLLazyLoadDefaultPKTests: DefaultLogger { }
+
+extension GraphQLLazyLoadDefaultPKTests {
+    
+    struct DefaultPKModels: AmplifyModelRegistration {
+        public let version: String = "version"
+        func registerModels(registry: ModelRegistry.Type) {
+            ModelRegistry.register(modelType: DefaultPKParent.self)
+            ModelRegistry.register(modelType: DefaultPKChild.self)
+        }
+    }
+}
