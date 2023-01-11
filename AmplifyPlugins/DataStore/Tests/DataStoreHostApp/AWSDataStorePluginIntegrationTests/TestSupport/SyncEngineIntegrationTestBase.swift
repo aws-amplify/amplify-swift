daa4a4b311f97bfb66c0d4d93c5b9a03613ecd67
Michael Law <1365977+lawmicha@users.noreply.github.com>
2023-01-11 15:51:54 -0500

    chore: add ds/api integration tests to github workflows (#2664)
    * chore: add ds/api integration tests to github workflows
    
    * address PR comments
    
    * fix lazy loading workflows/jobs

--- AmplifyPlugins/DataStore/Tests/DataStoreHostApp/AWSDataStorePluginIntegrationTests/TestSupport/SyncEngineIntegrationTestBase.swift
+++ AmplifyPlugins/DataStore/Tests/DataStoreHostApp/AWSDataStorePluginIntegrationTests/TestSupport/SyncEngineIntegrationTestBase.swift
@@ -41,7 +41,8 @@ class SyncEngineIntegrationTestBase: DataStoreTestBase {
 
         do {
             try Amplify.add(plugin: AWSAPIPlugin(modelRegistration: models))
-            try Amplify.add(plugin: AWSDataStorePlugin(modelRegistration: models))
+            try Amplify.add(plugin: AWSDataStorePlugin(modelRegistration: models,
+                                                       configuration: .custom(syncMaxRecords: 100)))
         } catch {
             XCTFail(String(describing: error))
             return
