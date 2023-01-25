c4f30bd3535d229e8c2a33aea4326773849abd24
Michael Law <1365977+lawmicha@users.noreply.github.com>
2023-01-25 10:35:36 -0500

    add lazy load tests

--- AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL14/GraphQLLazyLoadUserPostCommentTests.swift
+++ AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL14/GraphQLLazyLoadUserPostCommentTests.swift
@@ -51,21 +51,63 @@ final class GraphQLLazyLoadUserPostCommentTests: GraphQLLazyLoadBaseTest {
         try await mutate(.create(comment))
     }
     
-    func testQueryComment() async throws {
+    /// LazyLoad from queried models
+    ///
+    /// - Given: Created models
+    /// - When:
+    ///    - Querying for models
+    /// - Then:
+    ///    - Traversing from the models to its connected models are successful
+    ///
+    func testLazyLoad() async throws {
         await setup(withModels: UserPostCommentModels())
         let user = User(username: "name")
         let savedUser = try await mutate(.create(user))
+        let userSettings = UserSettings(language: "en-us", user: savedUser)
+        try await mutate(.create(userSettings))
         let post = Post(title: "title", rating: 1, status: .active, author: savedUser)
         let savedPost = try await mutate(.create(post))
         let comment = Comment(content: "content", post: savedPost, author: savedUser)
-        let savedComment = try await mutate(.create(comment))
+        try await mutate(.create(comment))
         
-        let response = try await Amplify.API.query(request: .get(Comment.self, byId: comment.id))
-        guard case .success(let queriedComment) = response,
-              let queriedComment = queriedComment else {
-            return
-        }
-        XCTAssertEqual(queriedComment.id, savedComment.id)
+        // Traverse from User
+        let queriedUser = try await query(for: user)!
+        try await queriedUser.posts?.fetch()
+        XCTAssertEqual(queriedUser.posts?.count, 1)
+        try await queriedUser.comments?.fetch()
+        XCTAssertEqual(queriedUser.comments?.count, 1)
+        // Cannot traverse from User to settings
+        //let queriedUserSettings = try await queriedUser.settings
+        //XCTAssertNotNil(queriedUserSettings)
+        
+        // Traverse from UserSettings
+        let queriedSettings = try await query(for: userSettings)!
+        let queriedSettingsUser = try await queriedSettings.user
+        XCTAssertEqual(queriedSettingsUser.id, user.id)
+
+        // Traverse from Post
+        let queriedPost = try await query(for: post)!
+        try await queriedPost.comments?.fetch()
+        XCTAssertEqual(queriedPost.comments?.count, 1)
+        let queriedPostUser = try await queriedPost.author
+        XCTAssertEqual(queriedPostUser.id, user.id)
+
+        // Traverse from Comment
+        let queriedComment = try await query(for: comment)!
+        let queriedCommentPost = try await queriedComment.post
+        XCTAssertEqual(queriedCommentPost?.id, post.id)
+        let queriedCommentUser = try await queriedComment.author
+        XCTAssertEqual(queriedCommentUser.id, user.id)
+
+        // Clean up - delete the saved models
+        try await mutate(.delete(comment))
+        try await assertModelDoesNotExist(comment)
+        try await mutate(.delete(post))
+        try await assertModelDoesNotExist(post)
+        try await mutate(.delete(userSettings))
+        try await assertModelDoesNotExist(userSettings)
+        try await mutate(.delete(user))
+        try await assertModelDoesNotExist(user)
     }
 }
 
