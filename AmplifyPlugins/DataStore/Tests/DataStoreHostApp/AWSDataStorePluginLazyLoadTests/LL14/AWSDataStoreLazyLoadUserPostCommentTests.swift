5339d99e1e470a14a43922a1fd258a3c5e926276
Michael Law <1365977+lawmicha@users.noreply.github.com>
2023-01-25 10:35:36 -0500

    add lazy load tests

--- AmplifyPlugins/DataStore/Tests/DataStoreHostApp/AWSDataStorePluginLazyLoadTests/LL14/AWSDataStoreLazyLoadUserPostCommentTests.swift
+++ AmplifyPlugins/DataStore/Tests/DataStoreHostApp/AWSDataStorePluginLazyLoadTests/LL14/AWSDataStoreLazyLoadUserPostCommentTests.swift
@@ -47,9 +47,64 @@ final class AWSDataStoreLazyLoadUserPostCommentTests: AWSDataStoreLazyLoadBaseTe
         let savedUser = try await saveAndWaitForSync(user)
         let post = Post(title: "title", rating: 1, status: .active, author: savedUser)
         let savedPost = try await saveAndWaitForSync(post)
-        
         let comment = Comment(content: "content", post: savedPost, author: savedUser)
         try await saveAndWaitForSync(comment)
+    }
+    
+    /// LazyLoad from queried models
+    ///
+    /// - Given: Saved and synced models
+    /// - When:
+    ///    - Querying for models
+    /// - Then:
+    ///    - Traversing from the models to its connected models are successful
+    ///
+    func testLazyLoad() async throws {
+        await setup(withModels: UserPostCommentModels())
+        let user = User(username: "name")
+        let savedUser = try await saveAndWaitForSync(user)
+        let userSettings = UserSettings(language: "en-us", user: savedUser)
+        try await saveAndWaitForSync(userSettings)
+        let post = Post(title: "title", rating: 1, status: .active, author: savedUser)
+        let savedPost = try await saveAndWaitForSync(post)
+        let comment = Comment(content: "content", post: savedPost, author: savedUser)
+        try await saveAndWaitForSync(comment)
+        
+        // Traverse from User
+        let queriedUser = try await query(for: user)
+        try await queriedUser.posts?.fetch()
+        XCTAssertEqual(queriedUser.posts?.count, 1)
+        try await queriedUser.comments?.fetch()
+        XCTAssertEqual(queriedUser.comments?.count, 1)
+        // Cannot traverse from User to settings
+        //let queriedUserSettings = try await queriedUser.settings
+        //XCTAssertNotNil(queriedUserSettings)
+        
+        // Traverse from UserSettings
+        let queriedSettings = try await query(for: userSettings)
+        let queriedSettingsUser = try await queriedSettings.user
+        XCTAssertEqual(queriedSettingsUser.id, user.id)
+        
+        // Traverse from Post
+        let queriedPost = try await query(for: post)
+        try await queriedPost.comments?.fetch()
+        XCTAssertEqual(queriedPost.comments?.count, 1)
+        let queriedPostUser = try await queriedPost.author
+        XCTAssertEqual(queriedPostUser.id, user.id)
+        
+        // Traverse from Comment
+        let queriedComment = try await query(for: comment)
+        let queriedCommentPost = try await queriedComment.post
+        XCTAssertEqual(queriedCommentPost?.id, post.id)
+        let queriedCommentUser = try await queriedComment.author
+        XCTAssertEqual(queriedCommentUser.id, user.id)
+        
+        // Clean up - delete the saved models
+        try await deleteAndWaitForSync(user)
+        try await assertModelDoesNotExist(comment)
+        try await assertModelDoesNotExist(post)
+        try await assertModelDoesNotExist(userSettings)
+        try await assertModelDoesNotExist(user)
     }
 }
 
