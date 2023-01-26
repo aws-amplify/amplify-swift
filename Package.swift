ff661de1b95ffc96af504523642fd677408ce81c
Tuan Pham <103537251+phantumcode@users.noreply.github.com>
2023-01-26 10:34:18 -0600

    chore: update AWS iOS SDK dependency to latest (#2703)
    * chore: update sdk dependency to latest
    
    * chore: update podfiles

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         
     ],
     dependencies: [
-        .package(name: "AWSiOSSDKV2", url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", .upToNextMinor(from: "2.29.1")),
+        .package(name: "AWSiOSSDKV2", url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", .upToNextMinor(from: "2.30.1")),
         .package(name: "AppSyncRealTimeClient", url: "https://github.com/aws-amplify/aws-appsync-realtime-client-ios.git", from: "2.0.0"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", .exact("0.13.2"))
     ],
