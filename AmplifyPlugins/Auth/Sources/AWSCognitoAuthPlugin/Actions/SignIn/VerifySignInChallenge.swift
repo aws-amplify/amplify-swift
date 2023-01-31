4d3d4243fc030082888a513f719d86839aec36c7
Jithin Roy <51138777+royjit@users.noreply.github.com>
2023-01-31 10:11:27 -0800

    fix(auth): Build failure due to typo in device binding (#2719)

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Actions/SignIn/VerifySignInChallenge.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Actions/SignIn/VerifySignInChallenge.swift
@@ -57,7 +57,7 @@ struct VerifySignInChallenge: Action {
         } catch let error where deviceNotFound(error: error, deviceMetadata: deviceMetadata) {
             logVerbose("\(#fileID) Received device not found \(error)", environment: environment)
             // Remove the saved device details and retry verify challenge
-            await DeviceMetadataHelper.removeDeviceMetaData(of: username, environment: environment)
+            await DeviceMetadataHelper.removeDeviceMetaData(for: username, with: environment)
             let event = SignInChallengeEvent(
                 eventType: .retryVerifyChallengeAnswer(confirmSignEventData)
             )
