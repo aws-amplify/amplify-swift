7766f49ff1b817e6c98c405dfbf8ffe5e29fddd5
Di Wu <diwud@amazon.com>
2023-02-08 22:57:15 -0800

    chore(test): add more integration test cases for lazy loading case#12

--- AmplifyPlugins/DataStore/Tests/DataStoreHostApp/AWSDataStorePluginLazyLoadTests/LL12/HasOneParentChild/AWSDataStoreLazyLoadHasOneTests.swift
+++ AmplifyPlugins/DataStore/Tests/DataStoreHostApp/AWSDataStorePluginLazyLoadTests/LL12/HasOneParentChild/AWSDataStoreLazyLoadHasOneTests.swift
@@ -22,13 +22,13 @@ class AWSDataStoreLazyLoadHasOneTests: AWSDataStoreLazyLoadBaseTest {
     func testSaveHasOneParent() async throws {
         await setup(withModels: HasOneModels())
         let parent = HasOneParent()
-        let savedParent = try await saveAndWaitForSync(parent)
+        try await saveAndWaitForSync(parent)
     }
     
     func testSaveHasOneChild() async throws {
         await setup(withModels: HasOneModels())
         let child = HasOneChild()
-        let savedChild = try await saveAndWaitForSync(child)
+        try await saveAndWaitForSync(child)
     }
     
     func testSaveParentWithChild() async throws {
@@ -77,36 +77,119 @@ class AWSDataStoreLazyLoadHasOneTests: AWSDataStoreLazyLoadBaseTest {
     }
 
     func testUpdateParentWithNewChild() async throws {
-            await setup(withModels: HasOneModels())
-            let child = HasOneChild()
-            let savedChild = try await saveAndWaitForSync(child)
-            let parent = HasOneParent(child: savedChild, hasOneParentChildId: savedChild.id)
-            let savedParent = try await saveAndWaitForSync(parent)
+        await setup(withModels: HasOneModels())
+        let child = HasOneChild()
+        let savedChild = try await saveAndWaitForSync(child)
+        let parent = HasOneParent(child: savedChild, hasOneParentChildId: savedChild.id)
+        let savedParent = try await saveAndWaitForSync(parent)
 
-            var queriedParent = try await query(for: savedParent)
-            XCTAssertEqual(queriedParent.hasOneParentChildId, savedChild.id)
+        var queriedParent = try await query(for: savedParent)
+        XCTAssertEqual(queriedParent.hasOneParentChildId, savedChild.id)
 
-            // The child can be lazy loaded.
-            assertLazyReference(queriedParent._child,
-                                state: .notLoaded(identifiers: [
-                                    .init(name: HasOneChild.keys.id.stringValue, value: child.id)
-                                ]))
+        // The child can be lazy loaded.
+        assertLazyReference(queriedParent._child,
+                            state: .notLoaded(identifiers: [
+                                .init(name: HasOneChild.keys.id.stringValue, value: child.id)
+                            ]))
 
-            let newChild = HasOneChild()
-            let savedNewChild = try await saveAndWaitForSync(newChild)
+        let newChild = HasOneChild()
+        let savedNewChild = try await saveAndWaitForSync(newChild)
 
-            // Update parent to new child
-            queriedParent.setChild(savedNewChild)
-            queriedParent.hasOneParentChildId = savedNewChild.id
-            try await updateAndWaitForSync(queriedParent)
+        // Update parent to new child
+        queriedParent.setChild(savedNewChild)
+        queriedParent.hasOneParentChildId = savedNewChild.id
+        try await updateAndWaitForSync(queriedParent)
 
-            let queriedParentWithNewChild = try await query(for: parent)
-            // The child can be lazy loaded.
-            assertLazyReference(queriedParentWithNewChild._child, state: .notLoaded(identifiers: [
-                .init(name: HasOneChild.keys.id.stringValue, value: savedNewChild.id)
-            ]))
-            XCTAssertEqual(queriedParentWithNewChild.hasOneParentChildId, savedNewChild.id)
+        let queriedParentWithNewChild = try await query(for: parent)
+        // The child can be lazy loaded.
+        assertLazyReference(queriedParentWithNewChild._child, state: .notLoaded(identifiers: [
+            .init(name: HasOneChild.keys.id.stringValue, value: savedNewChild.id)
+        ]))
+        XCTAssertEqual(queriedParentWithNewChild.hasOneParentChildId, savedNewChild.id)
+    }
+
+    func testObserveHasOneChild() async throws {
+        await setup(withModels: HasOneModels())
+        try await startAndWaitForReady()
+        let child = HasOneChild()
+        let mutationEventReceived = asyncExpectation(description: "Received mutation event")
+        let mutationEvents = Amplify.DataStore.observe(HasOneChild.self)
+
+        Task {
+            for try await mutationEvent in mutationEvents {
+                if let version = mutationEvent.version,
+                   version == 1,
+                   let receivedChild = try? mutationEvent.decodeModel(as: HasOneChild.self),
+                   receivedChild.identifier == child.identifier
+                {
+                    await mutationEventReceived.fulfill()
+                }
+            }
         }
+
+        let createRequest = GraphQLRequest<MutationSyncResult>.createMutation(of: child, modelSchema: HasOneChild.schema)
+        do {
+            _ = try await Amplify.API.mutate(request: createRequest)
+        } catch {
+            XCTFail("Failed to send mutation request \(error)")
+        }
+
+        await waitForExpectations([mutationEventReceived], timeout: 60)
+        mutationEvents.cancel()
+    }
+
+    func testDeleteHasOneChild() async throws {
+        await setup(withModels: HasOneModels())
+        let child = HasOneChild()
+        let savedChild = try await saveAndWaitForSync(child)
+        try await assertModelExists(savedChild)
+
+        try await deleteAndWaitForSync(savedChild)
+        try await assertModelDoesNotExist(savedChild)
+    }
+
+    func testDeleteHasOneParent() async throws {
+        await setup(withModels: HasOneModels())
+        let parent = HasOneParent()
+        let savedParent = try await saveAndWaitForSync(parent)
+        try await assertModelExists(savedParent)
+
+        try await deleteAndWaitForSync(savedParent)
+        try await assertModelDoesNotExist(savedParent)
+    }
+
+    func testDeleteHasOneParentWithChild() async throws {
+        await setup(withModels: HasOneModels())
+        let child = HasOneChild()
+        let savedChild = try await saveAndWaitForSync(child)
+        let parent = HasOneParent(child: savedChild, hasOneParentChildId: savedChild.id)
+        let savedParent = try await saveAndWaitForSync(parent)
+
+        let queriedParent = try await query(for: savedParent)
+        XCTAssertEqual(queriedParent.hasOneParentChildId, savedChild.id)
+
+        try await deleteAndWaitForSync(savedParent)
+        try await assertModelDoesNotExist(savedParent)
+        try await assertModelExists(savedChild)
+    }
+
+    func testDeleteHasOneChildWithParent() async throws {
+        await setup(withModels: HasOneModels())
+        let child = HasOneChild()
+        let savedChild = try await saveAndWaitForSync(child)
+        let parent = HasOneParent(child: savedChild, hasOneParentChildId: savedChild.id)
+        let savedParent = try await saveAndWaitForSync(parent)
+
+        let queriedParent = try await query(for: savedParent)
+        XCTAssertEqual(queriedParent.hasOneParentChildId, savedChild.id)
+
+        try await deleteAndWaitForSync(savedChild)
+        try await assertModelDoesNotExist(savedChild)
+        try await assertModelExists(savedParent)
+
+        let childIdInParent = try await query(for: savedParent).child?.id
+        XCTAssertEqual(childIdInParent, nil)
+    }
 
 }
 
