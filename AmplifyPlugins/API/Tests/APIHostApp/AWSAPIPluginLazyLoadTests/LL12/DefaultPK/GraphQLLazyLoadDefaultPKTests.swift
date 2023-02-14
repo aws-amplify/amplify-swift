39e7de3802865f05878e6bb60c7e566115ed5423
Di Wu <diwud@amazon.com>
2023-02-14 15:24:29 -0800

    chore(test): add API subscription integration test case for LazyLoading

--- AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL12/DefaultPK/GraphQLLazyLoadDefaultPKTests.swift
+++ AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL12/DefaultPK/GraphQLLazyLoadDefaultPKTests.swift
@@ -101,6 +101,278 @@ final class GraphQLLazyLoadDefaultPKTests: GraphQLLazyLoadBaseTest {
                                                         where: DefaultPKChild.keys.id == defaultPKChild.id))
         assertList(queriedChildren, state: .isLoaded(count: 1))
     }
+
+    /*
+     - Given: Api category setup with DefaultPKModels
+     - When:
+        - Subscribe onCreate events of DefaultPKChild
+        - Create new DefaultPKChild instance with API
+     - Then:
+        - the newly created instance is successfully created through API. onCreate event is received.
+     */
+    func testSubscribeDefaultPKChildOnCreate() async throws {
+        await setup(withModels: DefaultPKModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onCreate = asyncExpectation(description: "onCreate received")
+        let child = DefaultPKChild()
+        let subscription = Amplify.API.subscribe(request: .subscription(of: DefaultPKChild.self, type: .onCreate))
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        if newModel.identifier == child.identifier {
+                            await onCreate.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to create DefaultPKChild, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        await waitForExpectations([onCreate], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with DefaultPKModels
+     - When:
+        - Subscribe onCreate events of DefaultPKParent
+        - Create new DefaultPKChild and DefaultPKParent instances with API
+     - Then:
+        - the newly created parent is successfully created through API. onCreate event is received.
+     */
+    func testSubscribeDefaultPKParentOnCreate() async throws {
+        await setup(withModels: DefaultPKModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onCreate = asyncExpectation(description: "onCreate received")
+        let parent = DefaultPKParent()
+        let child = DefaultPKChild(parent: parent)
+        let subscription = Amplify.API.subscribe(request: .subscription(of: DefaultPKParent.self, type: .onCreate))
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        try await newModel.children?.fetch()
+                        let associatedChilden = newModel.children?.loadedState
+                        if newModel.identifier == parent.identifier,
+                           case .some(.loaded(let associatedChilden)) = associatedChilden,
+                           associatedChilden.map(\.identifier).contains(child.identifier)
+                        {
+                            await onCreate.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to create DefaultPKParent, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        try await mutate(.create(parent))
+        await waitForExpectations([onCreate], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with DefaultPKModels
+     - When:
+        - Subscribe onUpdate events of DefaultPKChild
+        - Create new DefaultPKChild instance with API
+        - Update the newly created with API
+     - Then:
+        - an onUpdate event is received, the identifier is same to the updated one.
+     */
+    func testSubscriptionDefaultPKChildOnUpdate() async throws {
+        await setup(withModels: DefaultPKModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onUpdate = asyncExpectation(description: "onUpdate received")
+        let child = DefaultPKChild()
+        let subscription = Amplify.API.subscribe(request: .subscription(of: DefaultPKChild.self, type: .onUpdate))
+
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        if newModel.identifier == child.identifier {
+                            await onUpdate.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to update DefaultPKChild, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        var updatingChild = child
+        updatingChild.content = UUID().uuidString
+        try await mutate(.update(updatingChild))
+        await waitForExpectations([onUpdate], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with DefaultPKModels
+     - When:
+        - Subscribe onUpdate events of HasOneParent
+        - Create new DefaultPKChild instance with API
+        - Create new DefaultPKParent instance with API
+        - Update the newly created parent to another DefaultPKChild instance
+     - Then:
+        - an onUpdate event is received, the identifier is same to the updated one.
+     */
+    func testSubscriptionDefaultPKParentOnUpdate() async throws {
+        await setup(withModels: DefaultPKModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onUpdate = asyncExpectation(description: "onUpdate received")
+
+        let parent = DefaultPKParent()
+        let child = DefaultPKChild(parent: parent)
+        let anotherChild = DefaultPKChild(parent: parent)
+
+        let subscription = Amplify.API.subscribe(request: .subscription(of: DefaultPKParent.self, type: .onUpdate))
+
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        try await newModel.children?.fetch()
+                        let associatedChilden = newModel.children?.loadedState
+                        if newModel.identifier == parent.identifier,
+                           case .some(.loaded(let associatedChilden)) = associatedChilden,
+                           associatedChilden.map(\.identifier).contains(child.identifier),
+                           associatedChilden.map(\.identifier).contains(anotherChild.identifier)
+                        {
+                            await onUpdate.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to update HasOneParent, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        try await mutate(.create(parent))
+        try await mutate(.create(anotherChild))
+        var updatingParent = parent
+        updatingParent.content = UUID().uuidString
+        try await mutate(.update(updatingParent))
+        await waitForExpectations([onUpdate], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with DefaultPKModels
+     - When:
+        - Subscribe onDelete events of DefaultPKChild
+        - Create new DefaultPKChild instance with API
+        - Delete the newly created one with API
+     - Then:
+        - an onDelete event is received, the identifier is same to the newly created one.
+     */
+    func testSubscriptionDefaultPKChildOnDelete() async throws {
+        await setup(withModels: DefaultPKModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onDelete = asyncExpectation(description: "onDelete received")
+        let child = DefaultPKChild()
+        let subscription = Amplify.API.subscribe(request: .subscription(of: DefaultPKChild.self, type: .onDelete))
+
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        if newModel.identifier == child.identifier {
+                            await onDelete.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to delete DefaultPKChild, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        try await mutate(.delete(child))
+        await waitForExpectations([onDelete], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with DefaultPKModels
+     - When:
+        - Subscribe onDelete events of DefaultPKParent
+        - Create new DefaultPKChild instance with API
+        - Create new DefaultPKParent instance with API
+        - Delete the newly created parent with API
+     - Then:
+        - an onDelete event is received, the identifier is same to the newly created one.
+     */
+    func testSubscriptionDefaultPKParentOnDelete() async throws {
+        await setup(withModels: DefaultPKModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onDelete = asyncExpectation(description: "onDelete received")
+        let parent = DefaultPKParent()
+        let child = DefaultPKChild(parent: parent)
+        let subscription = Amplify.API.subscribe(request: .subscription(of: DefaultPKParent.self, type: .onDelete))
+
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        try await newModel.children?.fetch()
+                        let associatedChilden = newModel.children?.loadedState
+                        if newModel.identifier == parent.identifier,
+                           case .some(.loaded(let associatedChildren)) = associatedChilden,
+                           associatedChildren.map(\.identifier).contains(child.identifier)
+                        {
+                            await onDelete.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to delete DefaultPKParent, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        try await mutate(.create(parent))
+        try await mutate(.delete(parent))
+        await waitForExpectations([onDelete], timeout: 10)
+        subscription.cancel()
+    }
 }
 
 extension GraphQLLazyLoadDefaultPKTests: DefaultLogger { }
--- AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL12/HasOneParentChild/GraphQLLazyLoadHasOneTests.swift
+++ AmplifyPlugins/API/Tests/APIHostApp/AWSAPIPluginLazyLoadTests/LL12/HasOneParentChild/GraphQLLazyLoadHasOneTests.swift
@@ -97,6 +97,270 @@ final class GraphQLLazyLoadHasOneTests: GraphQLLazyLoadBaseTest {
                                                        where: HasOneChild.keys.id == hasOneChild.id))
         assertList(queriedChildren, state: .isLoaded(count: 1))
     }
+
+    /*
+     - Given: Api category setup with HasOneParentChildModels
+     - When:
+        - Subscribe onCreate events of HasOneChild
+        - Create new HasOneChild instance with API
+     - Then:
+        - the newly created instance is successfully created through API. onCreate event is received.
+     */
+    func testSubscribeHasOneChildOnCreate() async throws {
+        await setup(withModels: HasOneParentChildModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onCreate = asyncExpectation(description: "onCreate received")
+        let child = HasOneChild()
+        let subscription = Amplify.API.subscribe(request: .subscription(of: HasOneChild.self, type: .onCreate))
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        if newModel.identifier == child.identifier {
+                            await onCreate.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to create HasOneChild, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        await waitForExpectations([onCreate], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with HasOneParentChildModels
+     - When:
+        - Subscribe onCreate events of HasOneParent
+        - Create new HasOneChild and HasOneParent instances with API
+     - Then:
+        - the newly created parent is successfully created through API. onCreate event is received.
+     */
+    func testSubscribeHasOneParentOnCreate() async throws {
+        await setup(withModels: HasOneParentChildModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onCreate = asyncExpectation(description: "onCreate received")
+        let child = HasOneChild()
+        let parent = HasOneParent(child: child, hasOneParentChildId: child.id)
+        let subscription = Amplify.API.subscribe(request: .subscription(of: HasOneParent.self, type: .onCreate))
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        let associatedChild = try await newModel.child
+                        if newModel.identifier == parent.identifier,
+                           associatedChild?.identifier == child.identifier
+                        {
+                            await onCreate.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to create HasOneParent, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        try await mutate(.create(parent))
+        await waitForExpectations([onCreate], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with HasOneParentChildModels
+     - When:
+        - Subscribe onUpdate events of HasOneChild
+        - Create new HasOneChild instance with API
+        - Update the newly created with API
+     - Then:
+        - an onUpdate event is received, the identifier is same to the updated one.
+     */
+    func testSubscriptionHasOnChildOnUpdate() async throws {
+        await setup(withModels: HasOneParentChildModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onUpdate = asyncExpectation(description: "onUpdate received")
+        let child = HasOneChild()
+        let subscription = Amplify.API.subscribe(request: .subscription(of: HasOneChild.self, type: .onUpdate))
+
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        if newModel.identifier == child.identifier {
+                            await onUpdate.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to update HasOneChild, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        var updatingChild = child
+        updatingChild.content = UUID().uuidString
+        try await mutate(.update(updatingChild))
+        await waitForExpectations([onUpdate], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with HasOneParentChildModels
+     - When:
+        - Subscribe onUpdate events of HasOneParent
+        - Create new HasOneChild instance with API
+        - Create new HasOneParent instance with API
+        - Update the newly created parent to another HasOneChild instance
+     - Then:
+        - an onUpdate event is received, the identifier is same to the updated one.
+     */
+    func testSubscriptionHasOnParentOnUpdate() async throws {
+        await setup(withModels: HasOneParentChildModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onUpdate = asyncExpectation(description: "onUpdate received")
+        let child = HasOneChild()
+        let anotherChild = HasOneChild()
+        let parent = HasOneParent(child: child, hasOneParentChildId: child.id)
+        let subscription = Amplify.API.subscribe(request: .subscription(of: HasOneParent.self, type: .onUpdate))
+
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        let associatedChild = try await newModel.child
+                        if newModel.identifier == parent.identifier,
+                           associatedChild?.identifier == anotherChild.identifier
+                        {
+                            await onUpdate.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to update HasOneParent, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        try await mutate(.create(parent))
+        var updatingParent = parent
+        updatingParent.setChild(anotherChild)
+        updatingParent.hasOneParentChildId = anotherChild.id
+        try await mutate(.create(anotherChild))
+        try await mutate(.update(updatingParent))
+        await waitForExpectations([onUpdate], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with HasOneParentChildModels
+     - When:
+        - Subscribe onDelete events of HasOneChild
+        - Create new HasOneChild instance with API
+        - Delete the newly created one with API
+     - Then:
+        - an onDelete event is received, the identifier is same to the newly created one.
+     */
+    func testSubscriptionHasOneChildOnDelete() async throws {
+        await setup(withModels: HasOneParentChildModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onDelete = asyncExpectation(description: "onDelete received")
+        let child = HasOneChild()
+        let subscription = Amplify.API.subscribe(request: .subscription(of: HasOneChild.self, type: .onDelete))
+
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        if newModel.identifier == child.identifier {
+                            await onDelete.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to delete HasOneChild, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        try await mutate(.delete(child))
+        await waitForExpectations([onDelete], timeout: 10)
+        subscription.cancel()
+    }
+
+    /*
+     - Given: Api category setup with HasOneParentChildModels
+     - When:
+        - Subscribe onDelete events of HasOneParent
+        - Create new HasOneChild instance with API
+        - Create new HasOneParent instance with API
+        - Delete the newly created parent with API
+     - Then:
+        - an onDelete event is received, the identifier is same to the newly created one.
+     */
+    func testSubscriptionHasOneParentOnDelete() async throws {
+        await setup(withModels: HasOneParentChildModels())
+        let connected = asyncExpectation(description: "Subscription connected")
+        let onDelete = asyncExpectation(description: "onDelete received")
+        let child = HasOneChild()
+        let parent = HasOneParent(child: child, hasOneParentChildId: child.id)
+        let subscription = Amplify.API.subscribe(request: .subscription(of: HasOneParent.self, type: .onDelete))
+
+        Task {
+            do {
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(.connected):
+                        await connected.fulfill()
+                    case let .data(.success(newModel)):
+                        let associatedChild = try await newModel.child
+                        if newModel.identifier == parent.identifier,
+                           associatedChild?.identifier == child.identifier
+                        {
+                            await onDelete.fulfill()
+                        }
+                    case let .data(.failure(error)):
+                        XCTFail("Failed to delete HasOneParent, error: \(error.errorDescription)")
+                    default: ()
+                    }
+                }
+            }
+        }
+
+        await waitForExpectations([connected], timeout: 10)
+        try await mutate(.create(child))
+        try await mutate(.create(parent))
+        try await mutate(.delete(parent))
+        await waitForExpectations([onDelete], timeout: 10)
+        subscription.cancel()
+    }
 }
 
 extension GraphQLLazyLoadHasOneTests: DefaultLogger { }
