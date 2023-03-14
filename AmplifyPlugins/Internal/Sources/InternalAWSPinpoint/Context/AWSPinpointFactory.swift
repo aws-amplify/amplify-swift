77d187cd6305a237e0cd9d7d791b4b55d58e02b4
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2023-03-14 17:52:57 -0400

    fix(PushNotifications): Checking if there is a Provisioning Profile with the APS Entitlement in order to determine ChannelType.

--- AmplifyPlugins/Internal/Sources/InternalAWSPinpoint/Context/AWSPinpointFactory.swift
+++ AmplifyPlugins/Internal/Sources/InternalAWSPinpoint/Context/AWSPinpointFactory.swift
@@ -29,10 +29,18 @@ public class AWSPinpointFactory {
             return existingContext
         }
 
-        var isDebug = false
+        var isDebug: Bool
+        /// Check for the APS Environment entitlement in a provisioning profile first
+        if let apsEnvironment = ProvisioningProfileReader().apsEnvironment() {
+            isDebug = apsEnvironment == .development
+        } else {
+        /// Fallback to the DEBUG flag
         #if DEBUG
             isDebug = true
+        #else
+            isDebug = false
         #endif
+        }
         let configuration = PinpointContextConfiguration(
             appId: appId,
             region: region,
@@ -43,5 +51,84 @@ public class AWSPinpointFactory {
         let pinpointContext = try PinpointContext(with: configuration)
         instances[key] = pinpointContext
         return pinpointContext
+    }
+}
+
+private struct ProvisioningProfileReader {
+    enum APSEnvironment: String {
+        case development
+        case production
+    }
+
+    private struct Keys {
+        static let entitlements = "Entitlements"
+        static var apsEnvironment: String {
+        #if os(macOS)
+            return "com.apple.developer.aps-environment"
+        #else
+            return "aps-environment"
+        #endif
+        }
+    }
+
+    private let fileName = "embedded"
+    private var fileExtension: String = {
+    #if os(macOS)
+        return "provisionprofile"
+    #else
+        return "mobileprovision"
+    #endif
+    }()
+
+    private var url: URL? {
+#if os(macOS)
+        if let url = Bundle.main.url(forResource: fileName, withExtension: fileExtension) {
+            return url
+        }
+
+        guard let enumerator =  FileManager.default.enumerator(
+            at: Bundle.main.bundleURL,
+            includingPropertiesForKeys: [.isRegularFileKey],
+            options: [.skipsHiddenFiles, .skipsPackageDescendants]
+        ) else {
+            return nil
+        }
+
+        let provisioningFile = "\(fileName).\(fileExtension)"
+        for case let url as URL in enumerator where provisioningFile == url.lastPathComponent {
+            return url
+        }
+
+        return nil
+#else
+        return Bundle.main.url(forResource: fileName, withExtension: fileExtension)
+#endif
+    }
+
+    private var contents: Data? {
+        guard let url = url,
+              let contents =  try? String(contentsOf: url, encoding: .isoLatin1) else {
+            return nil
+        }
+
+        let scanner = Scanner(string: contents)
+        guard scanner.scanUpToString("<plist") != nil,
+              let plist = scanner.scanUpToString("</plist>") else {
+            return nil
+        }
+
+        return "\(plist)</plist>".data(using: .isoLatin1)
+    }
+
+    func apsEnvironment() -> APSEnvironment? {
+        if let contents = contents,
+           let provisioning = try? PropertyListSerialization.propertyList(from: contents,
+                                                                          format: nil) as? [String: Any],
+           let entitlements = provisioning[Keys.entitlements] as? [String: Any],
+           let apnsEnvironment = entitlements[Keys.apsEnvironment] as? String {
+            return APSEnvironment(rawValue: apnsEnvironment)
+        }
+
+        return nil
     }
 }
