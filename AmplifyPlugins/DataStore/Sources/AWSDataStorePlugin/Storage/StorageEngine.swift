0a9f9c011286695be71439cff5ec8a4194da1464
Jithin Roy <51138777+royjit@users.noreply.github.com>
2023-03-17 19:32:08 -0700

    Fix typo

--- AmplifyPlugins/DataStore/Sources/AWSDataStorePlugin/Storage/StorageEngine.swift
+++ AmplifyPlugins/DataStore/Sources/AWSDataStorePlugin/Storage/StorageEngine.swift
@@ -65,7 +65,6 @@ final class StorageEngine: StorageEngineBehavior {
         return [
             ModelSyncMetadata.schema,
             MutationEvent.schema,
-            MutationSyncMetadata.schema.name
             MutationSyncMetadata.schema
         ]
     }
