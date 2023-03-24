37a416f01b5ae367c76ef4e7f71b4d840d97d1a0
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-03-24 16:40:12 -0400

    Added new unit tests for ConfirmSignUp Input

---
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/ResolverTests/SignUpState/ConfirmSignUpInputTests.swift
@@ -0,0 +1,64 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import XCTest
+
+@testable import AWSCognitoAuthPlugin
+
+import AWSCognitoIdentityProvider
+
+class ConfirmSignUpInputTests: XCTestCase {
+
+    func testSignUpInputWithClientSecret() throws {
+        let username = "jeff"
+        let clientSecret = UUID().uuidString
+        let userPoolConfiguration = UserPoolConfigurationData(poolId: "",
+                                                              clientId: "123456",
+                                                              region: "",
+                                                              clientSecret: clientSecret)
+        let environment = BasicUserPoolEnvironment(
+            userPoolConfiguration: userPoolConfiguration,
+            cognitoUserPoolFactory: Defaults.makeDefaultUserPool,
+            cognitoUserPoolASFFactory: Defaults.makeDefaultASF,
+            cognitoUserPoolAnalyticsHandlerFactory: Defaults.makeUserPoolAnalytics)
+
+        let confirmSignUpInput = ConfirmSignUpInput(
+            username: username,
+            confirmationCode: "123",
+            clientMetadata: [:],
+            asfDeviceId: "asdfDeviceId",
+            environment: environment)
+
+        XCTAssertNotNil(confirmSignUpInput.secretHash)
+        XCTAssertNotNil(confirmSignUpInput.userContextData)
+    }
+
+    func testSignUpInputWithoutClientSecret() throws {
+        let username = "jeff"
+        let password = "a2z"
+
+        let userPoolConfiguration = UserPoolConfigurationData(poolId: "",
+                                                              clientId: "123456",
+                                                              region: "",
+                                                              clientSecret: nil)
+        let environment = BasicUserPoolEnvironment(
+            userPoolConfiguration: userPoolConfiguration,
+            cognitoUserPoolFactory: Defaults.makeDefaultUserPool,
+            cognitoUserPoolASFFactory: Defaults.makeDefaultASF,
+            cognitoUserPoolAnalyticsHandlerFactory: Defaults.makeUserPoolAnalytics)
+
+        let confirmSignUpInput = ConfirmSignUpInput(
+            username: username,
+            confirmationCode: "123",
+            clientMetadata: [:],
+            asfDeviceId: nil,
+            environment: environment)
+
+        XCTAssertNil(confirmSignUpInput.secretHash)
+        XCTAssertNil(confirmSignUpInput.userContextData)
+    }
+}
