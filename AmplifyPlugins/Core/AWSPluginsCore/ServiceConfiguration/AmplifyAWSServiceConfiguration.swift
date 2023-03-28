ebd555fe97b40709ee25d0c6c62e83cc47b5a19b
aws-amplify-ops <aws-amplify-ops@amazon.com>
2023-03-28 17:48:41 +0000

    chore: release 2.7.0 [skip ci]

--- AmplifyPlugins/Core/AWSPluginsCore/ServiceConfiguration/AmplifyAWSServiceConfiguration.swift
+++ AmplifyPlugins/Core/AWSPluginsCore/ServiceConfiguration/AmplifyAWSServiceConfiguration.swift
@@ -9,7 +9,7 @@ import Foundation
 import AWSClientRuntime
 
 public class AmplifyAWSServiceConfiguration {
-    public static let amplifyVersion = "2.6.1"
+    public static let amplifyVersion = "2.7.0"
     public static let platformName = "amplify-ios"
 
     public static func frameworkMetaData() -> FrameworkMetadata {
