8410fea6c270589c77bcf847c4fcefc13a315d13
JC Jimenez <1117904+jcjimenez@users.noreply.github.com>
2023-04-03 11:21:23 -0500

    chore(storage): Added request swiftdoc tags

--- Amplify/Categories/Storage/StorageAccessLevel.swift
+++ Amplify/Categories/Storage/StorageAccessLevel.swift
@@ -9,14 +9,22 @@ import Foundation
 
 /// The access level for objects in Storage operations.
 /// See https://aws-amplify.github.io/docs/ios/storage#storage-access
+///
+/// - Tag: StorageAccessLevel
 public enum StorageAccessLevel: String {
 
     /// Objects can be read or written by any user without authentication
+    ///
+    /// - Tag: StorageAccessLevel.guest
     case guest
 
     /// Objects can be viewed by any user without authentication, but only written by the owner
+    ///
+    /// - Tag: StorageAccessLevel.protected
     case protected
 
     /// Objects can only be read and written by the owner
+    ///
+    /// - Tag: StorageAccessLevel.private
     case `private`
 }
