bb2e775533da7e090ab185a721b3cb870e94c513
aws-amplify-ops <aws-amplify-ops@amazon.com>
2023-04-14 17:23:04 +0000

    chore: release 1.29.2 [skip ci]

--- AmplifyPlugins.podspec
+++ AmplifyPlugins.podspec
@@ -7,7 +7,7 @@
 #
 
 # Version definitions
-$AMPLIFY_VERSION = '1.29.1'
+$AMPLIFY_VERSION = '1.29.2'
 $AMPLIFY_RELEASE_TAG = "#{$AMPLIFY_VERSION}"
 
 $AWS_SDK_VERSION = '2.30.1'
--- AmplifyPlugins/Core/AWSPluginsCore/ServiceConfiguration/AmplifyAWSServiceConfiguration.swift
+++ AmplifyPlugins/Core/AWSPluginsCore/ServiceConfiguration/AmplifyAWSServiceConfiguration.swift
@@ -9,7 +9,7 @@ import Foundation
 import AWSCore
 
 public class AmplifyAWSServiceConfiguration: AWSServiceConfiguration {
-    static let version = "1.29.1"
+    static let version = "1.29.2"
 
     override public class func baseUserAgent() -> String! {
         let platformInfo = AmplifyAWSServiceConfiguration.platformInformation()
