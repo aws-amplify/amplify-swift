8793fcf77802363cf6b7b9b3d98b9d17ef5894d5
Chris Ameter <29186822+ameter@users.noreply.github.com>
2023-04-20 14:02:21 -0600

    fix ActivityTracker for watchOS buils

--- AmplifyPlugins/Internal/Sources/InternalAWSPinpoint/Session/ActivityTracking/ActivityTracker.swift
+++ AmplifyPlugins/Internal/Sources/InternalAWSPinpoint/Session/ActivityTracking/ActivityTracker.swift
@@ -7,9 +7,11 @@
 
 import Amplify
 import Foundation
-#if canImport(UIKit)
+#if canImport(WatchKit)
+import WatchKit
+#elseif canImport(UIKit)
 import UIKit
-#else
+#elseif canImport(AppKit)
 import AppKit
 #endif
 
@@ -58,7 +60,7 @@ protocol ActivityTrackerBehaviour: AnyObject {
 
 class ActivityTracker: ActivityTrackerBehaviour {
 
-#if canImport(UIKit)
+#if canImport(UIKit) && !os(watchOS)
     private var backgroundTask: UIBackgroundTaskIdentifier = .invalid
 #endif
 
@@ -72,27 +74,34 @@ class ActivityTracker: ActivityTrackerBehaviour {
     private var stateMachineSubscriberToken: StateMachineSubscriberToken?
 
     private static let applicationDidMoveToBackgroundNotification: Notification.Name = {
-    #if canImport(UIKit)
-        UIApplication.didEnterBackgroundNotification
-    #else
-        NSApplication.didResignActiveNotification
-    #endif
+#if canImport(WatchKit)
+    WKExtension.applicationDidEnterBackgroundNotification
+#elseif canImport(UIKit)
+    UIApplication.didEnterBackgroundNotification
+#elseif canImport(AppKit)
+    NSApplication.didResignActiveNotification
+#endif
     }()
 
     private static  let applicationWillMoveToForegoundNotification: Notification.Name = {
-    #if canImport(UIKit)
-        UIApplication.willEnterForegroundNotification
-    #else
-        NSApplication.willBecomeActiveNotification
-    #endif
+#if canImport(WatchKit)
+    WKExtension.applicationWillEnterForegroundNotification
+#elseif canImport(UIKit)
+    UIApplication.willEnterForegroundNotification
+#elseif canImport(AppKit)
+    NSApplication.willBecomeActiveNotification
+#endif
     }()
 
     private static  var applicationWillTerminateNotification: Notification.Name = {
-    #if canImport(UIKit)
-        UIApplication.willTerminateNotification
-    #else
-        NSApplication.willTerminateNotification
-    #endif
+#if canImport(WatchKit)
+    // There's no willTerminateNotification on watchOS, so using applicationWillResignActive instead.
+    WKExtension.applicationWillResignActiveNotification
+#elseif canImport(UIKit)
+    UIApplication.willTerminateNotification
+#else
+    NSApplication.willTerminateNotification
+#endif
     }()
 
     private let notifications = [
@@ -128,14 +137,14 @@ class ActivityTracker: ActivityTrackerBehaviour {
     }
 
     private func beginBackgroundTracking() {
-    #if canImport(UIKit)
+#if canImport(UIKit) && !os(watchOS)
         if backgroundTrackingTimeout > 0 {
             backgroundTask = UIApplication.shared.beginBackgroundTask(withName: Constants.backgroundTask) { [weak self] in
                 self?.stateMachine.process(.backgroundTrackingDidTimeout)
                 self?.stopBackgroundTracking()
             }
         }
-    #endif
+#endif
         guard backgroundTrackingTimeout != .infinity else { return }
         backgroundTimer = Timer.scheduledTimer(withTimeInterval: backgroundTrackingTimeout, repeats: false) { [weak self] _ in
             self?.stateMachine.process(.backgroundTrackingDidTimeout)
@@ -145,13 +154,13 @@ class ActivityTracker: ActivityTrackerBehaviour {
 
     private func stopBackgroundTracking() {
         backgroundTimer = nil
-    #if canImport(UIKit)
+#if canImport(UIKit) && !os(watchOS)
         guard backgroundTask != .invalid else {
             return
         }
         UIApplication.shared.endBackgroundTask(backgroundTask)
         backgroundTask = .invalid
-    #endif
+#endif
     }
 
     @objc private func handleApplicationStateChange(_ notification: Notification) {
