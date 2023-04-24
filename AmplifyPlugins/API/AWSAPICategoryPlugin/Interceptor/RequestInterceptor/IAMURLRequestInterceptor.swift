7326782a97e541183a76e0d7dd8b751701928e76
Jithin Roy <51138777+royjit@users.noreply.github.com>
2023-04-24 14:53:34 -0700

    feat(api): Update URL interceptor to be async (#2876)

--- AmplifyPlugins/API/AWSAPICategoryPlugin/Interceptor/RequestInterceptor/IAMURLRequestInterceptor.swift
+++ AmplifyPlugins/API/AWSAPICategoryPlugin/Interceptor/RequestInterceptor/IAMURLRequestInterceptor.swift
@@ -62,4 +62,49 @@ struct IAMURLRequestInterceptor: URLRequestInterceptor {
 
         return mutableRequest as URLRequest
     }
+
+    func intercept(_ request: URLRequest, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+
+        guard let mutableRequest = (request as NSURLRequest).mutableCopy() as? NSMutableURLRequest else {
+            let apiError = APIError.unknown("Could not get mutable request", "")
+            completion(.failure(apiError))
+            return
+        }
+
+        let serviceConfiguration = AmplifyAWSServiceConfiguration(
+            region: region,
+            credentialsProvider: iamCredentialsProvider.getCredentialsProvider())
+        mutableRequest.setValue(NSDate().aws_stringValue(AWSDateISO8601DateFormat2),
+                                forHTTPHeaderField: URLRequestConstants.Header.xAmzDate)
+        mutableRequest.setValue(URLRequestConstants.ContentType.applicationJson,
+                                forHTTPHeaderField: URLRequestConstants.Header.contentType)
+        mutableRequest.setValue(serviceConfiguration.userAgent,
+                                forHTTPHeaderField: URLRequestConstants.Header.userAgent)
+
+        let endpoint: AWSEndpoint
+        switch endpointType {
+        case .graphQL:
+            endpoint = AWSEndpoint(region: region,
+                                   serviceName: URLRequestConstants.appSyncServiceName,
+                                   url: mutableRequest.url)
+        case .rest:
+            endpoint = AWSEndpoint(region: region,
+                                   service: AWSServiceType.APIGateway,
+                                   url: mutableRequest.url)
+        }
+
+        let signer: AWSSignatureV4Signer = AWSSignatureV4Signer(
+            credentialsProvider: iamCredentialsProvider.getCredentialsProvider(),
+            endpoint: endpoint)
+
+        signer.interceptRequest(mutableRequest).continueWith { task in
+            if let error = task.error {
+                let apiError = APIError.operationError("Got error trying to sign", "", error)
+                completion(.failure(apiError))
+                return nil
+            }
+            completion(.success(mutableRequest as URLRequest))
+            return nil
+        }
+    }
 }
