47146a89a0adb5feb68a7b0d93760d6ef62b33a5
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-04-26 12:34:39 -0400

    feat(predictions): update predictionserror (#2898)

--- Amplify/Categories/Predictions/Error/PredictionsError.swift
+++ Amplify/Categories/Predictions/Error/PredictionsError.swift
@@ -9,66 +9,56 @@ import Foundation
 
 /// Error occured while using Prediction category
 public enum PredictionsError {
-
     /// Access denied while executing the operation
-    case accessDenied(ErrorDescription, RecoverySuggestion, Error? = nil)
-    case auth(ErrorDescription, RecoverySuggestion, Error? = nil)
-    case configuration(ErrorDescription, RecoverySuggestion, Error? = nil)
-    case httpStatus(Int, RecoverySuggestion, Error? = nil)
-    case network(ErrorDescription, RecoverySuggestion, Error? = nil)
-    case service(ErrorDescription, RecoverySuggestion, Error? = nil)
+    case client(ClientError)
+    case service(ServiceError)
     case unknown(ErrorDescription, RecoverySuggestion, Error? = nil)
 
+    public struct ServiceError {
+        public let description: ErrorDescription
+        public let recoverySuggestion: RecoverySuggestion
+        public let httpStatusCode: Int?
+        public let underlyingError: Error?
+    }
+
+    public struct ClientError {
+        public let description: ErrorDescription
+        public let recoverySuggestion: RecoverySuggestion
+        public let underlyingError: Error?
+    }
 }
 
 extension PredictionsError: AmplifyError {
     public var errorDescription: ErrorDescription {
         switch self {
-        case .accessDenied(let errorDescription, _, _),
-             .auth(let errorDescription, _, _),
-             .configuration(let errorDescription, _, _):
-            return errorDescription
+        case .client(let clientError):
+            return "A client error occurred with message:\(clientError.description)"
+        case .service(let serviceError):
+            return "A service error occurred with message:\(serviceError.description)"
         case .unknown(let errorDescription, _, _):
             return "Unexpected error occurred with message: \(errorDescription)"
-        case .network(let errorDescription, _, _):
-            return "Network error occurred with message:\(errorDescription)"
-        case .httpStatus(let statusCode, _, _):
-            return "The HTTP response status code is [\(statusCode)]."
-        case .service(let errorDescription, _, _):
-            return "A service error occurred with message:\(errorDescription)"
         }
 
     }
 
     public var recoverySuggestion: RecoverySuggestion {
         switch self {
-        case .accessDenied(_, let recoverySuggestion, _),
-             .auth(_, let recoverySuggestion, _),
-             .configuration(_, let recoverySuggestion, _),
-             .service( _, let recoverySuggestion, _),
-             .network(_, let recoverySuggestion, _):
-            return recoverySuggestion
-
-        case .httpStatus(_, let recoverySuggestion, _):
-            return """
-            \(recoverySuggestion).
-            For more information on HTTP status codes, take a look at
-            https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-            """
+        case .client(let clientError):
+            return clientError.recoverySuggestion
+        case .service(let serviceError):
+            return serviceError.recoverySuggestion
         case .unknown:
             return AmplifyErrorMessages.shouldNotHappenReportBugToAWS()
-        }
+       }
     }
 
     public var underlyingError: Error? {
         switch self {
-        case .accessDenied(_, _, let underlyingError),
-             .auth(_, _, let underlyingError),
-             .configuration(_, _, let underlyingError),
-             .httpStatus(_, _, let underlyingError),
-             .network(_, _, let underlyingError),
-             .service(_, _, let underlyingError),
-             .unknown(_, _, let underlyingError):
+        case .client(let clientError):
+            return clientError.underlyingError
+        case .service(let serviceError):
+            return serviceError.underlyingError
+        case .unknown(_, _, let underlyingError):
             return underlyingError
         }
     }
@@ -80,11 +70,8 @@ extension PredictionsError: AmplifyError {
     ) {
         if let error = error as? Self {
             self = error
-        } else if error.isOperationCancelledError {
-            self = .unknown("Operation cancelled", "", error)
         } else {
             self = .unknown(errorDescription, recoverySuggestion, error)
         }
     }
-
 }
