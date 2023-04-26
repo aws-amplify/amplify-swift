4685a5108708ce50db167444e78df80645cb81f9
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-04-26 09:43:31 -0400

    cleanup

--- AmplifyPlugins/Predictions/AWSPredictionsPlugin/Dependency/AWSTranslateBehavior.swift
+++ AmplifyPlugins/Predictions/AWSPredictionsPlugin/Dependency/AWSTranslateBehavior.swift
@@ -8,26 +8,6 @@
 import Foundation
 import AWSTranslate
 
-struct PredictionServiceClientFetching<Service, Client> {
-    let getClient: (Service) -> Client
-}
-
-extension PredictionServiceClientFetching where
-    Service: PredictionsServiceBehavior,
-    Client == TranslateClient {
-
-
-}
-
-struct ServiceBehavior<A, B> {
-
-}
-
-protocol PredictionsServiceBehavior {
-    associatedtype Client
-    func getClient() -> Client
-}
-
 protocol AWSTranslateBehavior {
     func translateText(
         request: TranslateTextInput
