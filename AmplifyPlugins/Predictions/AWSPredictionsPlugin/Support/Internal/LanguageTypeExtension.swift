d020e416e857a22c11686fae363d427608122d34
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-04-26 14:39:02 -0400

    add sdk mappers

---
+++ AmplifyPlugins/Predictions/AWSPredictionsPlugin/Support/Internal/LanguageTypeExtension.swift
@@ -0,0 +1,70 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Amplify
+import AWSComprehend
+import AWSTranscribeStreaming
+
+extension Predictions.Language {
+
+    func toComprehendLanguage() -> ComprehendClientTypes.LanguageCode {
+        switch self {
+        case .english:
+            return .en
+        case .italian:
+            return .it
+        case .spanish:
+            return .es
+        case .french:
+            return .fr
+        case .german:
+            return .de
+        case .portuguese:
+            return .pt
+        default:
+            return .sdkUnknown(code)
+        }
+    }
+
+    func toSyntaxLanguage() -> ComprehendClientTypes.SyntaxLanguageCode {
+        switch self {
+        case .english:
+            return .en
+        case .italian:
+            return .it
+        case .spanish:
+            return .es
+        case .french:
+            return .fr
+        case .german:
+            return .de
+        case .portuguese:
+            return .pt
+        default:
+            return .sdkUnknown(code)
+        }
+    }
+
+
+    func toTranscribeLanguage() -> TranscribeStreamingClientTypes.LanguageCode {
+        switch self {
+        case .usEnglish,
+             .english:
+            return .enUs
+        case .french:
+            return .frFr
+        case .canadianFrench:
+            return .frCa
+        case .britishEnglish:
+            return .enGb
+        case .usSpanish:
+            return .esUs
+        default:
+            return .sdkUnknown(code)
+        }
+    }
+}
---
+++ AmplifyPlugins/Predictions/AWSPredictionsPlugin/Support/Internal/PredictionsAction.swift
@@ -0,0 +1,12 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+public enum PredictionsAction {
+    case identify
+    case interpret
+    case convert
+}
