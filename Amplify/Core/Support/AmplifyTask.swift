5c83a04690b8ae5dec5bd66170c49399405bce91
JC Jimenez <1117904+jcjimenez@users.noreply.github.com>
2023-05-12 13:44:27 -0500

    chore(core): Added documentation to AmplifyError, AmplifyErrorMessages, AmplifyTask, and DeviceInfo.

--- Amplify/Core/Support/AmplifyTask.swift
+++ Amplify/Core/Support/AmplifyTask.swift
@@ -10,15 +10,34 @@ import Foundation
 import Combine
 #endif
 
+/// Simple task that represents a unit of work and its result.
+///
+/// See Also: [AmplifyInProcessReportingTask](x-source-tag://AmplifyInProcessReportingTask)
+///
+/// - Tag: AmplifyTask
 public protocol AmplifyTask {
     associatedtype Request
     associatedtype Success
     associatedtype Failure: AmplifyError
 
+    /// Blocks until the receiver has successfully collected a result or throws if an error was encountered.
+    ///
+    /// - Tag: AmplifyTask.value
     var value: Success { get async throws }
 
+    /// Pauses the work represented by the receiver.
+    ///
+    /// - Tag: AmplifyTask.pause
     func pause()
+
+    /// Resumes any paused work represented by the receiver.
+    ///
+    /// - Tag: AmplifyTask.resume
     func resume()
+
+    /// Cancels any work represented by the receiver.
+    ///
+    /// - Tag: AmplifyTask.resume
     func cancel()
 
 #if canImport(Combine)
@@ -26,9 +45,17 @@ public protocol AmplifyTask {
 #endif
 }
 
+/// Simple task that represents a unit of work and its result and is able to report its progress.
+///
+/// See Also: [AmplifyTask](x-source-tag://AmplifyTask)
+///
+/// - Tag: AmplifyInProcessReportingTask
 public protocol AmplifyInProcessReportingTask {
     associatedtype InProcess
 
+    /// An async sequence that is able to report on the progress of the work represented by the receiver.
+    ///
+    /// - Tag: AmplifyInProcessReportingTask.inProcess
     var inProcess: AmplifyAsyncSequence<InProcess> { get async }
 
 #if canImport(Combine)
@@ -37,6 +64,11 @@ public protocol AmplifyInProcessReportingTask {
 }
 
 public extension AmplifyInProcessReportingTask where InProcess == Progress {
+
+    /// An async sequence that is able to report on the progress of the work represented by the receiver
+    /// using [Progress](x-source-tag://Progress).
+    ///
+    /// - Tag: AmplifyInProcessReportingTask.progress
     var progress : AmplifyAsyncSequence<InProcess> {
         get async {
             await inProcess
