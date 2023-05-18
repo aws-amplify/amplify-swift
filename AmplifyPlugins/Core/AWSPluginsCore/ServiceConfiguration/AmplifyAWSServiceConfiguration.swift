0b90b00e7f6deb07f7bb7d6a82e0474d8c1f8bdc
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-05-18 15:15:21 -0400

    chore: release 2.11.2 [skip ci]

--- AmplifyPlugins/Core/AWSPluginsCore/ServiceConfiguration/AmplifyAWSServiceConfiguration.swift
+++ AmplifyPlugins/Core/AWSPluginsCore/ServiceConfiguration/AmplifyAWSServiceConfiguration.swift
@@ -16,7 +16,7 @@ import Amplify
 public class AmplifyAWSServiceConfiguration {
 
     /// - Tag: AmplifyAWSServiceConfiguration.amplifyVersion
-    public static let amplifyVersion = "2.11.1"
+    public static let amplifyVersion = "2.11.2"
 
     /// - Tag: AmplifyAWSServiceConfiguration.platformName
     public static let platformName = "amplify-swift"
