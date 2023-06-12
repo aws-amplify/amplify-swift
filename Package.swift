2f08e4e67d9a1b17f96a522ade42ec5fca34658c
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2023-06-12 10:53:46 -0400

    chore: Adding unit tests jobs for the new platforms to CircleCI

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/aws-amplify/aws-appsync-realtime-client-ios.git", from: "3.0.0"),
     .package(url: "https://github.com/stephencelis/SQLite.swift.git", exact: "0.13.2"),
     .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: "2.1.0"),
-    .package(url: "https://github.com/aws-amplify/amplify-swift-utils-notifications.git", from: "1.0.0")
+    .package(url: "https://github.com/aws-amplify/amplify-swift-utils-notifications.git", branch: "feat/tvOS-and-watchOS")
 ]
 
 let amplifyTargets: [Target] = [
