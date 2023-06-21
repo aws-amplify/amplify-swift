5a918de87b388f044c4564a24610529f8df03805
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-06-21 16:06:24 -0400

    linting error files

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Service/ErrorMapping/AssociateSoftwareTokenOutputError+AuthError.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Service/ErrorMapping/AssociateSoftwareTokenOutputError+AuthError.swift
@@ -5,7 +5,6 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-import Foundation
 import Amplify
 import AWSCognitoIdentityProvider
 
@@ -13,25 +12,31 @@ extension AssociateSoftwareTokenOutputError: AuthErrorConvertible {
     var authError: AuthError {
         switch self {
         case .concurrentModificationException(let concurrentModificationException):
-            return .service(concurrentModificationException.message ?? "Concurrent modification error",
-                            AuthPluginErrorConstants.concurrentModificationException)
+            return .service(
+                concurrentModificationException.message ?? "Concurrent modification error",
+                AuthPluginErrorConstants.concurrentModificationException)
         case .forbiddenException(let forbiddenException):
-            return .service(forbiddenException.message ?? "Access to the requested resource is forbidden",
-                            AuthPluginErrorConstants.forbiddenError)
+            return .service(
+                forbiddenException.message ?? "Access to the requested resource is forbidden",
+                AuthPluginErrorConstants.forbiddenError)
         case .internalErrorException(let internalErrorException):
-            return .unknown(internalErrorException.message ?? "Internal exception occurred")
+            return .unknown(
+                internalErrorException.message ?? "Internal exception occurred")
         case .invalidParameterException(let invalidParameterException):
-            return .service(invalidParameterException.message ?? "Invalid parameter error",
-                            AuthPluginErrorConstants.invalidParameterError,
-                            AWSCognitoAuthError.invalidParameter)
+            return .service(
+                invalidParameterException.message ?? "Invalid parameter error",
+                AuthPluginErrorConstants.invalidParameterError,
+                AWSCognitoAuthError.invalidParameter)
         case .notAuthorizedException(let notAuthorizedException):
-            return .notAuthorized(notAuthorizedException.message ?? "Not authorized Error",
-                                  AuthPluginErrorConstants.notAuthorizedError,
-                                  nil)
+            return .notAuthorized(
+                notAuthorizedException.message ?? "Not authorized Error",
+                AuthPluginErrorConstants.notAuthorizedError,
+                nil)
         case .resourceNotFoundException(let resourceNotFoundException):
-            return AuthError.service(resourceNotFoundException.message ?? "Resource not found error",
-                                     AuthPluginErrorConstants.resourceNotFoundError,
-                                     AWSCognitoAuthError.resourceNotFound)
+            return AuthError.service(
+                resourceNotFoundException.message ?? "Resource not found error",
+                AuthPluginErrorConstants.resourceNotFoundError,
+                AWSCognitoAuthError.resourceNotFound)
         case .softwareTokenMFANotFoundException(let exception):
             return AuthError.service(
                 exception.message ?? "Software token TOTP multi-factor authentication (MFA) is not enabled for the user pool.",
