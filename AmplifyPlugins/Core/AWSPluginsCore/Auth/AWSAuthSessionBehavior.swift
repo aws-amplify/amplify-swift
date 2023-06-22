8b6efb77fb4d1a372a1bcd1d32115cc9914463ab
Abhash Kumar Singh <thisisabhash@gmail.com>
2023-06-22 16:42:49 -0700

    feat(auth): Add definitions for supporting OIDC plugin

---
+++ AmplifyPlugins/Core/AWSPluginsCore/Auth/AWSAuthSessionBehavior.swift
@@ -0,0 +1,16 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+import Foundation
+import Amplify
+
+public protocol AWSAuthSessionBehavior<TokensType> : AuthSession {
+    associatedtype TokensType
+    var awsCredentialsResult: Result<AWSTemporaryCredentials, AuthError> { get }
+    var identityIdResult: Result<String, AuthError> { get  }
+    var userSubResult: Result<String, AuthError> { get }
+    var tokensResult: Result<TokensType, AuthError> { get }
+}
