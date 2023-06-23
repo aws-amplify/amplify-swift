8960c54d41512d4cfad7f4228eed8c74a89c6061
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-06-23 01:17:40 -0400

    feat(Auth): Adding TOTP tasks and requests to AWSAuthCognitoPlugin

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/AWSCognitoAuthPlugin+EscapeHatch.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/AWSCognitoAuthPlugin+EscapeHatch.swift
@@ -12,26 +12,6 @@ import AWSCognitoIdentityProvider
 
 public extension AWSCognitoAuthPlugin {
 
-    func federateToIdentityPool(
-        withProviderToken: String,
-        for provider: AuthProvider,
-        options: AuthFederateToIdentityPoolRequest.Options? = nil
-    ) async throws -> FederateToIdentityPoolResult {
-
-        let options = options ?? AuthFederateToIdentityPoolRequest.Options()
-        let request = AuthFederateToIdentityPoolRequest(token: withProviderToken, provider: provider, options: options)
-        let task = AWSAuthFederateToIdentityPoolTask(request, authStateMachine: authStateMachine)
-        return try await task.value
-
-    }
-
-    func clearFederationToIdentityPool(options: AuthClearFederationToIdentityPoolRequest.Options? = nil) async throws {
-        let options = options ?? AuthClearFederationToIdentityPoolRequest.Options()
-        let request = AuthClearFederationToIdentityPoolRequest(options: options)
-        let task = AWSAuthClearFederationToIdentityPoolTask(request, authStateMachine: authStateMachine)
-        try await task.value
-    }
-
     func getEscapeHatch() -> AWSCognitoAuthService {
         var service: AWSCognitoAuthService?
         switch authConfiguration {
---
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/VerifyTOTPSetupTask.swift
@@ -0,0 +1,74 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+import Amplify
+import AWSPluginsCore
+import ClientRuntime
+import AWSCognitoIdentityProvider
+
+class VerifyTOTPSetupTask: AuthVerifyTOTPSetupTask, DefaultLogger {
+
+    typealias CognitoUserPoolFactory = () throws -> CognitoUserPoolBehavior
+
+    private let request: VerifyTOTPSetupRequest
+    private let authStateMachine: AuthStateMachine
+    private let userPoolFactory: CognitoUserPoolFactory
+    private let taskHelper: AWSAuthTaskHelper
+
+    var eventName: HubPayloadEventName {
+        HubPayload.EventName.Auth.verifyTOTPSetupAPI
+    }
+
+    init(_ request: VerifyTOTPSetupRequest,
+         authStateMachine: AuthStateMachine,
+         userPoolFactory: @escaping CognitoUserPoolFactory) {
+        self.request = request
+        self.authStateMachine = authStateMachine
+        self.userPoolFactory = userPoolFactory
+        self.taskHelper = AWSAuthTaskHelper(authStateMachine: authStateMachine)
+    }
+
+    func execute() async throws {
+        do {
+            await taskHelper.didStateMachineConfigured()
+            let accessToken = try await taskHelper.getAccessToken()
+            try await verifyTOTPSetup(
+                with: accessToken, userCode: request.code)
+        } catch let error as AuthErrorConvertible {
+            throw error.authError
+        } catch let error as AuthError {
+            throw error
+        } catch let error {
+            throw AuthError.unknown("Unable to execute auth task", error)
+        }
+    }
+
+    func verifyTOTPSetup(with accessToken: String, userCode: String) async throws  {
+        let userPoolService = try userPoolFactory()
+        let friendlyDeviceName = (request.options.pluginOptions as? VerifyTOTPSetupOptions)?.friendlyDeviceName
+        let input = VerifySoftwareTokenInput(
+            accessToken: accessToken,
+            friendlyDeviceName: friendlyDeviceName,
+            userCode: userCode)
+        let result = try await userPoolService.verifySoftwareToken(input: input)
+
+        guard let output = result.status else {
+            throw AuthError.service("Result cannot be retrieved", "")
+        }
+
+        switch output {
+        case .error:
+            throw AuthError.service("Unknown error", "")
+        case .success:
+            return
+        case .sdkUnknown(let error):
+            throw AuthError.service("Unknown error", error)
+        }
+
+    }
+}
