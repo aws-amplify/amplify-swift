90cf5783571f66ebbeccfe708acc8bec37f3b454
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-06-23 01:10:12 -0400

    feat(Auth): Adding TOTP related models to AWSCognitoPlugin

---
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Models/Options/VerifyTOTPSetupOptions.swift
@@ -0,0 +1,16 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+public struct VerifyTOTPSetupOptions {
+
+    /// Device name that would be provided to Cognito when setting up TOTP
+    public let friendlyDeviceName: String?
+
+    public init(friendlyDeviceName: String? = nil) {
+        self.friendlyDeviceName = friendlyDeviceName
+    }
+}
