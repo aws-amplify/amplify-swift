f234f079516562dd1462a4632c8065eceead7c55
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-07-03 12:08:53 -0400

    feat(Auth): Adding TOTP related models to AWSCognitoPlugin (#3041)
    * feat(Auth): Adding TOTP support in Amplify Auth category
    
    * feat(Auth): Adding TOTP related models to AWSCognitoPlugin

---
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Models/MFAPreference.swift
@@ -0,0 +1,55 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import AWSCognitoIdentityProvider
+import Foundation
+
+/// Input for updating the MFA preference for a MFA Type
+public enum MFAPreference {
+    
+    // enabled: false
+    case disabled
+
+    // enabled: true
+    case enabled
+
+    // enabled: true, preferred: true
+    case preferred
+
+    // enabled: true, preferred: false
+    case notPreferred
+
+}
+
+extension MFAPreference {
+    
+    var smsSetting: CognitoIdentityProviderClientTypes.SMSMfaSettingsType? {
+        switch self {
+        case .enabled:
+            return .init(enabled: true)
+        case .preferred:
+            return .init(enabled: true, preferredMfa: true)
+        case .notPreferred:
+            return .init(enabled: true, preferredMfa: false)
+        case .disabled:
+            return .init(enabled: false)
+        }
+    }
+    
+    var softwareTokenSetting: CognitoIdentityProviderClientTypes.SoftwareTokenMfaSettingsType? {
+        switch self {
+        case .enabled:
+            return .init(enabled: true)
+        case .preferred:
+            return .init(enabled: true, preferredMfa: true)
+        case .notPreferred:
+            return .init(enabled: true, preferredMfa: false)
+        case .disabled:
+            return .init(enabled: false)
+        }
+    }
+}
