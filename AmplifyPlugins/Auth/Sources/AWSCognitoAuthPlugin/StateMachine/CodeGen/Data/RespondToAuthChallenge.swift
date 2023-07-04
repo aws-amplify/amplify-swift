819ce8c4fbde6652a597682311225170221ec6f1
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-07-03 20:25:13 -0400

    feat(Auth): Adding TOTP states, events, data models and resolvers (#3045)
    * feat(Auth): Adding TOTP state machine actions
    
    * feat(Auth): Adding TOTP states, events, data models and resolvers

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/StateMachine/CodeGen/Data/RespondToAuthChallenge.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/StateMachine/CodeGen/Data/RespondToAuthChallenge.swift
@@ -39,6 +39,32 @@ extension RespondToAuthChallenge {
                                        attributeKey: nil)
     }
 
+    var getAllowedMFATypesForSelection: Set<MFAType> {
+        return getMFATypes(forKey: "MFAS_CAN_CHOOSE")
+    }
+
+    var getAllowedMFATypesForSetup: Set<MFAType> {
+        return getMFATypes(forKey: "MFAS_CAN_SETUP")
+    }
+
+    /// Helper method to extract MFA types from parameters
+    private func getMFATypes(forKey key: String) -> Set<MFAType> {
+        var mfaTypes = Set<MFAType>()
+        guard let mfaTypeParametersData = parameters?[key]?.data(using: .utf8),
+              let mfaTypesArray = try? JSONDecoder().decode(
+                [String].self, from: mfaTypeParametersData) else {
+            return mfaTypes
+        }
+
+        for mfaTypeValue in mfaTypesArray {
+            if let mfaType = MFAType(rawValue: String(mfaTypeValue)) {
+                mfaTypes.insert(mfaType)
+            }
+        }
+
+        return mfaTypes
+    }
+
     var debugDictionary: [String: Any] {
         return ["challenge": challenge,
                 "username": username.masked()]
@@ -46,11 +72,12 @@ extension RespondToAuthChallenge {
 
     func getChallengeKey() throws -> String {
         switch challenge {
-        case .customChallenge: return "ANSWER"
+        case .customChallenge, .selectMfaType: return "ANSWER"
         case .smsMfa: return "SMS_MFA_CODE"
+        case .softwareTokenMfa: return "SOFTWARE_TOKEN_MFA_CODE"
         case .newPasswordRequired: return "NEW_PASSWORD"
         default:
-            let message = "UnSupported challenge response \(challenge)"
+            let message = "Unsupported challenge type for response key generation \(challenge)"
             let error = SignInError.unknown(message: message)
             throw error
         }
