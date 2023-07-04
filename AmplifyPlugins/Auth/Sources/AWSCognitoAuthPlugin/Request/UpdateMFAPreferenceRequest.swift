5bef428ca3d274f1558a48ab4fe6ca1ba859867d
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-07-03 20:22:21 -0400

    feat(Auth): Adding TOTP state machine actions (#3044)
    * feat(Auth): Adding TOTP tasks and requests to AWSAuthCognitoPlugin
    
    * feat(Auth): Adding TOTP state machine actions
    
    * working on review comment

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Request/UpdateMFAPreferenceRequest.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Request/UpdateMFAPreferenceRequest.swift
@@ -11,14 +11,13 @@ import Foundation
 /// Request for udpating user MFA preferences
 public struct UpdateMFAPreferenceRequest: AmplifyOperationRequest {
 
-    public let smsPreference: MFAPreference?
-    
-    public let totpPreference: MFAPreference?
+    internal let smsPreference: MFAPreference?
+    internal let totpPreference: MFAPreference?
 
     /// Extra request options defined in `UpdateMFAPreferenceRequest.Options`
     public var options: Options
 
-    public init(options: Options,
+    internal init(options: Options,
                   smsPreference: MFAPreference?,
                   totpPreference: MFAPreference?) {
         self.options = options
--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/SetUpTOTPTask.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/SetUpTOTPTask.swift
@@ -57,9 +57,18 @@ class SetUpTOTPTask: AuthSetUpTOTPTask, DefaultLogger {
         }
 
         // Get the current user for passing in the result, so that TOTP URI could constructed
-        let taskHelper = AWSAuthTaskHelper(authStateMachine: authStateMachine)
-        let authUser = try await taskHelper.getCurrentUser()
+        let authUser: AuthUser
 
+        let currentState = await authStateMachine.currentState
+        if case .configured(let authNState, _) = currentState,
+           case .signedIn(let signInData) = authNState {
+            authUser = AWSAuthUser(username: signInData.username, userId: signInData.userId)
+        } else {
+            throw AuthError.invalidState(
+                "Auth State not in a valid state for the user",
+                AuthPluginErrorConstants.invalidStateError,
+                nil)
+        }
         return .init(secretCode: secretCode,
                      username: authUser.username)
 
