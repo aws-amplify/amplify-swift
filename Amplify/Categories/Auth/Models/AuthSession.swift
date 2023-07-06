474add4f37eebfcce535b70aa7cca54bf548a345
Tim Schmelter <schmelte@amazon.com>
2023-07-06 14:32:46 -0700

    chore: Clarify isSignedIn API doc

--- Amplify/Categories/Auth/Models/AuthSession.swift
+++ Amplify/Categories/Auth/Models/AuthSession.swift
@@ -10,10 +10,16 @@ import Foundation
 /// Defines the auth session behavior
 public protocol AuthSession {
 
-    /// Indicates whether a user is signed in or not
+    /// True if the current user has signed in
     ///
-    /// `true` if a user is authenticated. `isSignedIn` remains `true` till we call `Amplify.Auth.signOut`.
-    /// Please note that this value remains `true` even when the session is expired. Refer the underlying plugin
-    /// documentation regarding how to handle session expiry.
+    /// `true` if a user has authenticated, via any of:
+    /// - ``AuthCategoryBehavior/signIn(username:password:options:)``
+    /// - ``AuthCategoryBehavior/signInWithWebUI(presentationAnchor:options:)``
+    /// - ``AuthCategoryBehavior/signInWithWebUI(for:presentationAnchor:options:)``
+    /// - A plugin-specific sign in method like
+    ///   `AWSCognitoAuthPlugin.federateToIdentityPool(withProviderToken:for:options:)`
+    ///
+    /// `isSignedIn` remains `true` until we call `Amplify.Auth.signOut`. Notably, this value remains `true`
+    /// even when the session is expired. Refer the underlying plugin documentation regarding how to handle session expiry.
     var isSignedIn: Bool { get }
 }
