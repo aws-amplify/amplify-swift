fdbd2328ffb4405d268b9c7dab53f4f00590c94e
Tuan Pham <103537251+phantumcode@users.noreply.github.com>
2023-07-05 19:15:23 -0500

    feat(logging): add persistence support to logger plugin (#3068)
    * feat(logging): add initial supporting structures for new logger plugin
    
    * feat(logging): add persistence support to logger plugin
    
    * chore(logging): fix build error for watchOS
    
    * chore(logging): fix build error on macos
    
    * chore(logging): clean up unit tests
    
    * chore(logging): clean up unit tests
    
    * chore(logging): clean up unit tests
    
    * chore(logging): clean up unit tests
    
    * chore(logging): code cleanup
    
    * chore(logging): resolve review comments
    
    * chore(logging): resolve review comments

---
+++ AmplifyPlugins/Logging/Sources/AWSCloudWatchLoggingPlugin/Producer/RotatingLogger.swift
@@ -0,0 +1,107 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Amplify
+import Combine
+import Foundation
+
+final class RotatingLogger {
+    
+    var logLevel: Amplify.LogLevel
+    
+    private let category: String
+    private let namespace: String?
+    private let actor: LogActor
+    private let batchSubject: PassthroughSubject<LogBatch, Never>
+    private var rotationSubscription: Combine.Cancellable? {
+        willSet { rotationSubscription?.cancel() }
+    }
+
+    /// Initializes the logger with the given directory as its log rotation base directory.
+    ///
+    /// - Parameter directory: The URL of the directory to use for the log rotation.
+    /// - Parameter logLevel: Amplify.LogLevel by which to filter any incomming log events.
+    init(directory: URL,
+         category: String,
+         namespace: String?,
+         logLevel: Amplify.LogLevel,
+         fileSizeLimitInBytes: Int
+    ) throws {
+        self.category = category
+        self.namespace = namespace
+        self.actor = try LogActor(directory: directory, fileSizeLimitInBytes: fileSizeLimitInBytes)
+        self.batchSubject = PassthroughSubject()
+        self.logLevel = logLevel
+    }
+    
+    /// Attempts to flush the contents of the log to disk.
+    func synchronize() async throws {
+        try await actor.synchronize()
+    }
+    
+    func flushLogs() async throws {
+        try await setupSubscription()
+        try await actor.flushLogs()
+    }
+    
+    func record(level: LogLevel, message: @autoclosure () -> String) async throws {
+        try await setupSubscription()
+        let entry = LogEntry(category: self.category, namespace: self.namespace, level: level, message: message())
+        try await self.actor.record(entry)
+    }
+    
+    private func setupSubscription() async throws {
+        if (rotationSubscription == nil) {
+            let rotationPublisher = await self.actor.rotationPublisher()
+            rotationSubscription = rotationPublisher.sink { [weak self] url in
+                guard let self = self else { return }
+                self.batchSubject.send(RotatingLogBatch(url: url))
+            }
+        }
+    }
+
+    private func _record(level: LogLevel, message: @autoclosure () -> String) {
+        let payload = message()
+        Task {
+            try await self.record(level: level, message:payload)
+        }
+    }
+}
+
+extension RotatingLogger: LogBatchProducer {
+    var logBatchPublisher: AnyPublisher<LogBatch, Never> {
+        return batchSubject.eraseToAnyPublisher()
+    }
+}
+
+extension RotatingLogger: Logger {
+    
+    func error(_ message: @autoclosure () -> String) {
+        _record(level: .error, message: message())
+    }
+    
+    func error(error: Error) {
+        let message = String(describing: error)
+        _record(level: .error, message: message)
+    }
+    
+    func warn(_ message: @autoclosure () -> String) {
+        _record(level: .warn, message: message())
+    }
+    
+    func info(_ message: @autoclosure () -> String) {
+        _record(level: .info, message: message())
+    }
+    
+    func debug(_ message: @autoclosure () -> String) {
+        _record(level: .debug, message: message())
+    }
+    
+    func verbose(_ message: @autoclosure () -> String) {
+        _record(level: .verbose, message: message())
+    }
+}
---
+++ AmplifyPlugins/Logging/Tests/AWSCloudWatchLoggingPluginTests/LogRotationTests.swift
@@ -0,0 +1,166 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import XCTest
+
+@testable import AWSCloudWatchLoggingPlugin
+
+final class LogRotationTests: XCTestCase {
+    
+    var systemUnderTest: LogRotation!
+    var directory: URL!
+    var fileCountLimit = 5
+    var fileSizeLimitInBytes = 1024
+
+    override func setUp() async throws {
+        directory = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString)
+        try FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true)
+        systemUnderTest = try LogRotation(directory: directory,
+                                          fileSizeLimitInBytes: fileSizeLimitInBytes)
+    }
+    
+    override func tearDown() async throws {
+        systemUnderTest = nil
+        try FileManager.default.removeItem(at: directory)
+        directory = nil
+    }
+    
+    /// Given: a log rotation
+    /// When: the current log file is access
+    /// Then: the log file default to ampliyf.0.log
+    func testLogRotationDefaultState() throws {
+        XCTAssertEqual(systemUnderTest.currentLogFile.available, systemUnderTest.currentLogFile.sizeLimitInBytes)
+        XCTAssertEqual(systemUnderTest.currentLogFile.fileURL.lastPathComponent, "amplify.0.log")
+        let contents = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
+        XCTAssertEqual(contents.map { $0.lastPathComponent }, [
+            "amplify.0.log"
+        ])
+    }
+    
+    /// Given: a log rotation
+    /// When: a rotation occurs
+    /// Then: then a new rotated log file is created
+    func testLogRotationCreatesNewFiles() throws {
+        let originalContents = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
+        XCTAssertEqual(originalContents.map { $0.lastPathComponent }, [
+            "amplify.0.log"
+        ])
+
+        systemUnderTest = try LogRotation(directory: directory,
+                                          fileSizeLimitInBytes: fileSizeLimitInBytes)
+        XCTAssertEqual(systemUnderTest.currentLogFile.fileURL.lastPathComponent, "amplify.1.log")
+        try systemUnderTest.rotate()
+        
+        var rotatedContents = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
+        XCTAssertEqual(rotatedContents.map { $0.lastPathComponent }, [
+            "amplify.2.log",
+            "amplify.1.log",
+            "amplify.0.log",
+        ])
+        
+        try systemUnderTest.rotate()
+        rotatedContents = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
+        XCTAssertEqual(rotatedContents.map { $0.lastPathComponent }, [
+            "amplify.2.log",
+            "amplify.3.log",
+            "amplify.1.log",
+            "amplify.0.log",
+        ])
+        
+        try systemUnderTest.rotate()
+        rotatedContents = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
+        XCTAssertEqual(rotatedContents.map { $0.lastPathComponent }, [
+            "amplify.4.log",
+            "amplify.2.log",
+            "amplify.3.log",
+            "amplify.1.log",
+            "amplify.0.log",
+        ])
+        
+        systemUnderTest = try LogRotation(directory: directory,
+                                          fileSizeLimitInBytes: fileSizeLimitInBytes)
+        XCTAssertEqual(systemUnderTest.currentLogFile.fileURL.lastPathComponent, "amplify.0.log")
+    }
+    
+    /// Given: a log rotation
+    /// When: rotation occurs to the max limit
+    /// Then: the log rotation circles back to 0
+    func testLogRotationToMaxLimit() async throws {
+        for _ in 0..<(fileCountLimit) {
+            XCTAssertEqual(systemUnderTest.currentLogFile.available, systemUnderTest.currentLogFile.sizeLimitInBytes)
+            try systemUnderTest.rotate()
+            try await Task.sleep(seconds: 0.10)
+        }
+        let allFiles = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
+        XCTAssertEqual(Set(allFiles.map { $0.lastPathComponent }), Set([
+            "amplify.0.log",
+            "amplify.1.log",
+            "amplify.2.log",
+            "amplify.3.log",
+            "amplify.4.log"
+        ]))
+    }
+    
+    /// Given: a log rotation with existing under-utilized file
+    /// When: rotation occurs
+    /// Then: the log rotation rotates to the under-utilized log file
+    func testLogRotationUseUnderutilizedLogFile() async throws {
+        let bytes = (0..<systemUnderTest.currentLogFile.sizeLimitInBytes).map { _ in UInt8.random(in: 0..<255) }
+        let largeData = Data(bytes)
+        let tinyData = Data([1, 2, 3, 4])
+        
+        // Fill-up all files except for the one at chosen index
+        let chosenIndex = 1
+        for index in 0..<(fileCountLimit) {
+            if (index != chosenIndex) {
+                try systemUnderTest.currentLogFile.write(data: largeData)
+            } else {
+                try systemUnderTest.currentLogFile.write(data: tinyData)
+            }
+            try systemUnderTest.rotate()
+            try await Task.sleep(seconds: 0.10)
+        }
+        
+        // The last rotation should result in currentLogFile being the one at the chosen index.
+        XCTAssertEqual(systemUnderTest.currentLogFile.fileURL.lastPathComponent, "amplify.\(chosenIndex).log")
+        let contents = FileManager.default.contents(atPath: systemUnderTest.currentLogFile.fileURL.path)
+        XCTAssertEqual(contents, tinyData)
+    }
+    
+    /// Given: a log rotation
+    /// When: rotation occurs to the max limit
+    /// Then: the log rotation circles back to oldest last modified log file
+    func testLogRotationUsesOldestLastModifiedLogFile() async throws {
+        let bytes = (0..<systemUnderTest.currentLogFile.sizeLimitInBytes).map { _ in UInt8.random(in: 0..<255) }
+        let data = Data(bytes)
+        
+        for _ in 0..<(fileCountLimit) {
+            try systemUnderTest.currentLogFile.write(data: data)
+            try systemUnderTest.rotate()
+            try await Task.sleep(seconds: 0.10)
+        }
+        
+        // The last rotation should result in currentLogFile being one with the last modified date.
+        XCTAssertEqual(systemUnderTest.currentLogFile.fileURL.lastPathComponent, "amplify.0.log")
+    }
+    
+    /// Given: the minimum allowed file size
+    /// When: a log rotation is constructed below the minimum file size limit
+    /// Then: the construction fails with an invalid size limit error
+    func testLogRotationThrowsErrorWithInvalidFileSizeLimitInBytes() throws {
+        for fileSizeLimitInBytes in 0..<LogRotation.minimumFileSizeLimitInBytes {
+            do {
+                _ = try LogRotation(directory: directory,
+                                    fileSizeLimitInBytes: fileSizeLimitInBytes)
+                XCTFail("Expecting failure when initializing with fileCountLimit=\(fileSizeLimitInBytes)")
+                break
+            } catch {
+                XCTAssertEqual(String(describing: error), "invalidFileSizeLimitInBytes(\(fileSizeLimitInBytes))")
+            }
+        }
+    }
+}
