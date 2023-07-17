41ff0275b70ff8ffd62008d4a0b50a06cce15bf4
Tuan Pham <phantum@amazon.com>
2023-07-17 12:40:34 -0500

    chore(logging): add console logging check

--- Amplify/Categories/Logging/Internal/LoggingCategory+CategoryConfigurable.swift
+++ Amplify/Categories/Logging/Internal/LoggingCategory+CategoryConfigurable.swift
@@ -28,7 +28,7 @@ extension LoggingCategory: CategoryConfigurable {
         try plugin.configure(using: configuration?.plugins[plugin.key])
         self.plugins[plugin.key] = plugin
         
-        if let consolePlugin = try? self.getPlugin(for: AWSUnifiedLoggingPlugin.key) {
+        if plugin.key != AWSUnifiedLoggingPlugin.key, let consolePlugin = try? self.getPlugin(for: AWSUnifiedLoggingPlugin.key) {
             try consolePlugin.configure(using: configuration?.plugins[consolePlugin.key])
         }
         
