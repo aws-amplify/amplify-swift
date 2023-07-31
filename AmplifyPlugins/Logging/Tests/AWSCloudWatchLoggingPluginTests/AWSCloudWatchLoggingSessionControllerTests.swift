ae7a0e68c702760bcf740b60c32cfa1bbc68546c
Tuan Pham <phantum@amazon.com>
2023-07-31 18:24:47 -0500

    fix(logging): add unit test

--- AmplifyPlugins/Logging/Tests/AWSCloudWatchLoggingPluginTests/AWSCloudWatchLoggingSessionControllerTests.swift
+++ AmplifyPlugins/Logging/Tests/AWSCloudWatchLoggingPluginTests/AWSCloudWatchLoggingSessionControllerTests.swift
@@ -20,9 +20,13 @@ final class AWSCloudWatchLoggingSessionControllerTests: XCTestCase {
     let mockCloudWatchLogClient = MockCloudWatchLogsClient()
     let mockLoggingNetworkMonitor = MockLoggingNetworkMonitor()
     let category = "amplifytest"
+    var unsubscribeToken: UnsubscribeToken?
 
     override func tearDown() async throws {
         systemUnderTest = nil
+        if let token = unsubscribeToken {
+            Amplify.Hub.removeListener(token)
+        }
         let file = getLogFile()
         do {
             try FileManager.default.removeItem(atPath: file.path)
@@ -36,7 +40,7 @@ final class AWSCloudWatchLoggingSessionControllerTests: XCTestCase {
     /// Then: a flushLogFailure Hub Event is sent to the Logging channel
     func testConsumeFailureSendsHubEvent() async throws {
         let hubEventExpectation = expectation(description: "Should receive the hub event")
-        _ = Amplify.Hub.listen(to: .logging) { payload in
+        unsubscribeToken = Amplify.Hub.listen(to: .logging) { payload in
             switch payload.eventName {
             case HubPayload.EventName.Logging.flushLogFailure:
                 hubEventExpectation.fulfill()
@@ -66,6 +70,43 @@ final class AWSCloudWatchLoggingSessionControllerTests: XCTestCase {
         systemUnderTest.enable()
         try await systemUnderTest.flushLogs()
         await fulfillment(of: [hubEventExpectation], timeout: 2)
+    }
+    
+    /// Given: an AWSCloudWatchLoggingSessionController
+    /// When: the user identifier is changed
+    /// Then: a flush log is called and fails with a flushLogFailure hub event
+    func testResetLogsWhenIdentifierChanges() async throws {
+        let hubEventExpectation = expectation(description: "should receive flush failure event")
+        unsubscribeToken = Amplify.Hub.listen(to: .logging) { payload in
+            switch payload.eventName {
+            case HubPayload.EventName.Logging.flushLogFailure:
+                hubEventExpectation.fulfill()
+            default:
+                break
+            }
+        }
+
+        let bytes = (0..<1024).map { _ in UInt8.random(in: 0..<255) }
+        let fileURL = getLogFile()
+        FileManager.default.createFile(atPath: fileURL.path,
+                                       contents: Data(bytes),
+                                       attributes: [FileAttributeKey: Any]())
+        systemUnderTest = AWSCloudWatchLoggingSessionController(
+            credentialsProvider: mockCredentialProvider,
+            authentication: mockAuth,
+            logFilter: mockLoggingFilter,
+            category: category,
+            namespace: nil,
+            logLevel: .error,
+            logGroupName: "logGroupName",
+            region: "us-east-1",
+            localStoreMaxSizeInMB: 1,
+            userIdentifier: nil,
+            networkMonitor: mockLoggingNetworkMonitor)
+        systemUnderTest.client = mockCloudWatchLogClient
+        systemUnderTest.enable()
+        systemUnderTest.setCurrentUser(identifier: "123")
+        await fulfillment(of: [hubEventExpectation], timeout: 5)
     }
 
     private func getLogFile() -> URL {
