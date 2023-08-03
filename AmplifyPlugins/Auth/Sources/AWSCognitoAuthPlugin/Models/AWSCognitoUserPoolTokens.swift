6b67b662024b0652970935f0e7814b41873efbd4
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-08-03 14:30:46 -0400

    fix(Auth): Refactor the expires in logic of the authentication result (#3114)

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Models/AWSCognitoUserPoolTokens.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Models/AWSCognitoUserPoolTokens.swift
@@ -16,8 +16,10 @@ public struct AWSCognitoUserPoolTokens: AuthCognitoTokens {
 
     public let refreshToken: String
 
+    @available(*, deprecated, message: "Use of `expiration` is deprecated, use `exp` claim in the `idToken` or `accessToken` for expiries")
     public let expiration: Date
 
+    @available(*, deprecated, message: "Use of `init(idToken,accessToken,refreshToken:expiresIn)` is deprecated, use `exp` claim in the `idToken` or `accessToken` instead")
     public init(idToken: String,
                 accessToken: String,
                 refreshToken: String,
@@ -28,6 +30,7 @@ public struct AWSCognitoUserPoolTokens: AuthCognitoTokens {
         self.expiration = Date().addingTimeInterval(TimeInterval(expiresIn))
     }
 
+    @available(*, deprecated, message: "Use of `init(idToken,accessToken,refreshToken:expiration)` is deprecated, use `exp` claim in the `idToken` or `accessToken` instead")
     public init(idToken: String,
                 accessToken: String,
                 refreshToken: String,
@@ -38,6 +41,37 @@ public struct AWSCognitoUserPoolTokens: AuthCognitoTokens {
         self.expiration = expiration
     }
 
+    init(idToken: String,
+         accessToken: String,
+         refreshToken: String,
+         expiresIn: Int? = nil) {
+
+        self.idToken = idToken
+        self.accessToken = accessToken
+        self.refreshToken = refreshToken
+
+        if let expiresIn =  expiresIn {
+            self.expiration = Date().addingTimeInterval(TimeInterval(expiresIn))
+        } else {
+            let expirationDoubleValue: Double
+            let idTokenExpiration = try? AWSAuthService().getTokenClaims(tokenString: idToken).get()["exp"]?.doubleValue
+            let accessTokenExpiration = try? AWSAuthService().getTokenClaims(tokenString: accessToken).get()["exp"]?.doubleValue
+
+            switch (idTokenExpiration, accessTokenExpiration) {
+            case (.some(let idTokenValue), .some(let accessTokenValue)):
+                expirationDoubleValue = min(idTokenValue, accessTokenValue)
+            case (.none, .some(let accessTokenValue)):
+                expirationDoubleValue = accessTokenValue
+            case (.some(let idTokenValue), .none):
+                expirationDoubleValue = idTokenValue
+            case (.none, .none):
+                expirationDoubleValue = 0
+            }
+
+            self.expiration = Date().addingTimeInterval(TimeInterval((expirationDoubleValue ?? 0)))
+        }
+    }
+    
 }
 
 extension AWSCognitoUserPoolTokens: Equatable { }
