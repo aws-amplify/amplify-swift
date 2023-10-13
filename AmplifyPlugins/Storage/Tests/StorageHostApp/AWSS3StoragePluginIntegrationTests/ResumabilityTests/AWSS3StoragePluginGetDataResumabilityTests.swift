4d8da11f4180254f14a87289511a835cbeef5764
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-10-13 09:42:45 -0400

    fix(storage): timeout on inverted expectation in tests

--- AmplifyPlugins/Storage/Tests/StorageHostApp/AWSS3StoragePluginIntegrationTests/ResumabilityTests/AWSS3StoragePluginGetDataResumabilityTests.swift
+++ AmplifyPlugins/Storage/Tests/StorageHostApp/AWSS3StoragePluginIntegrationTests/ResumabilityTests/AWSS3StoragePluginGetDataResumabilityTests.swift
@@ -18,117 +18,112 @@ class AWSS3StoragePluginDownloadDataResumabilityTests: AWSS3StoragePluginTestBas
     /// When: Call the get API then pause
     /// Then: The operation is stalled (no progress, completed, or failed event)
     func testDownloadDataAndPause() async throws {
-        try await testTask(timeout: 600) {
-            let key = UUID().uuidString
-            let data = AWSS3StoragePluginTestBase.smallDataObject
-            let uploadKey = try await Amplify.Storage.uploadData(key: key, data: data).value
-            XCTAssertEqual(uploadKey, key)
+        let key = UUID().uuidString
+        let data = AWSS3StoragePluginTestBase.smallDataObject
+        let uploadKey = try await Amplify.Storage.uploadData(key: key, data: data).value
+        XCTAssertEqual(uploadKey, key)
 
-            Self.logger.debug("Downloading data")
-            let task = Amplify.Storage.downloadData(key: key)
+        Self.logger.debug("Downloading data")
+        let task = Amplify.Storage.downloadData(key: key)
 
-            let didPause = expectation(description: "did pause")
-            let didContinue = expectation(description: "did continue")
-            didContinue.isInverted = true
-            Task {
-                var paused = false
-                var progressAfterPause = 0
-                for await progress in await task.progress {
-                    Self.logger.debug("progress: \(progress)")
-                    if !paused {
-                        paused = true
-                        task.pause()
-                        didPause.fulfill()
-                    } else {
-                        progressAfterPause += 1
-                        if progressAfterPause > 1 {
-                            didContinue.fulfill()
-                        }
+        let didPause = expectation(description: "did pause")
+        let didContinue = expectation(description: "did continue")
+        didContinue.isInverted = true
+        Task {
+            var paused = false
+            var progressAfterPause = 0
+            for await progress in await task.progress {
+                Self.logger.debug("progress: \(progress)")
+                if !paused {
+                    paused = true
+                    task.pause()
+                    didPause.fulfill()
+                } else {
+                    progressAfterPause += 1
+                    if progressAfterPause > 1 {
+                        didContinue.fulfill()
                     }
                 }
             }
-            await fulfillment(of: [didPause], timeout: TestCommonConstants.networkTimeout)
-            await fulfillment(of: [didContinue], timeout: 5)
-
-            let completeInvoked = expectation(description: "Download is completed")
-            completeInvoked.isInverted = true
-            let downloadTask = Task {
-                let result = try await task.value
-                completeInvoked.fulfill()
-                return result
-            }
-
-            Self.logger.debug("Cancelling download task")
-            task.cancel()
-            await fulfillment(of: [completeInvoked])
-
-            let downloadData = try? await downloadTask.value
-            XCTAssertNil(downloadData)
-
-            // clean up
-            Self.logger.debug("Cleaning up after download task")
-            try await Amplify.Storage.remove(key: key)
         }
+        await fulfillment(of: [didPause], timeout: TestCommonConstants.networkTimeout)
+        await fulfillment(of: [didContinue], timeout: 5)
+
+        let completeInvoked = expectation(description: "Download is completed")
+        completeInvoked.isInverted = true
+        let downloadTask = Task {
+            let result = try await task.value
+            completeInvoked.fulfill()
+            return result
+        }
+
+        Self.logger.debug("Cancelling download task")
+        task.cancel()
+        await fulfillment(of: [completeInvoked], timeout: 1)
+
+        let downloadData = try? await downloadTask.value
+        XCTAssertNil(downloadData)
+
+        // clean up
+        Self.logger.debug("Cleaning up after download task")
+        try await Amplify.Storage.remove(key: key)
     }
 
     /// Given: A data object in storage
     /// When: Call the downloadData API, pause, and then resume the operation
     /// Then: The operation should complete successfully
     func testDownloadDataAndPauseThenResume() async throws {
-        try await testTask(timeout: 600) {
-            let key = UUID().uuidString
-            let data = AWSS3StoragePluginTestBase.smallDataObject
-            let uploadKey = try await Amplify.Storage.uploadData(key: key, data: data).value
-            XCTAssertEqual(uploadKey, key)
+        let key = UUID().uuidString
+        let data = AWSS3StoragePluginTestBase.smallDataObject
+        let uploadKey = try await Amplify.Storage.uploadData(key: key, data: data).value
+        XCTAssertEqual(uploadKey, key)
 
-            let task = Amplify.Storage.downloadData(key: key)
+        let task = Amplify.Storage.downloadData(key: key)
 
-            let progressInvoked = expectation(description: "Progress invoked")
-            Task {
-                var progressInvokedCalled = false
-                for await progress in await task.progress {
-                    Self.logger.debug("Download progress: \(progress.fractionCompleted)")
-                    if !progressInvokedCalled, progress.fractionCompleted > 0.1 {
-                        progressInvokedCalled = true
-                        progressInvoked.fulfill()
-                    }
+        let progressInvoked = expectation(description: "Progress invoked")
+        Task {
+            var progressInvokedCalled = false
+            for await progress in await task.progress {
+                Self.logger.debug("Download progress: \(progress.fractionCompleted)")
+                if !progressInvokedCalled, progress.fractionCompleted > 0.1 {
+                    progressInvokedCalled = true
+                    progressInvoked.fulfill()
                 }
             }
-            await fulfillment(of: [progressInvoked], timeout: TestCommonConstants.networkTimeout)
-
-            Self.logger.debug("Pausing download task")
-            task.pause()
-
-            Self.logger.debug("Sleeping")
-            try await Task.sleep(seconds: 0.25)
-
-            let completeInvoked = expectation(description: "Download is completed")
-            let downloadTask = Task {
-                let result = try await task.value
-                completeInvoked.fulfill()
-                return result
-            }
-
-            Self.logger.debug("Resuming download task")
-            task.resume()
-
-            await fulfillment(of: [completeInvoked], timeout: TestCommonConstants.networkTimeout)
-
-            Self.logger.debug("Waiting to finish download task")
-            let downloadData = try await downloadTask.value
-            XCTAssertEqual(downloadData, data)
-
-            // clean up
-            Self.logger.debug("Cleaning up after download task")
-            try await Amplify.Storage.remove(key: key)
         }
+        await fulfillment(of: [progressInvoked], timeout: TestCommonConstants.networkTimeout)
+
+        Self.logger.debug("Pausing download task")
+        task.pause()
+
+        Self.logger.debug("Sleeping")
+        try await Task.sleep(seconds: 0.25)
+
+        let completeInvoked = expectation(description: "Download is completed")
+        let downloadTask = Task {
+            let result = try await task.value
+            completeInvoked.fulfill()
+            return result
+        }
+
+        Self.logger.debug("Resuming download task")
+        task.resume()
+
+        await fulfillment(of: [completeInvoked], timeout: TestCommonConstants.networkTimeout)
+
+        Self.logger.debug("Waiting to finish download task")
+        let downloadData = try await downloadTask.value
+        XCTAssertEqual(downloadData, data)
+
+        // clean up
+        Self.logger.debug("Cleaning up after download task")
+        try await Amplify.Storage.remove(key: key)
     }
 
     /// Given: A data object in storage
     /// When: Call the get API then cancel the operation,
     /// Then: The operation should not complete or fail.
     func testDownloadDataAndCancel() async throws {
-        throw XCTSkip()
         let key = UUID().uuidString
         let data = AWSS3StoragePluginTestBase.smallDataObject
         let uploadKey = try await Amplify.Storage.uploadData(key: key, data: data).value
@@ -165,7 +160,7 @@ class AWSS3StoragePluginDownloadDataResumabilityTests: AWSS3StoragePluginTestBas
             return result
         }
 
-        await fulfillment(of: [completeInvoked])
+        await fulfillment(of: [completeInvoked], timeout: 1)
 
         Self.logger.debug("Waiting for download to complete")
         let downloadData = try? await downloadTask.value
