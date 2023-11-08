b4f5a708b566a20d7ac4de5d7be9e0a2039d0855
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-11-08 13:36:17 -0500

    core: rename xrOS to visionOS in os directive checks

--- Amplify/Core/Configuration/Internal/Amplify+Reset.swift
+++ Amplify/Core/Configuration/Internal/Amplify+Reset.swift
@@ -52,7 +52,7 @@ extension Amplify {
         ModelProviderRegistry.reset()
         log.verbose("Resetting ModelRegistry, ModelListDecoderRegistry, ModelProviderRegistry finished")
         
-#if os(iOS) && !os(xrOS)
+#if os(iOS)
         await MainActor.run {
             devMenu = nil
         }
