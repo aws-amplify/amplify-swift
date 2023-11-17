fb49c66b0cae81f6baae9f13f96d7f1813d44255
Abhash Kumar Singh <thisisabhash@gmail.com>
2023-11-17 12:40:34 -0800

    Address review comments

--- Amplify/Categories/Auth/Models/AuthPasswordlessFlow.swift
+++ Amplify/Categories/Auth/Models/AuthPasswordlessFlow.swift
@@ -10,20 +10,11 @@ import Foundation
 /// Auth Passwordless flow types
 ///
 /// `.signUpAndSignIn` will create a new user and initiate signing in the user
-/// `signIn` will initiate signing in an existing user
+/// `.signIn` will initiate signing in an existing user
 public enum AuthPasswordlessFlow {
     case signUpAndSignIn(
         userAttributes: [String: String]?,
         clientMetadata: [String: String]?
     )
     case signIn(clientMetadata: [String: String]?)
-}
-
-/// Delivery destination for the Auth Passwordless flows
-///
-/// For One Time Password, it can be either .`phone` or `.email`
-/// For Magic Link, it is `.email`
-public enum AuthPasswordlessDeliveryDestination {
-   case phone
-   case email
 }
