9e8eb59baa206d7528a9501c9a263d41a72758d9
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-11-29 09:45:19 -0500

    worked on review comments

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Models/AWSCognitoNetworkPreferences.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Models/AWSCognitoNetworkPreferences.swift
@@ -9,22 +9,21 @@ import Foundation
 
 public struct AWSCognitoNetworkPreferences {
 
-    /// The maximum number of retries for failed requests. The value needs to be between 0 and 10 inclusive.
-    /// If set to higher than 10, it becomes 10.
+    /// The maximum number of retries for failed requests.
     public let maxRetryCount: UInt32
 
     /// The timeout interval to use when waiting for additional data.
-    public let timeoutIntervalForRequest: Double
+    public let timeoutIntervalForRequest: TimeInterval
 
     /// The maximum amount of time that a resource request should be allowed to take.
     /// 
     /// NOTE: This value is only applicable to HostedUI because the underlying Swift SDK does
     /// not support resource timeouts
-    public let timeoutIntervalForResource: Double?
+    public let timeoutIntervalForResource: TimeInterval?
 
     public init(maxRetryCount: UInt32,
-                timeoutIntervalForRequest: Double,
-                timeoutIntervalForResource: Double? = nil) {
+                timeoutIntervalForRequest: TimeInterval,
+                timeoutIntervalForResource: TimeInterval? = nil) {
         self.maxRetryCount = maxRetryCount
         self.timeoutIntervalForRequest = timeoutIntervalForRequest
         self.timeoutIntervalForResource = timeoutIntervalForResource
