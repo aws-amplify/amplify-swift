f57e38714089b5b819fe66f743781cbcf4a5e3d4
aws-amplify-ops <aws-amplify-ops@amazon.com>
2023-12-13 18:27:38 +0000

    chore: release 1.30.5 [skip ci]

--- build-support/dependencies.rb
+++ build-support/dependencies.rb
@@ -1,7 +1,7 @@
 # Version definitions
 
 # Amplify release version
-$AMPLIFY_VERSION = '1.30.4'
+$AMPLIFY_VERSION = '1.30.5'
 
 # GitHub tag name for Amplify releases
 $AMPLIFY_RELEASE_TAG = "#{$AMPLIFY_VERSION}"
