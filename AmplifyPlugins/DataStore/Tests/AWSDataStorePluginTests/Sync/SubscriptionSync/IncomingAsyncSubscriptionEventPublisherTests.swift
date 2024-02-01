bc6bf64852f7893e4b92ec4f8b0c97fc5e91a305
Michael Law <1365977+lawmicha@users.noreply.github.com>
2024-02-01 15:45:02 -0500

    address PR comments

--- AmplifyPlugins/DataStore/Tests/AWSDataStorePluginTests/Sync/SubscriptionSync/IncomingAsyncSubscriptionEventPublisherTests.swift
+++ AmplifyPlugins/DataStore/Tests/AWSDataStorePluginTests/Sync/SubscriptionSync/IncomingAsyncSubscriptionEventPublisherTests.swift
@@ -15,6 +15,7 @@ final class IncomingAsyncSubscriptionEventPublisherTests: XCTestCase {
     var apiPlugin: MockAPICategoryPlugin!
     override func setUp() {
         apiPlugin = MockAPICategoryPlugin()
+        ModelRegistry.register(modelType: Post.self)
     }
 
     /// This test was written to to reproduce a bug where the subscribe would miss events emitted by the publisher.
@@ -56,6 +57,53 @@ final class IncomingAsyncSubscriptionEventPublisherTests: XCTestCase {
         }
 
         await fulfillment(of: [expectedEvents], timeout: 2)
+        sink.cancel()
+    }
+
+    /// Ensure that the publisher-subscriber with back pressure is receiving all the events in the order in which they were sent.
+    func testSubscriberRecievedEventsInOrder() async throws {
+        let expectedEvents = expectation(description: "Expected number of ")
+        let expectedOrder = AtomicValue<[String]>(initialValue: [])
+        let actualOrder = AtomicValue<[String]>(initialValue: [])
+        let numberOfEvents = 50
+        expectedEvents.expectedFulfillmentCount = numberOfEvents
+        let asyncEvents = await IncomingAsyncSubscriptionEventPublisher(
+            modelSchema: Post.schema,
+            api: apiPlugin,
+            modelPredicate: nil,
+            auth: nil,
+            authModeStrategy: AWSDefaultAuthModeStrategy(),
+            awsAuthService: nil)
+        let mapper = IncomingAsyncSubscriptionEventToAnyModelMapper()
+        asyncEvents.subscribe(subscriber: mapper)
+        let sink = mapper
+            .publisher
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { event in
+                    switch event {
+                    case .payload(let mutationSync):
+                        actualOrder.append(mutationSync.syncMetadata.modelId)
+                    default:
+                        break
+                    }
+                    expectedEvents.fulfill()
+                }
+            )
+
+        for index in 0..<numberOfEvents {
+            let post = Post(id: "\(index)", title: "title", content: "content", createdAt: .now())
+            expectedOrder.append(post.id)
+            asyncEvents.send(.data(.success(.init(model: AnyModel(post),
+                                                  syncMetadata: .init(modelId: post.id,
+                                                                      modelName: "Post",
+                                                                      deleted: false,
+                                                                      lastChangedAt: 0,
+                                                                      version: 0)))))
+        }
+
+        await fulfillment(of: [expectedEvents], timeout: 2)
+        XCTAssertEqual(expectedOrder.get(), actualOrder.get())
         sink.cancel()
     }
 }
