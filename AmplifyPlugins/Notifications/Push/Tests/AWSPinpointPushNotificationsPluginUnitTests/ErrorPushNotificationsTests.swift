1edc681018da7dd7ab365b52c8fc4e0d5a3353ca
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2024-02-15 09:38:05 -0500

    Revert "update pinpoint unit tests"
    This reverts commit 0f804d8ae7ee548881523d2cd94207944324d333.

--- AmplifyPlugins/Notifications/Push/Tests/AWSPinpointPushNotificationsPluginUnitTests/ErrorPushNotificationsTests.swift
+++ AmplifyPlugins/Notifications/Push/Tests/AWSPinpointPushNotificationsPluginUnitTests/ErrorPushNotificationsTests.swift
@@ -6,8 +6,11 @@
 //
 
 @testable import Amplify
+import AWSClientRuntime
+import AwsCommonRuntimeKit
 import AWSPinpoint
 @testable import AWSPinpointPushNotificationsPlugin
+import ClientRuntime
 import Foundation
 import XCTest
 
@@ -69,19 +72,12 @@ class ErrorPushNotificationsTests: XCTestCase {
             }
         }
     }
-}
-
-#if canImport(AWSClientRuntime)
-import AWSClientRuntime
-
-extension ErrorPushNotificationsTests {
 
     /// Given: An UnknownAWSHTTPServiceError
     /// When: pushNotificationsError is invoked
     /// Then: A .unknown error is returned
     func testPushNotificationError_withUnknownAWSHTTPServiceError_shouldReturnUnknownError() {
-        let error = UnknownAWSHTTPServiceError(
-            httpResponse: .init(body: .empty, statusCode: .accepted), message: "UnknownAWSHTTPServiceError", requestID: nil, typeName: nil)
+        let error = UnknownAWSHTTPServiceError(httpResponse: .init(body: .none, statusCode: .accepted), message: "UnknownAWSHTTPServiceError", requestID: nil, typeName: nil)
         let pushNotificationsError = error.pushNotificationsError
         switch pushNotificationsError {
         case .unknown(let errorDescription, let underlyingError):
@@ -91,14 +87,6 @@ extension ErrorPushNotificationsTests {
             XCTFail("Expected error of type .unknown, got \(pushNotificationsError)")
         }
     }
-}
-
-#endif
-
-#if canImport(AwsCommonRuntimeKit)
-import AwsCommonRuntimeKit
-
-extension ErrorPushNotificationsTests {
 
     /// Given: A CommonRunTimeError.crtError
     /// When: pushNotificationsError is invoked
@@ -115,5 +103,3 @@ extension ErrorPushNotificationsTests {
         }
     }
 }
-
-#endif
