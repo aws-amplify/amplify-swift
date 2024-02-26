7ae55d1c0728aff60837691d172709efa39d2300
Tomasz Trela <t.trela91@gmail.com>
2024-02-26 17:09:49 +0100

    fix(DataStore): endless retry of mutation request when server responds with 401 error code (#3511) (#3512)
    * fix(DataStore): endless retry of mutation request when server responds with 401 error code (#3511)
    
    * fix(DataStore): endless retry of mutation request when server responds with 401 error code (#3511)
    
    * fix(DataStore): endless retry of mutation request when server  responds with 401 error code (#3511)
    
    Co-authored-by: Michael Law <1365977+lawmicha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Michael Law <1365977+lawmicha@users.noreply.github.com>

---
+++ AmplifyPlugins/DataStore/Tests/AWSDataStorePluginTests/Sync/Support/AWSAuthorizationTypeIteratorTests.swift
@@ -0,0 +1,40 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import XCTest
+import AWSPluginsCore
+
+class AWSAuthorizationTypeIteratorTests: XCTestCase {
+    
+    func testEmptyIterator_hasNextValue_false() throws {
+        var iterator = AWSAuthorizationTypeIterator(withValues: [])
+       
+        XCTAssertFalse(iterator.hasNext)
+        XCTAssertNil(iterator.next())
+    }
+    
+    func testOneElementIterator_hasNextValue_once() throws {
+        var iterator = AWSAuthorizationTypeIterator(withValues: [.amazonCognitoUserPools])
+        
+        XCTAssertTrue(iterator.hasNext)
+        XCTAssertNotNil(iterator.next())
+        
+        XCTAssertFalse(iterator.hasNext)
+    }
+    
+    func testTwoElementsIterator_hasNextValue_twice() throws {
+        var iterator = AWSAuthorizationTypeIterator(withValues: [.amazonCognitoUserPools, .apiKey])
+        
+        XCTAssertTrue(iterator.hasNext)
+        XCTAssertNotNil(iterator.next())
+        
+        XCTAssertTrue(iterator.hasNext)
+        XCTAssertNotNil(iterator.next())
+        
+        XCTAssertFalse(iterator.hasNext)
+    }
+}
