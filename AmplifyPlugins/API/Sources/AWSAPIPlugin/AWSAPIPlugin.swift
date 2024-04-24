ef5f972e1adb78355923965c4e97088ec9c0078f
Michael Law <1365977+lawmicha@users.noreply.github.com>
2024-04-24 14:42:42 -0400

    add public

--- AmplifyPlugins/API/Sources/AWSAPIPlugin/AWSAPIPlugin.swift
+++ AmplifyPlugins/API/Sources/AWSAPIPlugin/AWSAPIPlugin.swift
@@ -11,7 +11,7 @@ import Foundation
 
 /// Used for the default GraphQL API represented by the `data` category in `amplify_outputs.json`
 /// This constant is not used for APIs present in `amplifyconfiguration.json` since they always have names.
-let defaultGraphQLAPI = "defaultGraphQLAPI"
+public let defaultGraphQLAPI = "defaultGraphQLAPI"
 
 final public class AWSAPIPlugin: NSObject, APICategoryPlugin, APICategoryGraphQLBehaviorExtended, AWSAPIAuthInformation {
     /// The unique key of the plugin within the API category.
