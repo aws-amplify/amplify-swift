df7e457ab64c1fcdcef917aa9b5af65939a586ed
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-04-29 19:45:21 -0400

    feat: update sqlite.swift dependency to 0.15.3

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let platforms: [SupportedPlatform] = [
 ]
 let dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/awslabs/aws-sdk-swift.git", exact: "0.36.1"),
-    .package(url: "https://github.com/stephencelis/SQLite.swift.git", exact: "0.15.0"),
+    .package(url: "https://github.com/stephencelis/SQLite.swift.git", exact: "0.15.3"),
     .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: "2.1.0"),
     .package(url: "https://github.com/aws-amplify/amplify-swift-utils-notifications.git", from: "1.1.0")
 ]
