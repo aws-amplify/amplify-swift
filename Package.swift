e47abb42efbdcb7fa336e3bce4ae4fd27b2b0c4a
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-04-30 10:17:52 -0400

    bump to swift 5.9 and back to sqlite 0.15.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -10,7 +10,7 @@ let platforms: [SupportedPlatform] = [
 ]
 let dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/awslabs/aws-sdk-swift.git", exact: "0.36.1"),
-    .package(url: "https://github.com/stephencelis/SQLite.swift.git", exact: "0.15.0"),
+    .package(url: "https://github.com/stephencelis/SQLite.swift.git", exact: "0.15.3"),
     .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: "2.1.0"),
     .package(url: "https://github.com/aws-amplify/amplify-swift-utils-notifications.git", from: "1.1.0")
 ]
