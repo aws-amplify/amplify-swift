fa445e66c8daf8bdc6336022dc84ff5ed2c7ce4d
Michael Law <1365977+lawmicha@users.noreply.github.com>
2024-05-02 09:55:26 -0400

    storage upload example

--- AmplifyPlugins/Storage/Tests/StorageHostApp/StorageHostApp/ContentView.swift
+++ AmplifyPlugins/Storage/Tests/StorageHostApp/StorageHostApp/ContentView.swift
@@ -9,12 +9,36 @@ import Amplify
 import AWSCognitoAuthPlugin
 import AWSS3StoragePlugin
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var image: Image?
+
     var body: some View {
-        VStack{
-            Text("Hello, Amplify world!")
-                .padding()
+        NavigationStack {
+            VStack {
+                image?
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding()
+            PhotosPicker(
+                selection: $selectedPhoto
+            ) {
+                Text("Select a photo to upload")
+            }
+            .task(id: selectedPhoto) {
+                //print(selectedPhoto?.supportedContentTypes)
+                image = try? await selectedPhoto?.loadTransferable(type: Image.self)
+                if let imageData = try? await selectedPhoto?.loadTransferable(type: Data.self) {
+                    let uploadTask = Amplify.Storage.uploadData(
+                        path: .fromString("picture-submissions/myPhoto.png"),
+                        data: imageData
+                    )
+                }
+
+            }
         }
     }
 }
