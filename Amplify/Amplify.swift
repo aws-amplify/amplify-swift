b3c6d2db83919512518de9685b2a95ea037ebdb6
Rozay Chen <rozayc@amazon.com>
2024-05-30 12:27:02 -0700

    chore: Test No API Break

--- Amplify/Amplify.swift
+++ Amplify/Amplify.swift
@@ -28,7 +28,7 @@ public class Amplify {
     /// - Tag: Amplify.isConfigured
     static var isConfigured = false
 
-    public static var test: float = 1.0
+    public static var test: Float = 1.0
 
     // Storage for the categories themselves, which will be instantiated during configuration, and cleared during reset.
     // It is not supported to mutate these category properties. They are `var` to support the `reset()` method for
