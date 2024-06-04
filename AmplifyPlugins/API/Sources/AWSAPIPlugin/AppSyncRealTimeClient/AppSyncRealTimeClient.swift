ad538164c62c47670499a58e63552c14006c50c3
Di Wu <github@wudi.me>
2024-06-04 03:29:24 +0000

    fix(datastore): reconnect the AppSyncRealTimeClient once connection is stale (#3734)

--- AmplifyPlugins/API/Sources/AWSAPIPlugin/AppSyncRealTimeClient/AppSyncRealTimeClient.swift
+++ AmplifyPlugins/API/Sources/AWSAPIPlugin/AppSyncRealTimeClient/AppSyncRealTimeClient.swift
@@ -298,6 +298,16 @@ actor AppSyncRealTimeClient: AppSyncRealTimeClientProtocol {
         .eraseToAnyPublisher()
     }
 
+    private func reconnect() async {
+        do {
+            log.debug("[AppSyncRealTimeClient] Reconnecting")
+            await disconnect()
+            try await connect()
+        } catch {
+            log.debug("[AppSyncRealTimeClient] Failed to reconnect, error: \(error)")
+        }
+    }
+
     private static func decodeAppSyncRealTimeResponseError(_ data: JSONValue?) -> [Error] {
         let knownAppSyncRealTimeRequestErorrs =
             Self.decodeAppSyncRealTimeRequestError(data)
@@ -414,7 +424,7 @@ extension AppSyncRealTimeClient {
             .sink(receiveValue: {
                 self.log.debug("[AppSyncRealTimeClient] KeepAlive timed out, disconnecting")
                 Task { [weak self] in
-                    await self?.disconnect()
+                    await self?.reconnect()
                 }.toAnyCancellable.store(in: &self.cancellables)
             })
             .store(in: &cancellablesBindToConnection)
