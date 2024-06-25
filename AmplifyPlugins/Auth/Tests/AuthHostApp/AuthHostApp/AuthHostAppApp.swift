20d2673cefe32ed787e346c96afbd2776f797111
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2024-06-24 22:53:24 -0400

    testing out usage of Amplify.Model

--- AmplifyPlugins/Auth/Tests/AuthHostApp/AuthHostApp/AuthHostAppApp.swift
+++ AmplifyPlugins/Auth/Tests/AuthHostApp/AuthHostApp/AuthHostAppApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import Amplify
+import protocol Amplify.Model
 
 @main
 struct AuthHostAppApp: App {
+
+    typealias AmplifyModel = Amplify.Model
+
+    func doOneThing() async -> AmplifyModel { fatalError() }
+
+    func doOtherThing() async -> Amplify.Model { fatalError() }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
