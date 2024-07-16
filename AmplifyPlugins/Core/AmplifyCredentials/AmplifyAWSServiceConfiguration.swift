5d298dca99c21d59fabbf215ff84f670497a1f7b
aws-amplify-ops <aws-amplify-ops@amazon.com>
2024-07-16 16:22:16 +0000

    chore: release 2.35.7 [skip ci]

--- AmplifyPlugins/Core/AmplifyCredentials/AmplifyAWSServiceConfiguration.swift
+++ AmplifyPlugins/Core/AmplifyCredentials/AmplifyAWSServiceConfiguration.swift
@@ -15,7 +15,7 @@ import Amplify
 public class AmplifyAWSServiceConfiguration {
 
     /// - Tag: AmplifyAWSServiceConfiguration.amplifyVersion
-    public static let amplifyVersion = "2.35.6"
+    public static let amplifyVersion = "2.35.7"
 
     /// - Tag: AmplifyAWSServiceConfiguration.platformName
     public static let platformName = "amplify-swift"
