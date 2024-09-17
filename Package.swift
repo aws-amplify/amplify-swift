0175e7912e157adc85553cf340acfe2edf6b2974
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2024-09-17 15:34:41 -0400

    feat: Upgrading to AWS SDK for Swift 1.0.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let platforms: [SupportedPlatform] = [
     .watchOS(.v9)
 ]
 let dependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/awslabs/aws-sdk-swift.git", exact: "0.77.0"),
+    .package(url: "https://github.com/awslabs/aws-sdk-swift.git", exact: "1.0.0"),
     .package(url: "https://github.com/stephencelis/SQLite.swift.git", exact: "0.15.3"),
     .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: "2.1.0"),
     .package(url: "https://github.com/aws-amplify/amplify-swift-utils-notifications.git", from: "1.1.0")
