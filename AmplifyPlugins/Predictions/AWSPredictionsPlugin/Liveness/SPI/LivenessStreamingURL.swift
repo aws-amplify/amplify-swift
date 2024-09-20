c94c9d6825bdd32ba947bd7eaa91d4e98d28e5a6
Pramod Prajapati <prampraj@amazon.com>
2024-09-20 12:33:14 +0530

    Added region handling for creating correct streaming endpoint

--- AmplifyPlugins/Predictions/AWSPredictionsPlugin/Liveness/SPI/LivenessStreamingURL.swift
+++ AmplifyPlugins/Predictions/AWSPredictionsPlugin/Liveness/SPI/LivenessStreamingURL.swift
@@ -7,8 +7,20 @@
 
 import Foundation
 
+private let ISO_PARTITION_BASE_DOMAIN: String = "csp.hci.ic.gov"
+private let DEFAULT_BASE_DOMAIN: String = "amazonaws.com"
+
 func streamingSessionURL(for region: String) throws -> URL {
-    let urlString = "wss://streaming-rekognition.\(region).amazonaws.com/start-face-liveness-session-websocket"
+
+    // Determine the base domain based on the region
+    let baseDomain: String
+    if region.lowercased().starts(with: "us-isof") {
+        baseDomain = ISO_PARTITION_BASE_DOMAIN
+    } else {
+        baseDomain = DEFAULT_BASE_DOMAIN
+    }
+
+    let urlString = "wss://streaming-rekognition.\(region).\(baseDomain)/start-face-liveness-session-websocket"
     guard let url = URL(string: urlString) else {
         throw FaceLivenessSessionError.invalidRegion
     }
