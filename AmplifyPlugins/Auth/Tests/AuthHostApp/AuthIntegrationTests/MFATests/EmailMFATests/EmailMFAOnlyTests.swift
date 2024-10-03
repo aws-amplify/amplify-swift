4aead44770a95b2e0759e2d8d77418a21ccdea98
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2024-10-02 21:48:29 -0400

    integration tests wave 2

--- AmplifyPlugins/Auth/Tests/AuthHostApp/AuthIntegrationTests/MFATests/EmailMFATests/EmailMFAOnlyTests.swift
+++ AmplifyPlugins/Auth/Tests/AuthHostApp/AuthIntegrationTests/MFATests/EmailMFATests/EmailMFAOnlyTests.swift
@@ -12,12 +12,12 @@ import AWSAPIPlugin
 
 // MFA Required
 //  - Email
-class EmailMFAOnlyTests: AWSAuthBaseTest {
+class EmailMFARequiredTests: AWSAuthBaseTest {
 
     override func setUp() async throws {
         onlyUseGen2Configuration = true
         // Use a custom configuration these tests
-        amplifyOutputsFile = "testconfiguration/amplify_outputs"
+        amplifyOutputsFile = "testconfiguration/AWSCognitoEmailMFARequiredTests-amplify_outputs"
 
         let awsApiPlugin = AWSAPIPlugin()
         try Amplify.add(plugin: awsApiPlugin)
@@ -38,15 +38,20 @@ class EmailMFAOnlyTests: AWSAuthBaseTest {
     ///    - I invoke signIn with valid values
     /// - Then:
     ///    - I should get a .continueSignInWithEmailMFASetup response
-    ///
+    /// - When:
+    ///    - I invoke confirm signIn with valid values,
+    /// - Then:
+    ///    - With series of challenges, sign in should succeed
     //Requires only Email MFA to be enabled
-    func disabled_testSuccessfulEmailMFASetupStep() async {
+    func testSuccessfulEmailMFASetupStep() async {
 
         do {
+            createMFASubscription()
+
             let uniqueId = UUID().uuidString
             let username = "integTest\(uniqueId)"
             let password = "Pp123@\(uniqueId)"
-
+            
             _ = try await AuthSignInHelper.signUpUserReturningResult(
                 username: username,
                 password: password)
@@ -61,7 +66,40 @@ class EmailMFAOnlyTests: AWSAuthBaseTest {
                 XCTFail("Result should be .continueSignInWithEmailMFASetup for next step, instead got: \(result.nextStep)")
                 return
             }
+
+            // Step 2: pass an email to setup
+            var confirmSignInResult = try await Amplify.Auth.confirmSignIn(
+                challengeResponse: username + "@integTest.com")
+            guard case .confirmSignInWithEmailMFACode(let deliveryDetails) = confirmSignInResult.nextStep else {
+                XCTFail("Result should be .continueSignInWithEmailMFASetup but got: \(confirmSignInResult.nextStep)")
+                return
+            }
+            if case .email(let destination) = deliveryDetails.destination {
+                XCTAssertNotNil(destination)
+            } else {
+                XCTFail("Destination should be email")
+            }
+
             XCTAssertFalse(result.isSignedIn, "Signin result should be complete")
+
+            // step 3: confirm sign in
+            guard let mfaCode = try await waitForMFACode(for: username.lowercased()) else {
+                XCTFail("failed to retrieve the mfa code")
+                return
+            }
+            confirmSignInResult = try await Amplify.Auth.confirmSignIn(
+                challengeResponse: mfaCode,
+                options: .init())
+            guard case .done = confirmSignInResult.nextStep else {
+                XCTFail("Result should be .done for next step")
+                return
+            }
+            XCTAssertTrue(confirmSignInResult.isSignedIn, "Signin result should NOT be complete")
+            XCTAssertFalse(result.isSignedIn, "Signin result should be complete")
+
+            // email should get added to the account
+            let attributes = try await Amplify.Auth.fetchUserAttributes()
+            XCTAssertEqual(attributes.first(where: { $0.key == .email})?.value, username + "@integTest.com")
         } catch {
             XCTFail("Received failure with error \(error)")
         }
