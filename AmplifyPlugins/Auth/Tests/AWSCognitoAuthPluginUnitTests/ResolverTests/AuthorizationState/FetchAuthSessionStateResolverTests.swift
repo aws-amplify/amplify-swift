2ae536ee22c5a890c6f27d77655a80c04b224ddc
Tuan Pham <phantum@amazon.com>
2024-10-14 09:57:10 -0500

    remove commented code

--- AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/ResolverTests/AuthorizationState/FetchAuthSessionStateResolverTests.swift
+++ AmplifyPlugins/Auth/Tests/AWSCognitoAuthPluginUnitTests/ResolverTests/AuthorizationState/FetchAuthSessionStateResolverTests.swift
@@ -28,25 +28,8 @@ extension FetchAuthSessionStateSequence {
 class FetchAuthSessionStateResolverTests: XCTestCase {
 
     func testValidFetchAuthSessionStateSequences() throws {
-//        let cognitoSession = AWSAuthCognitoSession.testData
 
-        let validSequences: [FetchAuthSessionStateSequence] = [
-            //            StateSequence(oldState: .initializingFetchAuthSession,
-//                          event: FetchAuthSessionEvent(eventType: .fetchIdentity(cognitoSession)),
-//                          expected: .fetchingIdentity(FetchIdentityState.configuring)),
-//            StateSequence(oldState: .initializingFetchAuthSession,
-//                          event: FetchAuthSessionEvent(eventType: .fetchUserPoolTokens(cognitoSession)),
-//                          expected: .fetchingUserPoolTokens(FetchUserPoolTokensState.configuring)),
-//            StateSequence(oldState: .fetchingUserPoolTokens(FetchUserPoolTokensState.configuring),
-//                          event: FetchAuthSessionEvent(eventType: .fetchIdentity(cognitoSession)),
-//                          expected: .fetchingIdentity(FetchIdentityState.configuring)),
-//            StateSequence(oldState: .fetchingIdentity(FetchIdentityState.configuring),
-//                          event: FetchAuthSessionEvent(eventType: .fetchAWSCredentials(cognitoSession)),
-//                          expected: .fetchingAWSCredentials(FetchAWSCredentialsState.configuring)),
-//            StateSequence(oldState: .fetchingAWSCredentials(FetchAWSCredentialsState.fetched),
-//                          event: FetchAuthSessionEvent(eventType: .fetchedAuthSession(cognitoSession)),
-//                          expected: .sessionEstablished)
-        ]
+        let validSequences: [FetchAuthSessionStateSequence] = []
 
         for sequence in validSequences {
             sequence.assertResolvesToExpected()
