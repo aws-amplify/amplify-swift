aaaf694557b69d00b3c5ee137d2ce552646dca00
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2024-11-06 11:31:55 -0500

    chore: update no-auth API's in the resolver

--- aws-sdk-swift/Sources/Services/AWSCognitoIdentityProvider/Sources/AWSCognitoIdentityProvider/AuthSchemeResolver.swift
+++ aws-sdk-swift/Sources/Services/AWSCognitoIdentityProvider/Sources/AWSCognitoIdentityProvider/AuthSchemeResolver.swift
@@ -86,6 +86,20 @@ public struct DefaultCognitoIdentityProviderAuthSchemeResolver: CognitoIdentityP
                 validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
             case "verifyUserAttribute":
                 validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
+            case "listWebAuthnCredentials":
+                validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
+            case "completeWebAuthnRegistration":
+                validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
+            case "deleteWebAuthnCredential":
+                validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
+            case "startWebAuthnRegistration":
+                validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
+            case "getUserAuthFactors":
+                validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
+            case "getWebAuthnRegistrationOptions":
+                validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
+            case "verifyWebAuthnRegistrationResult":
+                validAuthOptions.append(SmithyHTTPAuthAPI.AuthOption(schemeID: "smithy.api#noAuth"))
             default:
                 var sigV4Option = SmithyHTTPAuthAPI.AuthOption(schemeID: "aws.auth#sigv4")
                 sigV4Option.signingProperties.set(key: SmithyHTTPAuthAPI.SigningPropertyKeys.signingName, value: "cognito-idp")
