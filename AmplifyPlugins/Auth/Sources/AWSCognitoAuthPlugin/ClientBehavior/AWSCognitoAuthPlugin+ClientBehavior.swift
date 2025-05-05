7fba1a764902b0e7760cae971ec82480cf80b63b
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2025-05-05 15:09:57 -0400

    fix merge issue.

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/ClientBehavior/AWSCognitoAuthPlugin+ClientBehavior.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/ClientBehavior/AWSCognitoAuthPlugin+ClientBehavior.swift
@@ -115,10 +115,12 @@ extension AWSCognitoAuthPlugin: AuthCategoryBehavior {
         let options = options ?? AuthFetchSessionRequest.Options()
         let request = AuthFetchSessionRequest(options: options)
         let forceReconfigure = secureStoragePreferences?.accessGroup?.name != nil
-        let task = AWSAuthFetchSessionTask(request,
-                                           authStateMachine: authStateMachine,
-                                           configuration: authConfiguration,
-                                           forceReconfigure: forceReconfigure)
+        let task = AWSAuthFetchSessionTask(
+            request,
+            authStateMachine: authStateMachine,
+            configuration: authConfiguration,
+            environment: authEnvironment,
+            forceReconfigure: forceReconfigure)
         return try await taskQueue.sync {
             return try await task.value
         } as! AuthSession
--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/AWSAuthFetchSessionTask.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/AWSAuthFetchSessionTask.swift
@@ -24,6 +24,7 @@ class AWSAuthFetchSessionTask: AuthFetchSessionTask, DefaultLogger {
         _ request: AuthFetchSessionRequest,
         authStateMachine: AuthStateMachine,
         configuration: AuthConfiguration,
+        environment: Environment,
         forceReconfigure: Bool = false
     ) {
         self.request = request
