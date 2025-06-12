d2e936696d2261dc9dba6515a3f06c4c2bad5cb3
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2025-06-11 23:56:11 -0400

    initial commit

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/Helpers/AWSAuthTaskHelper.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/Task/Helpers/AWSAuthTaskHelper.swift
@@ -72,14 +72,8 @@ class AWSAuthTaskHelper: DefaultLogger {
             throw AuthError.unknown("Unable to fetch auth session", nil)
         }
 
-        do {
-            let tokens = try cognitoTokenProvider.getCognitoTokens().get()
-            return tokens.accessToken
-        } catch let error as AuthError {
-            throw error
-        } catch {
-            throw AuthError.unknown("Unable to fetch auth session", error)
-        }
+        let tokens = try cognitoTokenProvider.getCognitoTokens().get()
+        return tokens.accessToken
     }
 
     func getCurrentUser() async throws -> AuthUser {
