bed94eda61519b81cfebc8857f28c8fcd64baef1
Liam Nichols <liam.nichols.ln@gmail.com>
2025-06-26 20:41:45 +0100

    Create AmplifyAvailability shim for Availability.h imports

--- Package.swift
+++ Package.swift
@@ -179,6 +179,7 @@ let authTargets: [Target] = [
         name: "AWSCognitoAuthPlugin",
         dependencies: [
             .target(name: "Amplify"),
+            .target(name: "AmplifyAvailability"),
             .target(name: "AmplifySRP"),
             .target(name: "AWSPluginsCore"),
             .target(name: "InternalAmplifyCredentials"),
@@ -190,6 +191,10 @@ let authTargets: [Target] = [
         resources: [
             .copy("Resources/PrivacyInfo.xcprivacy")
         ]
+    ),
+    .systemLibrary(
+        name: "AmplifyAvailability",
+        path: "AmplifyPlugins/Auth/Sources/AmplifyAvailability"
     ),
     .target(
         name: "libtommathAmplify",
