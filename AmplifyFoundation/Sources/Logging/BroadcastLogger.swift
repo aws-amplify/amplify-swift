9e14c449dfaadf85789fb153fba0ee2ff79ffbbd
Abhash Kumar Singh <thisisabhash@gmail.com>
2026-02-23 10:01:11 -0800

    feat: add logging definitions (#4139)
    * feat: add definitions for foundational apis
    
    * add AmplifyFoundation as library to Package manifest
    
    * update code
    
    * feat: add foundational bridge module
    
    * feat: add logging definitions
    
    * add tests and restructure code
    
    * address review comments

---
+++ AmplifyFoundation/Sources/Logging/BroadcastLogger.swift
@@ -0,0 +1,50 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/// This class acts as a layer of indirection conforming to Logger
+/// protocol that delegates all work to its sinks.
+///
+final class BroadcastLogger {
+    
+    let sinks: [any LogSinkBehavior]
+    let name: String
+
+    init(name: String, sinks: [any LogSinkBehavior]) {
+        self.name = name
+        self.sinks = sinks
+    }
+}
+
+extension BroadcastLogger: Logger {
+    func error(_ message: @autoclosure () -> String, _ error: @autoclosure () -> (any Error)?) {
+        log(.error, message(), error())
+    }
+    
+    func warn(_ message: @autoclosure () -> String, _ error: @autoclosure () -> (any Error)?) {
+        log(.warn, message(), error())
+    }
+    
+    func info(_ message: @autoclosure () -> String, _ error: @autoclosure () -> (any Error)?) {
+        log(.info, message(), error())
+    }
+    
+    func debug(_ message: @autoclosure () -> String, _ error: @autoclosure () -> (any Error)?) {
+        log(.debug, message(), error())
+    }
+    
+    func verbose(_ message: @autoclosure () -> String, _ error: @autoclosure () -> (any Error)?) {
+        log(.verbose, message(), error())
+    }
+    
+    func log(_ logLevel: LogLevel, _ message: @autoclosure () -> String, _ error: @autoclosure () -> (any Error)?) {
+        let messageValue = message()
+        let errorValue = error()
+        sinks.forEach { sink in
+            sink.emit(message: .init(level: logLevel, name: name, content: messageValue, error: errorValue))
+        }
+    }
+}
