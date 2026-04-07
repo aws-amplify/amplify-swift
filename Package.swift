291e6c9ab76f81b0b748d30c351eb6e1b8d121af
Harsh <6162866+harsh62@users.noreply.github.com>
2026-04-07 13:59:42 -0400

    chore(deps): bump SQLite.swift from 0.15.3 to 0.16.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let platforms: [SupportedPlatform] = [
 ]
 let dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/awslabs/aws-sdk-swift", exact: "1.6.71"),
-    .package(url: "https://github.com/stephencelis/SQLite.swift.git", exact: "0.15.3"),
+    .package(url: "https://github.com/stephencelis/SQLite.swift.git", exact: "0.16.0"),
     .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: "2.1.0"),
     .package(url: "https://github.com/aws-amplify/amplify-swift-utils-notifications.git", from: "1.1.0")
 ]
