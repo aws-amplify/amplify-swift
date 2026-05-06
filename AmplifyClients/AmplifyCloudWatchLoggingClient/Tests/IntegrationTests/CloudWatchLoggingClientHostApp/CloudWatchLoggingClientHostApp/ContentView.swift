26c6dfe23078041c7730878a6c1a8051b31d8f20
Abhash Kumar Singh <thisisabhash@gmail.com>
2026-05-06 16:41:20 -0700

    chore(logging): add integration tests for cloudwatch client

---
+++ AmplifyClients/AmplifyCloudWatchLoggingClient/Tests/IntegrationTests/CloudWatchLoggingClientHostApp/CloudWatchLoggingClientHostApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
